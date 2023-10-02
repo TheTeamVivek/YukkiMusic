8024eacf8a42b90a7f6c90d2a8581dafb77884ff
Pranav Saraswat <85411336+Pranav-Saraswat@users.noreply.github.com>
2023-10-02 09:28:22 +0530

    fix updater

--- config/config.py
+++ config/config.py
@@ -57,9 +57,9 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Pranav-Saraswat/Musiciophile",
+    "https://github.com/Pranav-Saraswat/Testing-Alpha",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "dev")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
