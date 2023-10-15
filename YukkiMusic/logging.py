da84aa95673045af60df662785c1a05c972e3cd5
Pranav Saraswat <85411336+Pranav-Saraswat@users.noreply.github.com>
2023-10-15 08:18:27 +0530

    revert to old

--- YukkiMusic/logging.py
+++ YukkiMusic/logging.py
@@ -26,7 +26,7 @@ logging.basicConfig(
 )
 
 logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.INFO)
+logging.getLogger("pytgcalls").setLevel(logging.ERRORq1)
 logging.getLogger("ntgcalls").setLevel(logging.ERROR)
 logging.getLogger("httpx").setLevel(logging.ERROR)
 
