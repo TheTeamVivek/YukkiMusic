5032eb6a54b9eaef0dde43a75e1eded71fabbc50
Pranav Saraswat <pranavsaraswat20040124@gmail.com>
2023-10-24 06:23:12 +0530

    Revert "4k thumbnails"
    This reverts commit f4c3c1a6d537a12d26d7e9d707cad3ad3a5c7175.

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -20,6 +20,7 @@ from youtubesearchpython.__future__ import VideosSearch
 
 from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
+
 def changeImageSize(maxWidth, maxHeight, image):
     widthRatio = maxWidth / image.size[0]
     heightRatio = maxHeight / image.size[1]
@@ -27,6 +28,7 @@ def changeImageSize(maxWidth, maxHeight, image):
     newHeight = int(heightRatio * image.size[1])
     newImage = image.resize((newWidth, newHeight))
     return newImage
+
 
 async def gen_thumb(videoid):
     if os.path.isfile(f"cache/{videoid}.png"):
@@ -59,77 +61,86 @@ async def gen_thumb(videoid):
         async with aiohttp.ClientSession() as session:
             async with session.get(thumbnail) as resp:
                 if resp.status == 200:
-                    f = await aiofiles.open(f"cache/thumb{videoid}.png", mode="wb")
+                    f = await aiofiles.open(
+                        f"cache/thumb{videoid}.png", mode="wb"
+                    )
                     await f.write(await resp.read())
                     await f.close()
 
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        image1 = changeImageSize(2160, 3840, youtube)
+        image1 = changeImageSize(1280, 720, youtube)
         image2 = image1.convert("RGBA")
         background = image2.filter(filter=ImageFilter.BoxBlur(30))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
         Xcenter = youtube.width / 2
         Ycenter = youtube.height / 2
-        x1 = Xcenter - 1000
-        y1 = Ycenter - 1000
-        x2 = Xcenter + 1000
-        y2 = Ycenter + 1000
+        x1 = Xcenter - 250
+        y1 = Ycenter - 250
+        x2 = Xcenter + 250
+        y2 = Ycenter + 250
         logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((1040, 1040), Image.LANCZOS)
-        logo = ImageOps.expand(logo, border=30, fill="white")
-        background.paste(logo, (100, 300))
+        logo.thumbnail((520, 520), Image.LANCZOS)
+        logo = ImageOps.expand(logo, border=15, fill="white")
+        background.paste(logo, (50, 100))
         draw = ImageDraw.Draw(background)
-        font = ImageFont.truetype("assets/font2.ttf", 80)
-        font2 = ImageFont.truetype("assets/font2.ttf", 140)
-        arial = ImageFont.truetype("assets/font2.ttf", 60)
-        name_font = ImageFont.truetype("assets/font.ttf", 60)
+        font = ImageFont.truetype("assets/font2.ttf", 40)
+        font2 = ImageFont.truetype("assets/font2.ttf", 70)
+        arial = ImageFont.truetype("assets/font2.ttf", 30)
+        name_font = ImageFont.truetype("assets/font.ttf", 30)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text(
-            (10, 10), f"{MUSIC_BOT_NAME}", fill="white", font=name_font)
+            (5, 5), f"{MUSIC_BOT_NAME}", fill="white", font=name_font
+        )
         draw.text(
-            (1200, 300),
+            (600, 150),
             "NOW PLAYING",
             fill="white",
-            stroke_width=4,
+            stroke_width=2,
             stroke_fill="white",
-            font=font2)
+            font=font2,
+        )
         for line in para:
             if j == 1:
                 j += 1
                 draw.text(
-                    (1200, 680),
+                    (600, 340),
                     f"{line}",
                     fill="white",
-                    stroke_width=2,
+                    stroke_width=1,
                     stroke_fill="white",
-                    font=font)
+                    font=font,
+                )
             if j == 0:
                 j += 1
                 draw.text(
-                    (1200, 540),
+                    (600, 280),
                     f"{line}",
                     fill="white",
-                    stroke_width=2,
+                    stroke_width=1,
                     stroke_fill="white",
-                    font=font)
+                    font=font,
+                )
 
         draw.text(
-            (1200, 900),
+            (600, 450),
             f"Views : {views[:23]}",
             (255, 255, 255),
-            font=arial)
+            font=arial,
+        )
         draw.text(
-            (1200, 1000),
+            (600, 500),
             f"Duration : {duration[:23]} Mins",
             (255, 255, 255),
-            font=arial)
+            font=arial,
+        )
         draw.text(
-            (1200, 1100),
+            (600, 550),
             f"Channel : {channel}",
             (255, 255, 255),
-            font=arial)
+            font=arial,
+        )
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
