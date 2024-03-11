e3b581626e83bb2417517d13e04c322ab84085d3
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-11 09:20:36 +0530

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -42,18 +42,19 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_2"],
                     callback_data="help_callback hb2",
                 ),
-            ],
-            [
+                
+
                 InlineKeyboardButton(
                     text=_["H_B_3"],
                     callback_data="help_callback hb3",
                 ),
+            ],
+            [
+                
                 InlineKeyboardButton(
                     text=_["H_B_4"],
                     callback_data="help_callback hb4",
                 ),
-            ],
-            [
                 InlineKeyboardButton(
                     text=_["H_B_6"],
                     callback_data="help_callback hb5",
@@ -62,13 +63,41 @@ def help_pannel(_, START: Union[bool, int] = None):
                     text=_["H_B_7"],
                     callback_data="help_callback hb6",
                 ),
+            ],
+            [
+
 
                 InlineKeyboardButton(
                     text=_["H_B_8"],
                     callback_data="help_callback hb7",
                 ),
-            ],
-            mark,
+            
+                 InlineKeyboardButton(
+                    text=_["H_B_9"],
+                    callback_data="help_callback hb8",
+                ),
+           ],
+
+            [
+                
+
+                 InlineKeyboardButton(
+                    text=_["H_B_10"],
+                    callback_data="help_callback hb9",
+                ),
+
+
+                 InlineKeyboardButton(
+                    text=_["H_B_11"],
+                    callback_data="help_callback hb10",
+                ),
+
+
+
+                 InlineKeyboardButton(
+                    text=_["H_B_12"],
+                    callback_data="help_callback hb11",
+                ),            mark,
         ]
     )
     return upl
