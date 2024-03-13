5e7391fbebb7da9fa673c3b346c754ddb28a2a84
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-13 16:18:06 +0530

    Update utils

---
+++ YukkiMusic/utils/theme.py
@@ -0,0 +1,24 @@
+import random
+from .utils.database import get_theme
+
+themes = [
+    "alexa1",
+    "alexa2",
+    "alexa3",
+    "alexa4",
+    "alexa5",
+    "alexa6",
+    "alexa7",
+    "alexa8",
+]
+
+
+async def check_theme(chat_id: int):
+    _theme = await get_theme(chat_id, "theme")
+    if not _theme:
+        theme = random.choice(themes)
+    else:
+        theme = _theme["theme"]
+        if theme == "Random":
+            theme = random.choice(themes)
+    return theme
