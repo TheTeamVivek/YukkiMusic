439b5adde9c9349ccd3991789814b644d4045f6a
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-13 12:00:28 +0530

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -37,7 +37,7 @@ selections = [
 
 
 def stream_markup_timer(_, videoid, chat_id, played, dur):
-        played_sec = time_to_seconds(played)
+    played_sec = time_to_seconds(played)
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     anon = math.floor(percentage)
