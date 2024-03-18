9e753396665ceb7787098218041ea684dfe4b0f1
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-18 16:08:49 +0530

    Update inline

---
+++ YukkiMusic/utils/inline/extras.py
@@ -0,0 +1,19 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from config import SUPPORT_CHAT
+
+
+
+
+def supp_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["S_B_9"],
+                    url=SUPPORT_CHAT,
+                ),
+            ]
+        ]
+    )
+    return upl
