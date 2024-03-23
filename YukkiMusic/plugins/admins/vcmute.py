7f53a3d69b7b592eb933fbc27382ac69370dcf9f
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-23 21:55:52 +0530

    Update vcmute.py

--- YukkiMusic/plugins/admins/vcmute.py
+++ YukkiMusic/plugins/admins/vcmute.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from pyrogram import filters
 from pyrogram.types import Message
 from config import BANNED_USERS
