eb8ca2369572804583970f41a2909ce230d0d1e5
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-23 08:56:03 +0530

    Add files via upload

---
+++ YukkiMusic/plugins/tools/fake.py
@@ -0,0 +1,45 @@
+import requests
+from YukkiMusic import app
+from pyrogram import filters
+
+
+@app.on_message(filters.command(["FAKE","fake"]))
+async def fkadress(_, message):
+    query = message.text.split(maxsplit=1)[1].strip()
+    url = f"https://randomuser.me/api/?nat={query}"
+    response = requests.get(url)
+    data = response.json()
+
+    if "results" in data:
+        fk = data["results"][0]
+
+
+        name = f"{fk['name']['title']} {fk['name']['first']} {fk['name']['last']}"
+        address = f"{fk['location']['street']['number']} {fk['location']['street']['name']}" 
+        city = fk['location']['city']
+        state = fk['location']['state']
+        country = fk['location']['country'] 
+        postal = fk['location']['postcode']
+        email = fk['email']
+        phone = fk['phone']
+        picture = fk['picture']['large']
+        gender = fk['gender']
+
+
+        fkinfo= f"""
+﹝⌬﹞**ɴᴀᴍᴇ** ⇢ {name}
+﹝⌬﹞**ɢᴇɴᴅᴇʀ** ⇢ {gender}
+﹝⌬﹞**ᴀᴅᴅʀᴇss** ⇢ {address}
+﹝⌬﹞**ᴄᴏᴜɴᴛʀʏ** ⇢ {country}
+﹝⌬﹞**ᴄɪᴛʏ** ⇢ {city}
+﹝⌬﹞**sᴛᴀᴛᴇ** ⇢ {state}
+﹝⌬﹞**ᴘᴏsᴛᴀʟ** ⇢ {postal}
+﹝⌬﹞**ᴇᴍᴀɪʟ** ⇢ {email}
+﹝⌬﹞**ᴘʜᴏɴᴇ** ⇢ {phone}
+
+        """
+
+
+        await message.reply_photo(photo=picture, caption=fkinfo)
+    else:
+        await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.\nᴛʀʏ ᴀɢᴀɪɴ")
