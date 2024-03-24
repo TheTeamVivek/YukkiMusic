b13a5b996716a8ddac015529436bcdb4986f3f07
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-24 11:48:58 +0530

    Update misc

---
+++ YukkiMusic/plugins/misc/bot_add_left.py
@@ -0,0 +1,51 @@
+from pyrogram import Client
+from pyrogram.types import Message
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, InputMediaVideo, Message
+from config import LOGGER_ID
+from YukkiMusic import app  
+from YukkiMusic.core.userbot import Userbot
+from YukkiMusic.utils.database import get_assistant
+from YukkiMusic.utils.database import delete_served_chat
+
+@app.on_message(filters.new_chat_members, group=2)
+async def join_watcher(_, message):    
+    try:
+        userbot = await get_assistant(message.chat.id)
+        chat = message.chat
+        for members in message.new_chat_members:
+            if members.id == app.id:
+                count = await app.get_chat_members_count(chat.id)
+                username = message.chat.username if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐆ʀᴏᴜᴘ"
+                msg = (
+                    f"**📝𝐌ᴜsɪᴄ 𝐁ᴏᴛ 𝐀ᴅᴅᴇᴅ 𝐈ɴ 𝐀 #𝐍ᴇᴡ_𝐆ʀᴏᴜᴘ**\n\n"
+                    f"**📌𝐂ʜᴀᴛ 𝐍ᴀᴍᴇ:** {message.chat.title}\n"
+                    f"**🍂𝐂ʜᴀᴛ 𝐈ᴅ:** {message.chat.id}\n"
+                    f"**🔐𝐂ʜᴀᴛ 𝐔sᴇʀɴᴀᴍᴇ:** @{username}\n"
+                    f"**📈𝐆ʀᴏᴜᴘ 𝐌ᴇᴍʙᴇʀs:** {count}\n"
+                    f"**🤔𝐀ᴅᴅᴇᴅ 𝐁ʏ:** {message.from_user.mention}"
+                )
+                await app.send_photo(LOGGER_ID, text=msg, reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton(f"😍𝐀ᴅᴅᴇᴅ 𝐁ʏ😍", url=f"tg://openmessage?user_id={message.from_user.id}")]
+             ]))
+                await userbot.join_chat(f"{username}")
+    except Exception as e:
+        print(f"Error: {e}")
+        
+@app.on_message(filters.left_chat_member)
+async def on_left_chat_member(_, message: Message):
+    try:
+        userbot = await get_assistant(message.chat.id)
+
+        left_chat_member = message.left_chat_member
+        if left_chat_member and left_chat_member.id == (await app.get_me()).id:
+            remove_by = message.from_user.mention if message.from_user else "𝐔ɴᴋɴᴏᴡɴ 𝐔sᴇʀ"
+            title = message.chat.title
+            username = f"@{message.chat.username}" if message.chat.username else "𝐏ʀɪᴠᴀᴛᴇ 𝐂ʜᴀᴛ"
+            chat_id = message.chat.id
+            left = f"✫ <b><u>#𝐋ᴇғᴛ_𝐆ʀᴏᴜᴘ</u></b> ✫\n\n𝐂ʜᴀᴛ 𝐓ɪᴛʟᴇ : {title}\n\n𝐂ʜᴀᴛ 𝐈ᴅ : {chat_id}\n\n𝐑ᴇᴍᴏᴠᴇᴅ 𝐁ʏ : {remove_by}\n\n𝐁ᴏᴛ : @{app.username}"
+            await app.send_photo(LOGGER_ID, photo=left)
+            await delete_served_chat(chat_id)
+            await userbot.leave_chat(chat_id)
+    except Exception as e:
+        print(f"Error: {e}")
