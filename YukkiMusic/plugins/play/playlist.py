3f03435db8fde30230336be0a7a6075b78463d4b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-25 10:00:07 +0530

    Update playlist.py

--- YukkiMusic/plugins/play/playlist.py
+++ YukkiMusic/plugins/play/playlist.py
@@ -270,8 +270,7 @@ async def play_playlist(client, CallbackQuery, _):
     return await mystic.delete()
 
 @app.on_message(filters.command(["playplaylist"]) & ~BANNED_USERS & filters.group)
-#@languageCB
-@PlayWrapper
+@languageCB
 async def play_playlist_command(client, message, _):
     mode = message.command[1] if len(message.command) > 1 else None
     user_id = message.from_user.id
