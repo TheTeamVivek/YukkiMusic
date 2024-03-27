62a175d2d49a73ad0f84d725b4272c8f69a24a84
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-27 12:29:13 +0530

    Update tools

---
+++ YukkiMusic/plugins/tools/clearmongo.py
@@ -0,0 +1,32 @@
+from pyrogram import Client, filters
+from pymongo import MongoClient
+from .. Mukesh as app
+# Replace with your MongoDB URI
+mongodb_uri = ""
+
+
+
+def clear_mongodb_data(mongodb_uri):
+    try:
+        # Connect to MongoDB
+        client = MongoClient(mongodb_uri)
+        
+        # Select the database
+        db = client.get_default_database()
+        
+        # Clear all data from the collection
+        db.collection_name.delete_many({})
+        
+        return "All data cleared from MongoDB successfully."
+        
+    except Exception as e:
+        return f"An error occurred: {e}"
+
+@app.on_message(filters.command("clearmongo") & ~filters.private)
+def clear_mongo_data(client, message):
+    if len(message.command) == 2:
+        mongodb_uri = message.command[1]
+        response = clear_mongodb_data(mongodb_uri)
+        message.reply_text(response)
+    else:
+        message.reply_text("Invalid command usage. Correct format: /clearmongo <mongodb_uri>")
