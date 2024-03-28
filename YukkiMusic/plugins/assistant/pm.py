c8c21638b2365094d2e5122e407f20408e8e5c3d
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-28 10:37:12 +0530

    Update assistant

---
+++ YukkiMusic/plugins/assistant/pm.py
@@ -0,0 +1,10 @@
+import asyncio
+from YukkiMusic.core.userbot import assistants
+
+async def initialize_clients():
+    client = await get_client(num)
+    return client
+
+async def main():
+    client = await initialize_clients()
+    return client
