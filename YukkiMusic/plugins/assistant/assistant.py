0e71ebf6b78f17df53aa6008c4fbf4df26674354
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-28 08:27:03 +0530

    Update assistant.py

--- YukkiMusic/plugins/assistant/assistant.py
+++ YukkiMusic/plugins/assistant/assistant.py
@@ -1,4 +1,5 @@
 import os
+from inspect import getfullargspec
 from pyrogram import Client, filters
 from pyrogram.types import (CallbackQuery, InlineKeyboardButton,
                             InlineKeyboardMarkup, InlineQueryResultArticle,
@@ -40,7 +41,7 @@ async def set_bio(client, message):
         return await eor(message, text="Give some text to set as bio.")
     elif len(message.command) > 1:
     	userbot = await get_client(1)
-        bio = message.text.split(None, 1)[1]
+     bio = message.text.split(None, 1)[1]
         try:
             await userbot.update_profile(bio=bio)
             await eor(message, text="Changed Bio.")
@@ -48,3 +49,13 @@ async def set_bio(client, message):
             await eor(message, text=e)
     else:
         return await eor(message, text="Give some text to set as bio.")
+
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
