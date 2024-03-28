6356a542e9e463888273533d05b1e756919b6cec
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-28 08:14:38 +0530

    Update assistant.py

--- YukkiMusic/plugins/assistant/assistant.py
+++ YukkiMusic/plugins/assistant/assistant.py
@@ -47,3 +47,13 @@ async def set_bio(client, message):
             await eor(message, text=e)
     else:
         return await eor(message, text="Give some text to set as bio.")
+
+
+async def eor(msg: Message, **kwargs):
+    func = (
+        (msg.edit_text if msg.from_user.is_self else msg.reply)
+        if msg.from_user
+        else msg.reply
+    )
+    spec = getfullargspec(func.__wrapped__).args
+    return await func(**{k: v for k, v in kwargs.items() if k in spec})
