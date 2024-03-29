9877ee4d3355ad66b382dbd3ba40a9588f9c3e81
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-29 17:19:54 +0530

    Update help.py

--- YukkiMusic/utils/inline/help.py
+++ YukkiMusic/utils/inline/help.py
@@ -27,7 +27,7 @@ InlineKeyboardButton(text="ɴᴇxᴛ", callback_data="yukkisecondpage")
                 InlineKeyboardButton(text=_["H_B_8"], callback_data="help_callback hb7")],
          [    InlineKeyboardButton(text=_["H_B_3"], callback_data="help_callback hb3"),
                 InlineKeyboardButton(text=_["H_B_10"], callback_data="help_callback hb9"),
-                InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb6"]
+                InlineKeyboardButton(text=_["H_B_7"], callback_data="help_callback hb6")],
           [    InlineKeyboardButton(text=_["H_B_6"], callback_data="help_callback hb5" ),
                 InlineKeyboardButton(text=_["H_B_4"], callback_data="help_callback hb4"),
                 InlineKeyboardButton(text=_["H_B_2"],callback_data="help_callback hb2")],
