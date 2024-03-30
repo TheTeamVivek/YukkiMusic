b54842964401f9edb518b4a12a936c1e3b4b08ce
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-03-30 15:29:48 +0530

    Update seeker.py

--- YukkiMusic/plugins/misc/seeker.py
+++ YukkiMusic/plugins/misc/seeker.py
@@ -41,7 +41,7 @@ asyncio.create_task(timer())
 
 
 async def markup_timer():
-    while not await asyncio.sleep(20):
+    while not await asyncio.sleep(300):
         active_chats = await get_active_chats()
         for chat_id in active_chats:
             try:
