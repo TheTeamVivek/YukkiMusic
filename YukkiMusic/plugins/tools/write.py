75be4f38fa0760c5532210fa84efa24cd7437020
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-02 15:04:29 +0530

    Update write.py

--- YukkiMusic/plugins/tools/write.py
+++ YukkiMusic/plugins/tools/write.py
@@ -4,18 +4,11 @@ from YukkiMusic import app
 
 @app.on_message(filters.command(["w","n"]))
 def write_text(client, message):
-    # Check if there is text following the command
-    if len(message.command) < 1:
-        # Reply to the user asking to provide text
-        message.reply_text("Please provide text after the /write command.")
+    if len(message.command) < 2:
+        message.reply_text("Please provide text after the /write command.\nMust be greater than 2 words")
         return
-    
-    # Extract the text after /write command
     text = " ".join(message.command[1:])
-    
-    # Replace <text> in the URL with the user input message
     photo_url = "https://apis.xditya.me/write?text=" + text
-    
     app.send_photo(
         chat_id=message.chat.id,
         photo=photo_url,
