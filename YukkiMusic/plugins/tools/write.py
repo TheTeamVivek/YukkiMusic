52e5ec31eb45c10858e2b606130fa1fe09b27d9d
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-02 15:46:29 +0530

    Update write.py

--- YukkiMusic/plugins/tools/write.py
+++ YukkiMusic/plugins/tools/write.py
@@ -8,9 +8,9 @@ async def write_text(client, message):
     if len(message.command) < 2:
         await message.reply_text("**Usage**:- `/write jai shree ram`")
         return
-    # Join all command arguments into a single string
+
     text = " ".join(message.command[1:])
-    # Encode the text to replace spaces with %20
+
     encoded_text = quote(text)
     photo_url = "https://apis.xditya.me/write?text=" + encoded_text
     await app.send_photo(
