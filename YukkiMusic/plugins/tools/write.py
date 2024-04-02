b242739310cbef2facbc7da449b4333c79872f4c
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-02 15:10:22 +0530

    Update write.py

--- YukkiMusic/plugins/tools/write.py
+++ YukkiMusic/plugins/tools/write.py
@@ -3,13 +3,14 @@ import requests
 from YukkiMusic import app 
 
 @app.on_message(filters.command(["write","note"]))
-def write_text(client, message):
+
+async def write_text(client, message):
     if len(message.command) < 2:
         message.reply_text("**ᴜsᴀsɢᴇ**:- `/write jai shree ram`")
         return
     text = " ".join(message.command[1:])
     photo_url = "https://apis.xditya.me/write?text=" + text
-    app.send_photo(
+    await app.send_photo(
         chat_id=message.chat.id,
         photo=photo_url,
         caption="ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɴᴏᴛᴇs"
