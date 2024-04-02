fdddd3ab7d9ff10badfc20dbb66c7330b1de77e1
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-02 15:15:12 +0530

    Update write.py

--- YukkiMusic/plugins/tools/write.py
+++ YukkiMusic/plugins/tools/write.py
@@ -2,15 +2,14 @@ from pyrogram import Client, filters
 from YukkiMusic import app 
 
 @app.on_message(filters.command(["write","note"]))
-
 async def write_text(client, message):
     if len(message.command) < 2:
-        message.reply_text("**ᴜsᴀsɢᴇ**:- `/write jai shree ram`")
+        await message.reply_text("**Usage**:- `/write jai shree ram`")
         return
     text = " ".join(message.command[1:])
     photo_url = "https://apis.xditya.me/write?text=" + text
     await app.send_photo(
         chat_id=message.chat.id,
         photo=photo_url,
-        caption="ʜᴇʀᴇ ɪs ʏᴏᴜʀ ɴᴏᴛᴇs"
+        caption="Here is your note"
     )
