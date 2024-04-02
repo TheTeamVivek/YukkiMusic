e87af9943c7a503ce4546e9624397ffad99d6055
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-02 15:48:06 +0530

    Update write.py

--- YukkiMusic/plugins/tools/write.py
+++ YukkiMusic/plugins/tools/write.py
@@ -8,8 +8,14 @@ async def write_text(client, message):
     if len(message.command) < 2:
         await message.reply_text("**Usage**:- `/write jai shree ram`")
         return
+    
 
-    text = " ".join(message.command[1:])
+    if message.reply_to_message and message.reply_to_message.text:
+        text = message.reply_to_message.text
+    else:
+
+        text = " ".join(message.command[1:])
+        
 
     encoded_text = quote(text)
     photo_url = "https://apis.xditya.me/write?text=" + encoded_text
