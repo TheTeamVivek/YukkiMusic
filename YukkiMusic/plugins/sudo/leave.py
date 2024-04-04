d9dc3d4e8b3ae600a9e9ab13c2758b867d2f84fe
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-04 10:16:05 +0530

    Update sudo

---
+++ YukkiMusic/plugins/sudo/leave.py
@@ -0,0 +1,31 @@
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from YukkiMusic import app
+from YukkiMusic.misc import SUDOERS
+from YukkiMusic.utils.database import get_assistant
+
+@app.on_message(filters.command(["leave"]) & SUDOERS)
+async def leave_group(client: Client, message: Message):
+    if len(message.command) != 2:
+        await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛɪᴅ ᴀғᴛᴇʀ /leave ᴛᴏ ʟᴇᴀᴠᴇ")
+        return
+
+    group_id = message.command[1]
+    try:
+        chat = await client.get_chat(int(group_id))
+
+        if chat is None:
+            await message.reply("ɪ ᴛʜɪɴᴋ ᴛʜᴇ ᴄʜᴀᴛɪᴅ ɪs ᴡʀᴏɴɢ ᴄᴀɴ ʏᴏᴜ ᴄʜᴇᴄᴋ ᴛʜɪs ᴀɢᴀɪɴ ᴘʟᴇᴀsᴇ")
+            return
+
+        try:
+            lol = await message.reply(f"ʟᴇᴀᴠɪɴ ғʀᴏᴍ {chat.title}")
+            await client.leave_chat(int(group_id))
+            await asyncio.sleep(1)
+            await lol.edit(f"ʙᴏᴛ ʟᴇғᴛᴇᴅ ғʀᴏᴍ {chat.title}")         
+        except Exception as e:
+            await message.reply(f"sᴏᴍᴇ ᴇxᴄᴇᴘᴛɪᴏɴ ᴡʜɪʟᴇ ʟᴇᴀᴠɪɴɢ \n {e}")
+            return
+    except ValueError:
+        await message.reply("ɪɴᴠᴀʟɪᴅ ᴄʜᴀᴛ ɪᴅ")
