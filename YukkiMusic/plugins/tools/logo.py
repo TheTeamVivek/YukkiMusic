a7363fcb30837e81b81ac7202548adf970d2c94b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-05 19:27:04 +0530

    Update logo.py

--- YukkiMusic/plugins/tools/logo.py
+++ YukkiMusic/plugins/tools/logo.py
@@ -4,11 +4,13 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 @app.on_message(filters.command(["logo", "logomaker"], prefixes=["'", '!', '/']))
-def handle_logo_command(client, message: Message):
+async def handle_logo_command(client, message: Message):
     if len(message.command) != 2:
-            return await message.reply_text(
-                "**» ɢɪᴠᴇ ᴀ  ɴᴀᴍᴇ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ..**"
-            )
+        await message.reply_text(
+            "**» ɢɪᴠᴇ ᴀ  ɴᴀᴍᴇ ᴛᴏ ᴄʀᴇᴀᴛᴇ ʟᴏɢᴏ..**"
+        )
+        return
+
     name = message.text.split(maxsplit=1)[1]
     base_url = "https://logomaker.apinepdev.workers.dev/?logoname={}".format(name)
 
@@ -17,17 +19,10 @@ def handle_logo_command(client, message: Message):
         response.raise_for_status()
         if 'logo' in response.json():
             logo_url = response.json()['logo']
-            
-            message.reply_photo(logo_url)
+            await message.reply_photo(logo_url)
         else:
-            message.reply_text("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ")
+            await message.reply_text("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ")
     except Exception as e:
         print(f"Error fetching or sending image: {e}")
-        message.reply_text("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴏʀ sᴇɴᴅ ɪᴍᴀɢᴇ.")
+        await message.reply_text("Fᴀɪʟᴇᴅ ᴛᴏ ғᴇᴛᴄʜ ᴏʀ sᴇɴᴅ ɪᴍᴀɢᴇ.")
         await app.send_message(LOG_GROUP_ID,text=f"ᴇʀʀᴏʀ ᴏɴ logo.py \n ᴇʀʀᴏʀ ɪs {e}")
-        
-"""
-ᴛʜɪs ʙᴏᴛ ᴄᴀɴ ᴄʀᴇᴀᴛᴇ sᴏᴍᴇ ʙᴇᴀᴜᴛɪғᴜʟ ᴀɴᴅ ᴀᴛᴛʀᴀᴄᴛɪᴠᴇ ʟᴏɢᴏ ғᴏʀ ʏᴏᴜʀ ᴘʀᴏғɪʟᴇ ᴘɪᴄs.
-
-❍ /logo (Text) *:* ᴄʀᴇᴀᴛᴇ ᴀ ʟᴏɢᴏ ᴏғ ʏᴏᴜʀ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴡɪᴛʜ ʀᴀɴᴅᴏᴍ ᴠɪᴇᴡ.
-"""
