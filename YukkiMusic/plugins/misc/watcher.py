35fbe118f9767093d1fa84fe2371d33bece173d8
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-07 19:42:20 +0530

    Update misc

---
+++ YukkiMusic/plugins/misc/watcher.py
@@ -0,0 +1,10 @@
+from pyrogram import filters
+from pyrogram.types import Message
+
+from YukkiMusic import app
+from YukkiMusic.core.call import Yukki
+
+@app.on_message(filters.video_chat_started, group=20)
+@app.on_message(filters.video_chat_ended, group=30)
+async def vc_close_open(_, message: Message):
+    await Yukki.stop_stream_force(message.chat.id)
