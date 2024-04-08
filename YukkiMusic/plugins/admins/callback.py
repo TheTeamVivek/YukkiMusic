52b5bd534e0fb29fcb109bd67e1cd51afb120bc7
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-08 14:54:40 +0530

    Update callback.py

--- YukkiMusic/plugins/admins/callback.py
+++ YukkiMusic/plugins/admins/callback.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
 #
 # This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
 # and is released under the "GNU v3.0 License Agreement".
@@ -38,12 +38,13 @@ from YukkiMusic.utils.formatters import seconds_to_min
 from YukkiMusic.utils.inline.play import (
     panel_markup_1,
     panel_markup_2,
-    panel_markup_3,
+    controler_markup
     stream_markup,
     telegram_markup,
 )
 from YukkiMusic.utils.stream.autoclear import auto_clean
 from YukkiMusic.utils.thumbnails import gen_thumb
+
 
 
 wrong = {}
@@ -69,6 +70,33 @@ async def markup_panel(client, CallbackQuery: CallbackQuery, _):
     if chat_id not in wrong:
         wrong[chat_id] = {}
     wrong[chat_id][CallbackQuery.message.id] = False
+    
+    
+@app.on_callback_query(filters.regex("ControlMarkup") & ~BANNED_USERS)
+@languageCB
+async def markup_panel(client, CallbackQuery: CallbackQuery, _):
+    await CallbackQuery.answer()
+    callback_data = CallbackQuery.data.strip()
+    callback_request = callback_data.split(None, 1)[1]
+    videoid, chat_id = callback_request.split("|")
+    chat_id = CallbackQuery.message.chat.id
+    playing = db.get(chat_id)
+    buttons = controler_markup(_,
+                            playing[0]["vidid"],
+                            chat_id,
+                            seconds_to_min(playing[0]["played"]),
+                            playing[0]["dur"],)
+    try:
+        await CallbackQuery.edit_message_reply_markup(
+            reply_markup=InlineKeyboardMarkup(buttons)
+        )
+    except:
+        return
+    if chat_id not in wrong:
+        wrong[chat_id] = {}
+    wrong[chat_id][CallbackQuery.message.id] = False
+    
+    
 
 
 @app.on_callback_query(filters.regex("MainMarkup") & ~BANNED_USERS)
@@ -78,10 +106,7 @@ async def del_back_playlist(client, CallbackQuery, _):
     callback_data = CallbackQuery.data.strip()
     callback_request = callback_data.split(None, 1)[1]
     videoid, chat_id = callback_request.split("|")
-    if videoid == str(None):
-        buttons = telegram_markup(_, chat_id)
-    else:
-        buttons = stream_markup(_, videoid, chat_id)
+    buttons = stream_markup(_, videoid, chat_id)
     chat_id = CallbackQuery.message.chat.id
     try:
         await CallbackQuery.edit_message_reply_markup(
@@ -105,18 +130,15 @@ async def del_back_playlist(client, CallbackQuery, _):
     pages = int(pages)
     if state == "Forw":
         if pages == 0:
-            buttons = panel_markup_2(_, videoid, chat_id)
-        if pages == 2:
             buttons = panel_markup_1(_, videoid, chat_id)
         if pages == 1:
-            buttons = panel_markup_3(_, videoid, chat_id)
+            buttons = panel_markup_2(_, videoid, chat_id)
     if state == "Back":
-        if pages == 2:
-            buttons = panel_markup_2(_, videoid, chat_id)
-        if pages == 1:
-            buttons = panel_markup_1(_, videoid, chat_id)
         if pages == 0:
-            buttons = panel_markup_3(_, videoid, chat_id)
+            buttons = panel_markup_1(_, videoid, chat_id)
+        
+        if pages == 1:
+            buttons = panel_markup_2(_, videoid, chat_id)
     try:
         await CallbackQuery.edit_message_reply_markup(
             reply_markup=InlineKeyboardMarkup(buttons)
