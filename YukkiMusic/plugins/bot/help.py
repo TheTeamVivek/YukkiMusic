bc38f92c23ad1da8d231060581f5d534d2060486
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-09 17:20:53 +0530

    Update help.py

--- YukkiMusic/plugins/bot/help.py
+++ YukkiMusic/plugins/bot/help.py
@@ -65,14 +65,16 @@ async def helper_private(
         keyboard = first_page(_)
         if START_IMG_URL:
             await update.reply_photo(
-                photo=START_IMG_URL, caption=_["help_1"],
+                photo=START_IMG_URL, 
+                caption=_["help_1"],
                 PK, 
                 reply_markup=keyboard
             )
                 
         else:
             await update.reply_photo(
-                photo=random.choice(PHOTO), caption=_["help_1"],
+                photo=random.choice(PHOTO), 
+                caption=_["help_1"],
                 PK,
                 reply_markup=keyboard
             )
