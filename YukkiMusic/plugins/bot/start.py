70e89e0bacf35d16c7f2a4f9cf2e4c99a7f85c7e
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-09 15:42:53 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -135,9 +135,14 @@ async def start_comm(client, message: Message, _):
 
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            return await message.reply_photo(
-                photo=config.START_IMG_URL, caption=_["help_1"], reply_markup=keyboard
-            )
+            if config.START_IMG_URL:
+                return await message.reply_photo(
+                    photo=config.START_IMG_URL, caption=_["help_1"], reply_markup=keyboard
+                )
+            else:
+                return await message.reply_photo(
+                    photo=random.choice(PHOTO), caption=_["help_1"], reply_markup=keyboard
+                )
         if name[0:4] == "song":
             await message.reply_text(_["song_2"])
             return await app.send_reaction(chat_id, message_id)
