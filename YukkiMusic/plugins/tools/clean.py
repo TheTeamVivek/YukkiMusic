e3ebf64a157127b2eab36e4046daaaae595b81a7
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-11 17:51:16 +0530

    Update tools

---
+++ YukkiMusic/plugins/tools/clean.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+from os import listdir, mkdir
+from YukkiMusic import app
+from YukkiMusi.misc import SUDOERS
+@app.on_message(filters.command("clean") & SUDOERS)
+async def clean(_, message):
+    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
