f1e20e1b6c686ac54d1f236633107b69ed739880
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-13 22:33:03 +0530

    Update filter.py

--- YukkiMusic/utils/filter.py
+++ YukkiMusic/utils/filter.py
@@ -1,32 +1,12 @@
-from pyrogram.types import Message
-from pyrogram.enums import ChatType, ChatMemberStatus
-from YukkiMusic.misc import SUDOERS
+from pyrogram import filters
+from YukkiMusic.utils.admin_check import admin_check
 
-async def admin_check(message: Message) -> bool:
-    if not message.from_user:
-        return False
 
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
-        return False
+async def admin_filter_f(filt, client, message):
+    return (
+        not message.edit_date
+        and await admin_check(message)
+    )
 
-    if message.from_user.id in [
-        777000,  # Telegram Service Notifications
-        1087968824,  # GroupAnonymousBot
-    ]:
-        return True
 
-    if message.from_user.id in SUDOERS:
-        return True
-
-    client = message._client
-    chat_id = message.chat.id
-    user_id = message.from_user.id
-
-    check_status = await client.get_chat_member(chat_id=chat_id, user_id=user_id)
-    if check_status.status not in [
-        ChatMemberStatus.OWNER,
-        ChatMemberStatus.ADMINISTRATOR,
-    ]:
-        return False
-    else:
-        return True
+admin_filter = filters.create(func=admin_filter_f, name="AdminFilter")
