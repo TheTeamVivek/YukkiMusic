fa7f544c196fd9eb2084304fb85917c312be4de7
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-13 23:35:47 +0530

    Update speed.py

--- YukkiMusic/plugins/admins/speed.py
+++ YukkiMusic/plugins/admins/speed.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram import filters
 from pyrogram.types import Message
