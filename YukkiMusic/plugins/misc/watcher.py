a4a0e961176fd9139c360f144d766c63a135eac4
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-14 13:51:18 +0530

    Update watcher.py

--- YukkiMusic/plugins/misc/watcher.py
+++ YukkiMusic/plugins/misc/watcher.py
@@ -8,4 +8,5 @@ from YukkiMusic.core.call import Yukki
 @app.on_message(filters.video_chat_started, group=20)
 @app.on_message(filters.video_chat_ended, group=30)
 async def vc_close_open(_, message: Message):
-    await Yukki.stop_stream_force(message.chat.id)
+    await Yukki.force_stop_stream(message.chat.id)
+
