ced9e01faa79f371566c350a571fc6301341628d
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-16 22:26:25 +0530

    Update memorydatabase.py

--- YukkiMusic/utils/database/memorydatabase.py
+++ YukkiMusic/utils/database/memorydatabase.py
@@ -11,6 +11,7 @@
 
 import config
 from config import PRIVATE_BOT_MODE
+from typing import Dict, List, Union
 from YukkiMusic.core.mongo import mongodb
 
 channeldb = mongodb.cplaymode
@@ -25,6 +26,7 @@ onoffdb = mongodb.onoffper
 suggdb = mongodb.suggestion
 autoenddb = mongodb.autoend
 coupledb = mongodb.couple
+notesdb = mongodb.notes
 
 # Shifting to memory [ mongo sucks often]
 loop = {}
@@ -45,6 +47,66 @@ vlimit = []
 maintenance = []
 suggestion = {}
 autoend = {}
+
+async def get_notes_count() -> dict:
+    chats_count = 0
+    notes_count = 0
+    async for chat in notesdb.find({"chat_id": {"$exists": 1}}):
+        notes_name = await get_note_names(chat["chat_id"])
+        notes_count += len(notes_name)
+        chats_count += 1
+    return {"chats_count": chats_count, "notes_count": notes_count}
+
+
+async def _get_notes(chat_id: int) -> Dict[str, int]:
+    _notes = await notesdb.find_one({"chat_id": chat_id})
+    if not _notes:
+        return {}
+    return _notes["notes"]
+
+
+async def get_note_names(chat_id: int) -> List[str]:
+    _notes = []
+    for note in await _get_notes(chat_id):
+        _notes.append(note)
+    return _notes
+
+
+async def get_note(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    if name in _notes:
+        return _notes[name]
+    return False
+
+
+async def save_note(chat_id: int, name: str, note: dict):
+    name = name.lower().strip()
+    _notes = await _get_notes(chat_id)
+    _notes[name] = note
+
+    await notesdb.update_one(
+        {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
+    )
+
+
+async def delete_note(chat_id: int, name: str) -> bool:
+    notesd = await _get_notes(chat_id)
+    name = name.lower().strip()
+    if name in notesd:
+        del notesd[name]
+        await notesdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"notes": notesd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+async def deleteall_notes(chat_id: int):
+    return await notesdb.delete_one({"chat_id": chat_id})
+
 
 
 # couple
