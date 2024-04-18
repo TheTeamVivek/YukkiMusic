e87b5a82873736d48ae1da9a14e7a6c99a8b2554
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-18 20:12:58 +0530

    Update clone.py

--- YukkiMusic/plugins/tools/clone.py
+++ YukkiMusic/plugins/tools/clone.py
@@ -1,4 +1,4 @@
-"""import re
+import re
 import logging
 from pymongo import MongoClient
 from pyrogram import Client, filters
@@ -121,4 +121,3 @@ async def restart_bots():
             await ai.start()
         except Exception as e:
             logging.exception(f"Error while restarting bot with token {bot_token}: {e}")
-"""
