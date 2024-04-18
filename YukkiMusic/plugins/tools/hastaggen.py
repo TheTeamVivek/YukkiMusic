2d76af4bc9f91010e57ca5b04f55cd53f6fca262
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-18 12:41:05 +0530

    Update tools

---
+++ YukkiMusic/plugins/tools/hastaggen.py
@@ -0,0 +1,24 @@
+import requests
+from YukkiMusic import app
+from pyrogram import filters
+from MukeshAPI import api
+
+@app.on_message(filters.command("hastag"))
+async def hastag(bot, message):
+
+    try:
+        text = message.text.split(' ',1)[1]
+        res = api.hashtag(text)
+        results = ' '.join(res)
+        hashtags = results.replace(",", "").replace("[", "").replace("]", "")
+
+    except IndexError:
+        return await message.reply_text("Example:\n\n/hastag python")
+
+
+    await message.reply_text(f"ʜᴇʀᴇ ɪs ʏᴏᴜʀ  ʜᴀsᴛᴀɢ :\n<pre>{hashtags}</pre>", quote=True)
+
+help= """
+Yᴏᴜ ᴄᴀɴ ᴜsᴇ ᴛʜɪs ʜᴀsʜᴛᴀɢ ɢᴇɴᴇʀᴀᴛᴏʀ ᴡʜɪᴄʜ ᴡɪʟʟ ɢɪᴠᴇ ʏᴏᴜ ᴛʜᴇ ᴛᴏᴘ 𝟹𝟶 ᴀɴᴅ ᴍᴏʀᴇ ʜᴀsʜᴛᴀɢs ʙᴀsᴇᴅ ᴏғғ ᴏғ ᴏɴᴇ ᴋᴇʏᴡᴏʀᴅ sᴇʟᴇᴄᴛɪᴏɴ.
+° /hastag enter word to generate hastag.
+°Exᴀᴍᴘʟᴇ:  /hastag python """
