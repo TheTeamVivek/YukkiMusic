00de5ad981d4223a06382c42c775e0fdc2d0014f
Vivek Kumar <167422483+vivekkumar07089@users.noreply.github.com>
2024-04-19 13:34:10 +0530

    Add files via upload

---
+++ YukkiMusic/cplugin/vcmute.py
@@ -0,0 +1,44 @@
+#
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import BANNED_USERS
+from YukkiMusic.core.call import Yukki
+from YukkiMusic.utils.database import is_muted, mute_on
+from YukkiMusic.utils.decorators import AdminRightsCheck
+
+
+@Client.on_message(filters.command(["vcmute"]) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def mute_stream(cli, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if await is_muted(chat_id):
+        return await message.reply_text(_["admin_5"], disable_web_page_preview=True)
+    await mute_on(chat_id)
+    await Yukki.mute_stream(chat_id)
+    await message.reply_text(
+        _["admin_6"].format(message.from_user.mention), disable_web_page_preview=True
+    )
+
+
+@Client.on_message(filters.command(["vcunmute"]) & filters.group & ~BANNED_USERS)
+@AdminRightsCheck
+async def unmute_stream(Client, message: Message, _, chat_id):
+    if not len(message.command) == 1 or message.reply_to_message:
+        return await message.reply_text(_["general_2"])
+    if not await is_muted(chat_id):
+        return await message.reply_text(_["admin_7"], disable_web_page_preview=True)
+    await mute_off(chat_id)
+    await Yukki.unmute_stream(chat_id)
+    await message.reply_text(
+        _["admin_8"].format(message.from_user.mention), disable_web_page_preview=True
+    )
