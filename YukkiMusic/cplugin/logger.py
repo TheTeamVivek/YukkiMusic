cd7b6d995a7150e498af8db5de3bee9cefad612b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-21 22:54:05 +0530

    Update cplugin

---
+++ YukkiMusic/cplugin/logger.py
@@ -0,0 +1,45 @@
+#
+# Copyright (C) 2024 - present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from pyrogram inport Client
+from YukkiMusic.utils.database import is_on_off
+from config import LOG_GROUP_ID
+
+
+async def play_logs(message, streamtype):
+    if await is_on_off(2):
+        if message.chat.username:
+            chatusername = f"@{message.chat.username}"
+        else:
+            chatusername = "ᴘʀɪᴠᴀᴛᴇ ɢʀᴏᴜᴘ"
+
+        logger_text = f"""
+<b>ᴘʟᴀʏ ʟᴏɢ</b>
+
+<b>ᴄʜᴀᴛ ɪᴅ :</b> <code>{message.chat.id}</code>
+<b>ᴄʜᴀᴛ ɴᴀᴍᴇ :</b> {message.chat.title}
+<b>ᴄʜᴀᴛ ᴜsᴇʀɴᴀᴍᴇ :</b> {chatusername}
+
+<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>
+<b>ɴᴀᴍᴇ :</b> {message.from_user.mention}
+<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}
+
+<b>ǫᴜᴇʀʏ :</b> {message.text.split(None, 1)[1]}
+<b>sᴛʀᴇᴀᴍᴛʏᴘᴇ :</b> {streamtype}"""
+        if message.chat.id != LOG_GROUP_ID:
+            try:
+                await client.send_message(
+                    chat_id=LOG_GROUP_ID,
+                    text=logger_text,
+                    disable_web_page_preview=True,
+                )
+            except Exception as e:
+                print(e)
+        return
