57bc86b82ac8a320f7af51afdee2286c77559f07
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-22 11:29:29 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -156,17 +156,16 @@ async def gen_thumb(videoid):
     except Exception:
         return YOUTUBE_IMG_URL
 
-
-async def gen_qthumb(videoid):
-    if os.path.isfile(f"cache/{videoid}.png"):
-        return f"cache/{videoid}.png"
+async def gen_qthumb(videoid, user_id):
+    if os.path.isfile(f"cache/{videoid}_{user_id}.png"):
+        return f"cache/{videoid}_{user_id}.png"
 
     url = f"https://www.youtube.com/watch?v={videoid}"
     try:
         results = VideosSearch(url, limit=1)
         for result in (await results.next())["result"]:
             try:
-                title = result["title"]
+                title = result["title"][:30]
                 title = re.sub("\W+", " ", title)
                 title = title.title()
             except:
@@ -174,12 +173,12 @@ async def gen_qthumb(videoid):
             try:
                 duration = result["duration"]
             except:
-                duration = "Unknown Mins"
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+                duration = "Unknown"
             try:
                 views = result["viewCount"]["short"]
             except:
-                views = "Unknown Views"
+                views = "Unknown" 
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
             try:
                 channel = result["channel"]["name"]
             except:
@@ -192,27 +191,45 @@ async def gen_qthumb(videoid):
                     await f.write(await resp.read())
                     await f.close()
 
+        try:
+            wxy = await app.download_media(
+                (await app.get_users(user_id)).photo.big_file_id,
+                file_name=f"{user_id}.jpg",
+            )
+        except:
+            wxy = await app.download_media(
+                (await app.get_users(app.id)).photo.big_file_id,
+                file_name=f"{app.id}.jpg",
+            )
+        xy = Image.open(wxy)
+        a = Image.new("L", [640, 640], 0)
+        b = ImageDraw.Draw(a)
+        b.pieslice([(0, 0), (640, 640)], 0, 360, fill=255, outline="white")
+        c = np.array(xy)
+        d = np.array(a)
+        e = np.dstack((c, d))
+        f = Image.fromarray(e)
+        x = f.resize((153, 153))
+
         youtube = Image.open(f"cache/thumb{videoid}.png")
-        image1 = changeImageSize(1280, 720, youtube)
+        zyoutube = Image.open(f"cache/thumb{videoid}.png")
+        bg = Image.open(f"assets/ZSTREAM_adobe_express.png")
+        image1 = youtube.resize((1280, 720))
         image2 = image1.convert("RGBA")
-        background = image2.filter(filter=ImageFilter.BoxBlur(30))
+        background = image2.filter(filter=ImageFilter.BoxBlur(25))
         enhancer = ImageEnhance.Brightness(background)
         background = enhancer.enhance(0.6)
-        Xcenter = youtube.width / 2
-        Ycenter = youtube.height / 2
-        x1 = Xcenter - 250
-        y1 = Ycenter - 250
-        x2 = Xcenter + 250
-        y2 = Ycenter + 250
-        logo = youtube.crop((x1, y1, x2, y2))
-        logo.thumbnail((520, 520), Image.LANCZOS)
-        logo = ImageOps.expand(logo, border=15, fill="white")
-        background.paste(logo, (50, 100))
-        draw = ImageDraw.Draw(background)
+        y = circle(zyoutube).resize((473, 473))
+        background.paste(y, (49, 125), mask=y)  # Adjusted placement of YouTube circle image
+        image3 = bg.resize((1280, 720))
+        image5 = image3.convert("RGBA")
+        result_img = Image.alpha_composite(background, image5)
+        result_img.paste(x, (353, 472), mask=x)  # Adjusted placement of user photo
+        # Adding text to the thumbnail
+        draw = ImageDraw.Draw(result_img)
         font = ImageFont.truetype("assets/font2.ttf", 40)
-        font2 = ImageFont.truetype("assets/font2.ttf", 75)
         arial = ImageFont.truetype("assets/font2.ttf", 30)
-        name_font = ImageFont.truetype("assets/font.ttf", 30)
+        font2 = ImageFont.truetype("assets/font2.ttf", 75)
         para = textwrap.wrap(title, width=32)
         j = 0
         draw.text((5, 5), f"{app.name}", fill="white", font=name_font)
@@ -264,11 +281,16 @@ async def gen_qthumb(videoid):
             (255, 255, 255),
             font=arial,
         )
+        
         try:
             os.remove(f"cache/thumb{videoid}.png")
         except:
             pass
-        background.save(f"cache/{videoid}.png")
-        return f"cache/{videoid}.png"
-    except Exception:
+
+        result_img.save(f"cache/{videoid}_{user_id}.png")
+
+        return f"cache/{videoid}_{user_id}.png"
+
+    except Exception as e:
+        print(e)
         return YOUTUBE_IMG_URL
