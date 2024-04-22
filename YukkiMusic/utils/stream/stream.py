a37ee086920b0591386b6aba8a4fb016980cd289
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-22 11:11:20 +0530

    Update stream.py

--- YukkiMusic/utils/stream/stream.py
+++ YukkiMusic/utils/stream/stream.py
@@ -169,7 +169,7 @@ async def stream(
                 "video" if video else "audio",
             )
             position = len(db.get(chat_id)) - 1
-            qimg = await gen_qthumb(vidid, user_id)
+            qimg = await gen_qthumb(vidid)
             button = queue_markup(_, vidid, chat_id)
             run = await app.send_photo(
                 original_chat_id,
