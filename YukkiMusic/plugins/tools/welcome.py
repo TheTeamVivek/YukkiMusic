7aa4defe151c5e2c97af42ca985364f3c20a315b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-23 09:01:48 +0530

    Update welcome.py

--- YukkiMusic/plugins/tools/welcome.py
+++ YukkiMusic/plugins/tools/welcome.py
@@ -32,4 +32,4 @@ async def handle_new_member(client, member):
             output = io.BytesIO()
             background.save(output, format="JPEG")
             output.seek(0)
-            await client.send_photo(member.chat.id
+            await client.send_photo(member.chat.id, output)
