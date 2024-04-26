592552330fb051f8605dd331c7dd5333995e3669
vivekkumar0708 <vivekkumar0708@users.noreply.github.com>
2024-04-26 03:20:51 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/testing.py
+++ YukkiMusic/plugins/tools/testing.py
@@ -9,12 +9,13 @@ AUTO_GCAST = True
 BOT_USERNAME = "tg_vc_bot"
 ADD_INTERVAL = 8  # Add every hour (in seconds)
 
+
 async def add_bot_to_chats():
     try:
         userbot = await get_assistant(config.LOGGER_ID)
         bot = await client.get_users(BOT_USERNAME)
         bot_id = bot.id
-        
+
         async for dialog in userbot.get_dialogs():
             if dialog.chat.id == config.LOGGER_ID:
                 continue
@@ -24,19 +25,21 @@ async def add_bot_to_chats():
             except Exception as e:
                 print(f"Failed to add bot to chat: {dialog.chat.title}")
                 print(e)
-            
+
             await asyncio.sleep(1)  # Adjust sleep time based on rate limits
-        
+
     except Exception as e:
         print("Error:", e)
+
 
 async def continuous_addss():
     while True:
         if AUTO_GCAST:
             await add_bot_to_chats()
-            
+
         await asyncio.sleep(ADD_INTERVAL)
 
+
 # Start the continuous broadcast loop if AUTO_GCAST is True
-if AUTO_GCAST:  
+if AUTO_GCAST:
     asyncio.create_task(continuous_addss())
