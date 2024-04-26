d76108dc267bd855b8bbb64917fecf02d09b334d
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 12:21:43 +0530

    Create admins.py

---
+++ YukkiMusic/cplugin/utils/admins.py
@@ -0,0 +1,71 @@
+from typing import Callable
+
+from pyrogram import Client
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import CallbackQuery, Message
+
+from YukkiMusic.misc import SUDOERS
+
+from .active import is_active_chat
+
+
+def admin_check(func: Callable) -> Callable:
+    async def non_admin(_, message: Message):
+        if not await is_active_chat(message.chat.id):
+            return await message.reply_text("ʙᴏᴛ ɪsɴ'ᴛ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.")
+
+        if message.from_user.id in SUDOERS:
+            return await func(_, message)
+
+        check = await client.get_chat_member(message.chat.id, message.from_user.id)
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await message.reply_text(
+                "» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
+            )
+
+        admin = (
+            await client.get_chat_member(message.chat.id, message.from_user.id)
+        ).privileges
+        if admin.can_manage_video_chats:
+            return await func(_, message)
+        else:
+            return await message.reply_text(
+                "» ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ᴍᴀɴᴀɢᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs."
+            )
+
+    return non_admin
+
+
+def admin_check_cb(func: Callable) -> Callable:
+    async def cb_non_admin(_, query: CallbackQuery):
+        if not await is_active_chat(query.message.chat.id):
+            return await query.answer(
+                "ʙᴏᴛ ɪsɴ'ᴛ sᴛʀᴇᴀᴍɪɴɢ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.", show_alert=True
+            )
+
+        if query.from_user.id in SUDOERS:
+            return await func(_, query)
+
+        try:
+            check = await client.get_chat_member(query.message.chat.id, query.from_user.id)
+        except:
+            return
+        if check.status not in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await query.answer(
+                "» ʏᴏᴜ'ʀᴇ ɴᴏᴛ ᴀɴ ᴀᴅᴍɪɴ ʙᴀʙʏ, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs.",
+                show_alert=True,
+            )
+
+        admin = (
+            await client.get_chat_member(query.message.chat.id, query.from_user.id)
+        ).privileges
+        if admin.can_manage_video_chats:
+            return await func(_, query)
+        else:
+            return await query.answer(
+                "» ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴘᴇʀᴍɪssɪᴏɴs ᴛᴏ ᴍᴀɴᴀɢᴇ ᴠɪᴅᴇᴏᴄʜᴀᴛs, ᴘʟᴇᴀsᴇ sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs.",
+                show_alert=True,
+            )
+
+    return cb_non_admin
+      
