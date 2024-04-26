0070f4c2bd9cdba2b8fe14b848000b219bd018c9
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 18:54:19 +0530

    Update utils

---
+++ YukkiMusic/cplugin/utils/inline.py
@@ -0,0 +1,36 @@
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from ..start import APP.USERNAME
+
+pm_buttons = [
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            url=f"https://t.me/{APP_USERNAME}?startgroup=true",
+        )
+    ],
+    [InlineKeyboardButton(text="ʜᴇʟᴩ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="clone_help")],
+    [
+        InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url=SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=SUPPORT_GROUP),
+    ],
+    [
+        InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID),
+    ],
+]
+
+
+gp_buttons = [
+    [
+        InlineKeyboardButton(
+            text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+            url=f"https://t.me/{APP_USERNAME}?startgroup=true",
+        )
+    ],
+    [
+        InlineKeyboardButton(text="❄ ᴄʜᴀɴɴᴇʟ ❄", url=SUPPORT_CHANNEL),
+        InlineKeyboardButton(text="✨ sᴜᴩᴩᴏʀᴛ ✨", url=SUPPORT_GROUP),
+    ],
+    [
+        InlineKeyboardButton(text="🥀 ᴅᴇᴠᴇʟᴏᴩᴇʀ 🥀", user_id=OWNER_ID),
+    ],
+]
