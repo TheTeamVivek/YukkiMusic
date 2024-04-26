bfb5dd3eb6452575f6421d61517e7e745602ad89
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 10:57:22 +0530

    Update cplugin

---
+++ YukkiMusic/cplugin/active.py
@@ -0,0 +1,39 @@
+
+active = []
+stream = {}
+
+
+async def is_active_chat(chat_id: int) -> bool:
+    if chat_id not in active:
+        return False
+    else:
+        return True
+
+
+async def add_active_chat(chat_id: int):
+    if chat_id not in active:
+        active.append(chat_id)
+
+
+async def remove_active_chat(chat_id: int):
+    if chat_id in active:
+        active.remove(chat_id)
+
+
+async def get_active_chats() -> list:
+    return active
+
+
+async def is_streaming(chat_id: int) -> bool:
+    run = stream.get(chat_id)
+    if not run:
+        return False
+    return run
+
+
+async def stream_on(chat_id: int):
+    stream[chat_id] = True
+
+
+async def stream_off(chat_id: int):
+    stream[chat_id] = False
