d5d1b87995eb328f191a08fca9414e499be0003e
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 12:12:15 +0530

    Update start.py

--- YukkiMusic/cplugin/start.py
+++ YukkiMusic/cplugin/start.py
@@ -22,7 +22,7 @@ START_TEXT = """
 
 @Client.on_message(filters.command(["start"]) & ~filters.forwarded)
 @Client.on_edited_message(filters.command(["start"]) & ~filters.forwarded)
-async def fallen_st(_, message: Message):
+async def clone_st(_, message: Message):
 	viv = await client.get_me()
     if message.chat.type == ChatType.PRIVATE:
         if len(message.text.split()) > 1:
