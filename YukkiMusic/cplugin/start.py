24fc452c08d617a5e0cb2089fa1d1002db72d8a5
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 18:53:27 +0530

    Update start.py

--- YukkiMusic/cplugin/start.py
+++ YukkiMusic/cplugin/start.py
@@ -4,7 +4,6 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 from config import *
 from YukkiMusic.utils.decorators.language import LanguageStart
-from .start import APP_USERNAME
 
 PM_START_TEXT = """
 ʜᴇʏ {0}, 🥀
