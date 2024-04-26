466d6cadab22118018535b91bb1af72ba7329879
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-26 15:06:55 +0530

    Update clone.py

--- YukkiMusic/plugins/tools/clone.py
+++ YukkiMusic/plugins/tools/clone.py
@@ -88,7 +88,7 @@ async def on_clone(client, message):
             except BaseException as e:
                 logging.exception("Error while cloning bot.")
                 await msg.edit_text(
-                    f"⚠️ <b>Bot Error:</b>\n\n<code>{e}</code>\n\n**Kindly forward this message to @vivek_zone to get assistance.**"
+                    f"⚠️ <b>Bot Error:</b>\n\n<code>{e}</code>\n\n**Kindly forward this message to @viv_zone to get assistance.**"
                 )
     except Exception as e:
         logging.exception("Error while handling message.")
