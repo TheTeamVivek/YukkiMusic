f2b6d220ce8a8eda29f381f1d85caceccbf81687
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-28 14:41:17 +0530

    Update cplugin

---
+++ YukkiMusic/cplugin/skip.py
@@ -0,0 +1,57 @@
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pytgcalls.types import MediaStream, AudioQuality
+
+from .play import BOT_USERNAME, pytgcalls
+from .utils import admin_check, buttons
+from .utils.active import _clear_
+from YukkiMusic.utils.thumbnails import gen_thumb
+from YukkiMusic.misc import clonedb
+
+
+@Client.on_message(filters.command(["skip", "next"]) & filters.group)
+@admin_check
+async def skip_str(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    get = clonedb.get(message.chat.id)
+    if not get:
+        try:
+            await _clear_(message.chat.id)
+            await pytgcalls.leave_group_call(message.chat.id)
+            await message.reply_text(
+                text=f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀\n\n**» ɴᴏ ᴍᴏʀᴇ ǫᴜᴇᴜᴇᴅ ᴛʀᴀᴄᴋs ɪɴ** {message.chat.title}, **ʟᴇᴀᴠɪɴɢ ᴠɪᴅᴇᴏᴄʜᴀᴛ.**",
+            )
+        except:
+            return
+    else:
+        title = get[0]["title"]
+        duration = get[0]["duration"]
+        file_path = get[0]["file_path"]
+        videoid = get[0]["videoid"]
+        req_by = get[0]["req"]
+        user_id = get[0]["user_id"]
+        get.pop(0)
+
+        stream = MediaStream(file_path, audio_parameters=AudioQuality.HIGH)
+        try:
+            await pytgcalls.change_stream(
+                message.chat.id,
+                stream,
+            )
+        except:
+            await _clear_(message.chat.id)
+            return await pytgcalls.leave_group_call(message.chat.id)
+
+        await message.reply_text(
+            text=f"➻ sᴛʀᴇᴀᴍ sᴋɪᴩᴩᴇᴅ 🥺\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        )
+        img = await gen_thumb(videoid, user_id)
+        return await message.reply_photo(
+            photo=img,
+            caption=f"**➻ sᴛᴀʀᴛᴇᴅ sᴛʀᴇᴀᴍɪɴɢ**\n\n‣ **ᴛɪᴛʟᴇ :** [{title[:27]}](https://t.me/{BOT_USERNAME}?start=info_{videoid})\n‣ **ᴅᴜʀᴀᴛɪᴏɴ :** `{duration}` ᴍɪɴᴜᴛᴇs\n‣ **ʀᴇǫᴜᴇsᴛᴇᴅ ʙʏ :** {req_by}",
+            reply_markup=buttons,
+        )
