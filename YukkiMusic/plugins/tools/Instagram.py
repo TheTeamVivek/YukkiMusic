52e6ae0125e84fc8305033c223bb80520bda881d
vivekkumar0708 <vivekkumar0708@users.noreply.github.com>
2024-04-28 06:05:18 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/Instagram.py
+++ YukkiMusic/plugins/tools/Instagram.py
@@ -4,18 +4,23 @@ from pyrogram import filters
 import requests
 from YukkiMusic import app
 
+
 @app.on_message(filters.command("im", prefixes="/"))
 async def download_instagram_video(client, message):
     if len(message.command) < 3:
-        await message.reply_text("Please provide the Instagram video URL after the command.")
+        await message.reply_text(
+            "Please provide the Instagram video URL after the command."
+        )
         return
-    
+
     url = message.command[1]
-    api_url = f"https://nodejs-1xn1lcfy3-jobians.vercel.app/v2/downloader/instagram?url={url}"
-    
+    api_url = (
+        f"https://nodejs-1xn1lcfy3-jobians.vercel.app/v2/downloader/instagram?url={url}"
+    )
+
     response = requests.get(api_url)
     data = response.json()
-    
+
     if data["status"]:
         video_url = data["data"][0]["url"]
         await client.send_video(message.chat.id, video_url)
