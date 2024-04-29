a448ce595d07e9ce1dbed2058ca8e69bd5dd944f
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-29 14:07:11 +0530

    Update pause.py

--- YukkiMusic/cplugin/pause.py
+++ YukkiMusic/cplugin/pause.py
@@ -55,11 +55,9 @@ async def pause_str(client, message: Message):
             return await message.reply_text(
                 "ᴅɪᴅ ʏᴏᴜ ʀᴇᴍᴇᴍʙᴇʀ ᴛʜᴀᴛ ʏᴏᴜ ᴘᴀᴜsᴇᴅ ᴛʜᴇ sᴛʀᴇᴀᴍ ?"
             )
-            try:
-                await stream_on(message.chat.id)
-                await pytgcalls.resume_stream(message.chat.id)
-                return await message.reply_text(
-                    text=f"➻ sᴛʀᴇᴀᴍ ʀᴇsᴜᴍᴇᴅ 💫\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
-                )
-            except Exception as e:
-                logging.exception(e)
+        await stream_on(message.chat.id)
+        await pytgcalls.resume_stream(message.chat.id)
+        return await message.reply_text(
+            text=f"➻ sᴛʀᴇᴀᴍ ʀᴇsᴜᴍᴇᴅ 💫\n│ \n└ʙʏ : {message.from_user.mention} 🥀",
+        )
+
