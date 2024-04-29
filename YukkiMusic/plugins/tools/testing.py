8cb5a03995d2bc807e8aa4339e27fd386359a14c
vivekkumar0708 <vivekkumar0708@users.noreply.github.com>
2024-04-29 12:10:50 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/testing.py
+++ YukkiMusic/plugins/tools/testing.py
@@ -3,7 +3,12 @@ from pyrogram.types import Message
 from YukkiMusic import app
 
 
-@app.on_message(filters.command("nak") )
+@app.on_message(filters.command("nak"))
 async def clean(_, message):
-	a = await app.ask(chat_id=message.chat.id, text="your name??", user_id=message.from_user.id, timeout=10)
-	await message.reply_text(f"your name is {a}")
+    a = await app.ask(
+        chat_id=message.chat.id,
+        text="your name??",
+        user_id=message.from_user.id,
+        timeout=10,
+    )
+    await message.reply_text(f"your name is {a}")
