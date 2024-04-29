29cb08ed28135536bca96e06e8b3a27b3dd0d4d9
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-29 20:50:11 +0530

    Update history.py

--- YukkiMusic/plugins/tools/history.py
+++ YukkiMusic/plugins/tools/history.py
@@ -58,3 +58,46 @@ async def sg(client: Client, message: Message):
         pass
 
     await lol.delete()
+
+
+
+@app.on_message(filters.command(["truecaller"]))
+async def sg(client: Client, message: Message):
+    if len(message.text.split()) < 2 and not message.reply_to_message:
+        return await message.reply("sg username/id/reply")
+    if message.reply_to_message and message.reply_to_message.text:
+        input = message.reply_to_message.text
+    else:
+        input = " ".join(message.command[1:])
+
+    if not re.match(r'^\+[1-9]\d{1,14}$', args):
+        return await message.reply_text("Please provide a valid phone number including country code.")
+        
+    lol = await message.reply("<code>Processing...</code>")
+    tr = ["@TrueCaller_Z_Bot"]
+    cli = random.choice(assistants)
+    ubot = await get_client(cli)
+
+    try:
+        a = await ubot.send_message(tr, f"{input}")
+        await a.delete()
+    except Exception as e:
+        return await lol.edit(str(e))
+    await asyncio.sleep(1)
+
+    async for stalk in ubot.search_messages(a.chat.id):
+        if stalk.text is None:
+            continue
+        if not stalk:
+            await message.reply("The bot encountered an issue. Please try again later")
+        elif stalk:
+            await message.reply(f"{stalk.text}")
+            break
+
+    try:
+        user_info = await ubot.resolve_peer(sg)
+        await ubot.send(DeleteHistory(peer=user_info, max_id=0, revoke=True))
+    except Exception as e:
+        logging.exception(e)
+
+    await lol.delete()
