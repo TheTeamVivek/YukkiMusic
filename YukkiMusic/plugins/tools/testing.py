150276257dfef65625d6b839f0878e1855e4698b
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-29 17:40:24 +0530

    Update testing.py

--- YukkiMusic/plugins/tools/testing.py
+++ YukkiMusic/plugins/tools/testing.py
@@ -1,39 +1,9 @@
-"""import asyncio
-import datetime
-from YukkiMusic.utils.database import get_assistant
-import config
+from pyrogram import filters
+from pyrogram.types import Message
 from YukkiMusic import app
 
-AUTO_GCAST = True
-BOT_USERNAME = "tg_vc_bot"
-ADD_INTERVAL = 8
 
-
-async def add_bot_to_chats():
-    try:
-        userbot = await get_assistant(config.LOG_GROUP_ID)
-        bot = await userbot.get_users(BOT_USERNAME)
-        async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == config.LOG_GROUP_ID:
-                continue
-            try:
-                await userbot.add_chat_members(dialog.chat.id, bot.id)
-                print(f"Added bot to chat: {dialog.chat.title}")
-            except Exception as e:
-                print(f"Failed to add bot to chat: {dialog.chat.title}\nException {e}")
-
-            await asyncio.sleep(1)
-    except Exception as e:
-        print("Error:", e)
-
-
-async def continuous_addss():
-    while True:
-        if AUTO_GCAST:
-            await add_bot_to_chats()
-
-        await asyncio.sleep(ADD_INTERVAL)
-
-
-if AUTO_GCAST:
-    asyncio.create_task(continuous_addss())"""
+@app.on_message(filters.command("nak") )
+async def clean(_, message):
+	a = await app.ask(chat_id=message.chat.id, text="your name??", user_id=message.from_user.id, timeout=10)
+	await message.reply_text(f"your name is {a}")
