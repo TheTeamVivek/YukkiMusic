90e2ba04b878231ca7cc828a7ea5942b873e8915
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-29 07:30:53 +0530

    Update watcher.py

--- YukkiMusic/cplugin/watcher.py
+++ YukkiMusic/cplugin/watcher.py
@@ -10,19 +10,19 @@ async def welcome(client: Client, message: Message):
     i = await client.get_me()
     if chat_type != ChatType.SUPERGROUP:
         await message.reply_text("ᴘʟᴇᴀsᴇ ᴄᴏɴᴠᴇʀᴛ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴀ sᴜᴘᴇʀɢʀᴏᴜᴘ ᴏʀ ᴍᴀᴋᴇ ʏᴏᴜʀ ɢʀᴏᴜᴘ ʜɪsᴛᴏʀʏ ᴠɪɪsɪʙʟᴇ sᴏ ɪ ᴡᴏʀᴋ ᴘᴇʀғᴇᴄᴛʟʏ")
-        return await client.leave_chat(message.chat.id)
+         return await client.leave_chat(message.chat.id)
     a = await client.get_chat_member(message.chat.id, i.id)
     if a.status != ChatMemberStatus.ADMINISTRATOR:
     	await message.reply_text("ᴘʟᴇᴀsᴇ ᴍᴀᴋᴇ ᴍᴇ ᴀɴ ᴀᴅᴍɪɴ ᴡɪᴛʜ **ɪɴᴠɪᴛᴇ ᴜsᴇʀ** ᴘᴇʀᴍɪssɪᴏɴ ᴛᴏ ᴘʟᴀʏ ᴍᴜsɪᴄ")
         return await client.leave_chat(message.chat.id)
     try:
-    	b = await client.get_chat_member(message.chat.id, BOTID)
+        b = await client.get_chat_member(message.chat.id, BOTID)
         if (
             b.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER]
         ):
         	await message.reply_text("sᴏʀʀʏ! ᴍʏ ᴍᴀsᴛᴇʀ ʙᴏᴛ ɪs ᴀʟʀᴇᴀᴅʏ ʜᴇʀᴇ sᴏ ɪ ᴀᴍ ʟᴇᴀᴠᴇɪɴɢ")
-            return await client.leave_chat(message.chat.id)
+        return await client.leave_chat(message.chat.id)
     except Exception:
-    	pass
+        pass
         
     
