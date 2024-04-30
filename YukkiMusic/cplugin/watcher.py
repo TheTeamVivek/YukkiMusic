f1e8b4551b51364a8b8c2d97028bb88df51644d5
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-30 17:44:53 +0530

    Update watcher.py

--- YukkiMusic/cplugin/watcher.py
+++ YukkiMusic/cplugin/watcher.py
@@ -15,8 +15,7 @@ close = 30
 @Client.on_message(filters.video_chat_ended, group=close)
 async def welcome(client: Client, message: Message):
     try:
-        i = await client.get_me()
-        await _clear_(message.chat.id, i.id)
+        await _clear_(message.chat.id)
         await pytgcalls.leave_group_call(message.chat.id)
     except Exception as e:
         logging.exception(e)
