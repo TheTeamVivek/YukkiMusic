a11ab0acec444b9c5b242a5d49c3514ee5fb2d59
vivekkumar0708 <vivekkumar0708@users.noreply.github.com>
2024-04-30 07:04:24 +0000

    Auto fixes

--- YukkiMusic/cplugin/utils/active.py
+++ YukkiMusic/cplugin/utils/active.py
@@ -8,9 +8,11 @@ async def add_active_chat(chat_id: int, bot_id: int):
     if chat_id not in active:
         active.append((chat_id, bot_id))
 
+
 async def remove_active_chat(chat_id: int, bot_id: int):
     if (chat_id, bot_id) in active:
         active.remove((chat_id, bot_id))
+
 
 async def is_active_chat(chat_id: int, bot_id: int) -> bool:
     if (chat_id, bot_id) not in active:
@@ -18,17 +20,21 @@ async def is_active_chat(chat_id: int, bot_id: int) -> bool:
     else:
         return True
 
+
 async def is_streaming(chat_id: int, bot_id: int) -> bool:
     run = stream.get((chat_id, bot_id))
     if not run:
         return False
     return run
 
+
 async def stream_on(chat_id: int, bot_id: int):
     stream[(chat_id, bot_id)] = True
 
+
 async def stream_off(chat_id: int, bot_id: int):
     stream[(chat_id, bot_id)] = False
+
 
 async def _clear_(chat_id, bot_id):
     try:
