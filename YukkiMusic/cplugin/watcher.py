79d6e8472dc4371cfd3680d0152fb68b045cf296
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-30 17:54:22 +0530

    Update watcher.py

--- YukkiMusic/cplugin/watcher.py
+++ YukkiMusic/cplugin/watcher.py
@@ -4,6 +4,7 @@ from pyrogram.types import Message
 from pyrogram.enums import ChatMemberStatus, ChatType
 from YukkiMusic import app
 from YukkiMusic.plugins.tools.clone import CLONES
+frim .utils.active import _clear_
 from .play import pytgcalls
 
 BOT_ID = app.id
