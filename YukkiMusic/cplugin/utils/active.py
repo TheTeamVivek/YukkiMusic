4de992972a7ed374683628f17408720635165206
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-04-30 12:33:55 +0530

    Update active.py

--- YukkiMusic/cplugin/utils/active.py
+++ YukkiMusic/cplugin/utils/active.py
@@ -4,52 +4,35 @@ active = []
 stream = {}
 
 
-async def is_active_chat(chat_id: int) -> bool:
+async def add_active_chat(chat_id: int, bot_id: int):
     if chat_id not in active:
+        active.append((chat_id, bot_id))
+
+async def remove_active_chat(chat_id: int, bot_id: int):
+    if (chat_id, bot_id) in active:
+        active.remove((chat_id, bot_id))
+
+async def is_active_chat(chat_id: int, bot_id: int) -> bool:
+    if (chat_id, bot_id) not in active:
         return False
     else:
         return True
 
-
-async def add_active_chat(chat_id: int):
-    if chat_id not in active:
-        active.append(chat_id)
-
-
-async def remove_active_chat(chat_id: int):
-    if chat_id in active:
-        active.remove(chat_id)
-
-
-async def get_active_chats() -> list:
-    return active
-
-
-async def is_streaming(chat_id: int) -> bool:
-    run = stream.get(chat_id)
+async def is_streaming(chat_id: int, bot_id: int) -> bool:
+    run = stream.get((chat_id, bot_id))
     if not run:
         return False
     return run
 
+async def stream_on(chat_id: int, bot_id: int):
+    stream[(chat_id, bot_id)] = True
 
-async def iss_streaming(chat_id: int) -> bool:
-    run = stream.get(chat_id)
-    if not run:
-        return False
-    return True
+async def stream_off(chat_id: int, bot_id: int):
+    stream[(chat_id, bot_id)] = False
 
-
-async def stream_on(chat_id: int):
-    stream[chat_id] = True
-
-
-async def stream_off(chat_id: int):
-    stream[chat_id] = False
-
-
-async def _clear_(chat_id):
+async def _clear_(chat_id, bot_id):
     try:
-        clonedb[chat_id] = []
-        await remove_active_chat(chat_id)
+        clonedb[(chat_id, bot_id)] = []
+        await remove_active_chat(chat_id, bot_id)
     except:
         return
