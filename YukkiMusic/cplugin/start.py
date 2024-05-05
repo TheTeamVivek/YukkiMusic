3a998a24881a79191d22144cb8abd0dd85f1a6b2
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-05 07:10:12 +0530

    Update start.py

--- YukkiMusic/cplugin/start.py
+++ YukkiMusic/cplugin/start.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2024-present by TeamYukki@Github, < https://github.com/TeamYukki >.
+#
+# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
+#
+# All rights reserved.
+#
 from pyrogram import Client, filters
 from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
