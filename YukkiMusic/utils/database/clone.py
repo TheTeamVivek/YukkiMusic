75cbcbd6e0d5efefc33de1a19fb62723a8ed3af6
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-10 09:18:31 +0000

    Auto fixes

--- YukkiMusic/utils/database/clone.py
+++ YukkiMusic/utils/database/clone.py
@@ -6,8 +6,10 @@ cloneownerdb = mongodb.cloneownerdb
 clonebotdb = mongodb.clonebotdb
 clonebotnamedb = mongodb.clonebotnamedb
 
+
 async def save_clonebot_owner(bot_id, user_id):
     await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
 
 async def get_clonebot_owner(bot_id):
     result = await cloneownerdb.find_one({"bot_id": bot_id})
