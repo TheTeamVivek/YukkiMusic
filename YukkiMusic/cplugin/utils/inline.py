6cc498b71fc2d7f38ca305322be00277dbdf0ffa
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-10 19:07:35 +0530

    Update inline.py

--- YukkiMusic/cplugin/utils/inline.py
+++ YukkiMusic/cplugin/utils/inline.py
@@ -1,5 +1,20 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
+
+helpmenu = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="ᴘʟᴀʏ", callback_data="clone_cb play")
+InlineKeyboardButton(text="ᴛᴇʟᴇɢʀᴀᴘʜ", callback_data="clone_cb telegraph")],
+                [
+                    InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data="clone_home"),
+                    InlineKeyboardButton(text="ᴄʟᴏsᴇ", callback_data="close"),
+                ],
+            ],
+        )
+
+
+
+
 buttons = InlineKeyboardMarkup(
     [
         [
