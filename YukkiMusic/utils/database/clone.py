6379bd6560e97893fdf2fd1f40c1f81af0e53450
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-10 14:48:15 +0530

    Update and rename pmpermit.py to clone.py

---
+++ YukkiMusic/utils/database/clone.py
@@ -0,0 +1,17 @@
+from YukkiMusic.core.mongo import mongodb, pymongodb
+from typing import Dict, List, Union
+from config import OWNER_ID
+
+cloneownerdb = mongodb.cloneownerdb
+clonebotdb = mongodb.clonebotdb
+clonebotnamedb = mongodb.clonebotnamedb
+
+async def save_clonebot_owner(bot_id, user_id):
+    await cloneownerdb.insert_one({"bot_id": bot_id, "user_id": user_id})
+
+async def get_clonebot_owner(bot_id):
+    result = await cloneownerdb.find_one({"bot_id": bot_id})
+    if result:
+        return result.get("user_id")
+    else:
+        return OWNER_ID
