f9747db502325ae4cb8e6134720d9e0c5443747d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-10 15:11:29 +0530

    Update clone.py

--- YukkiMusic/plugins/tools/clone.py
+++ YukkiMusic/plugins/tools/clone.py
@@ -137,3 +137,29 @@ async def delete_all_cloned_bots(client, message):
     except Exception as e:
         await message.reply_text("An error occurred while deleting all cloned bots.")
         logging.exception(e)
+
+async def restart_bots():
+    global CLONES
+    logging.info("Restarting all bots........")
+    cursor = clonebotdb.find()
+    async for bot in cursor:
+        bot_token = bot["token"]
+        try:
+            ai = Client(
+                f"{bot_token}",
+                API_ID,
+                API_HASH,
+                bot_token=bot_token,
+                plugins=dict(root="YukkiMusic.cplugin"),
+            )
+            await ai.start()
+            bot = await ai.get_me()
+            if bot.id not in CLONES:
+                try:
+                    CLONES.add(bot.id)
+                except Exception:
+                    pass
+        except (AccessTokenExpired, AccessTokenInvalid):
+            clonebotdb.delete_one({"token": bot_token})
+        except Exception as e:
+            logging.exception(f"Error while restarting bot with token {bot_token}: {e}")
