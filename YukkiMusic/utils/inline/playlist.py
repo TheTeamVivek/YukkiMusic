5a96445680cd77738b590f952aa14c5fd26a8919
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-11 20:12:36 +0530

    Update playlist.py

--- YukkiMusic/utils/inline/playlist.py
+++ YukkiMusic/utils/inline/playlist.py
@@ -17,11 +17,13 @@ def botplaylist_markup(_):
             InlineKeyboardButton(
                 text=_["PL_B_1"],
                 callback_data="get_playlist_playmode",
-            ),
+            ),],
+            [
             InlineKeyboardButton(text=_["PL_B_8"], callback_data="get_top_playlists"),
         ],
         [
-            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),
+            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),],
+        [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
