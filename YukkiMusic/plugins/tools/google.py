384e32fa78b24d04f5949c1462eef17cfa9ad836
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-13 19:08:45 +0530

    Update google.py

--- YukkiMusic/plugins/tools/google.py
+++ YukkiMusic/plugins/tools/google.py
@@ -1,3 +1,4 @@
+import logging
 from pyrogram import filters
 from pyrogram.enums import ChatAction
 from googlesearch import search
@@ -5,7 +6,7 @@ from YukkiMusic import app
 
 
 @app.on_message(
-    filters.command(["shv"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"])
+    filters.command(["google"])
 )
 async def google(bot, message):
     if len(message.command) < 2 and not message.reply_to_message:
@@ -16,12 +17,16 @@ async def google(bot, message):
         user_input = message.reply_to_message.text
     else:
         user_input = " ".join(message.command[1:])
-
-    a = search(user_input, advanced=True)
-    txt = f"Search Query: {user_input}\n\nresults"
-    for result in a:
-        txt += f"\n\n[❍ {result.title}]({result.url})\n<b>{result.description}</b>"
-    await message.reply_text(
-        txt,
-        disable_web_page_preview=True,
-    )
+    b = await message.reply_text("**Sᴇᴀʀᴄʜɪɴɢ ᴏɴ Gᴏᴏɢʟᴇ....**")
+    try:
+        a = search(user_input, advanced=True)
+        txt = f"Search Query: {user_input}\n\nresults"
+        for result in a:
+            txt += f"\n\n[❍ {result.title}]({result.url})\n<b>{result.description}</b>"
+        await b.edit(
+            txt,
+            disable_web_page_preview=True,
+        )
+    except Exception as e:
+        await b.edit(e)
+        logging.exception(e)
