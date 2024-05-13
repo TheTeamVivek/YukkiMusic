894229870609844f85412142aa304b745f19a0a2
VivekKumar-IN <VivekKumar-IN@users.noreply.github.com>
2024-05-13 01:24:41 +0000

    Auto fixes

--- YukkiMusic/plugins/tools/vcuser.py
+++ YukkiMusic/plugins/tools/vcuser.py
@@ -3,10 +3,11 @@ from pyrogram import filters
 from YukkiMusic.core.call import Yukki
 from YukkiMusic import app
 
+
 @app.on_message(filters.command("vcuser"))
 async def get_vc_users(client, message):
     try:
-        AB = await Yukki.get_participant( message.chat.id)
+        AB = await Yukki.get_participant(message.chat.id)
     except GroupCallNotFound:
         return await message.reply_text("Assisitant iss not in vc")
     users_info = ""
@@ -14,5 +15,5 @@ async def get_vc_users(client, message):
         user_id = participant.user_id
         user = await app.get_users(user_id)
         users_info += f"[{user.first_name}](tg://user?id={user_id})\n"
-    
+
     await message.reply(users_info)
