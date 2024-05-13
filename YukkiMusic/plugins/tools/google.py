8ccff02488d082ab7f96187ba468d2813edc3453
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-13 19:02:21 +0530

    Update google.py

--- YukkiMusic/plugins/tools/google.py
+++ YukkiMusic/plugins/tools/google.py
@@ -1,63 +1,28 @@
-import urllib
-import re
-import urllib.request
-import random
-
-from search_engine_parser import GoogleSearch
-
-from YukkiMusic import telethn
-from YukkiMusic.utils.events import register
-
-opener = urllib.request.build_opener()
-
-user_agents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36 Edg/99.0.999.99",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 YaBrowser/21.3.3.306 Yowser/2.5 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7; rv:99.0) Gecko/20100101 Firefox/99.0",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36 OPR/99.0.9999.99",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Safari/537.36 Edge/99.0.999.99",
-    "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
-    "Mozilla/5.0 (Linux; Android 10; SM-G960U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.9999.99 Mobile Safari/537.36",
-    "Mozilla/5.0 (iPad; CPU OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1",
-]
-opener.addheaders = [("User-agent", random.choice(user_agents))]
+from pyrogram import filters
+from pyrogram.enums import ChatAction
+from googlesearch import search
+from YukkiMusic import app
 
 
-@register(pattern="^/google (.*)")
-async def _(event):
-    if event.fwd_from:
+@app.on_message(
+    filters.command(
+        ["shv"], prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]
+    )
+)
+async def google(bot, message):
+    if len(message.command) < 2 and not message.reply_to_message:
+        await message.reply_text(
+            "Example:\n\n`/google lord ram`"
+        )
         return
 
-    webevent = await event.reply("Searching...")
-    match = event.pattern_match.group(1)
-    page = re.findall(r"page=\d+", match)
-    try:
-        page = page[0]
-        page = page.replace("page=", "")
-        match = match.replace("page=" + page[0], "")
-    except IndexError:
-        page = 1
-    search_args = (str(match), int(page))
-    gsearch = GoogleSearch()
-    gresults = await gsearch.async_search(*search_args)
-    msg = ""
-    for i in range(len(gresults["links"])):
-        try:
-            title = gresults["titles"][i]
-            link = gresults["links"][i]
-            desc = gresults["descriptions"][i]
-            msg += f"❍[{title}]({link})\n**{desc}**\n\n"
-        except IndexError:
-            break
-    await webevent.edit(
-        "**Search Query:**\n`" + match + "`\n\n**Results:**\n" + msg, link_preview=False
-    )
+    if message.reply_to_message and message.reply_to_message.text:
+        user_input = message.reply_to_message.text
+    else:
+        user_input = " ".join(message.command[1:])
 
-
-__help__ = """
- ❍ /google <text>*:* Perform a google search
-
-"""
+    a = search(user_input, advanced=True)
+    txt = f"Search Query: {user_input}\n\nresults"
+    for result in a:
+        txt += f"\n\n[❍ {result.title}]({result.url})\n<b>{result.description}</b>"
+    await message.reply_text(txt, disable_web_page_preview=True,)
