5d7cc1fd1aea8ddea9903ce50e014dee12c99284
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-29 12:32:03 +0530

    Update voicechat.py

--- YukkiMusic/plugins/tools/voicechat.py
+++ YukkiMusic/plugins/tools/voicechat.py
@@ -59,6 +59,8 @@ async def endvc(client, message: Message):
             "Give me Manage vc power To My Assistant instead to use this Command"
         )
     except Exception as e:
+        if "'NoneType' object has no attribute 'write'" in str(e):
+            await hell.edit_text("vc is already off baby")
         logging.exception(e)
         await hell.edit_text(e)
 
