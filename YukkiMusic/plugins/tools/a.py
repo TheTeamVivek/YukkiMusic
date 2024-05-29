082b7d9561947a3643d98f86bd2a5888003ae1a9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-29 05:47:28 +0530

    Update a.py

--- YukkiMusic/plugins/tools/a.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import filters
-
-from YukkiMusic import userbot
-
-
-@userbot.one.on_message(filters.text)
-def handle_message(client, message):
-    message.reply_text("Hello, world!")
