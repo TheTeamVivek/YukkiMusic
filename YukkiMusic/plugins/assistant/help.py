ad146917ef35121f25108d93a22c07ceba6decde
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 22:35:53 +0530

    Update help.py

--- YukkiMusic/plugins/assistant/help.py
+++ YukkiMusic/plugins/assistant/help.py
@@ -1,12 +1,12 @@
-from pyrogram import filters
+from pyrogram import Client, filters
 
 from YukkiMusic import app
 
 
-@app.on_message(filters.command(["help"], prefixes=["."]))
-async def inline_help_menu(client, message):
-    bot_results = await app.get_inline_bot_results(f"@{app.username}", "help_menu")
-    await app.send_inline_bot_result(
+@Client.on_message(filters.command(["help"], prefixes=["."]))
+async def inline_help_menu(client: Client, message):
+    bot_results = await client.get_inline_bot_results(f"@{app.username}", "help_menu")
+    await client.send_inline_bot_result(
         chat_id=message.chat.id,
         query_id=bot_results.query_id,
         result_id=bot_results.results[0].id,
