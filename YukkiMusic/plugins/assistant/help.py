9d49df692c2dc96671d462d3467936f6d3b39c01
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 22:28:17 +0530

    Update help.py

--- YukkiMusic/plugins/assistant/help.py
+++ YukkiMusic/plugins/assistant/help.py
@@ -3,14 +3,14 @@ from YukkiMusic import app
 
 @app.on_message(filters.command(["help"],prefixes=["."]))
 async def inline_help_menu(client, message):
-        bot_results = await app.get_inline_bot_results(
+    bot_results = await app.get_inline_bot_results(
             f"@{app.username}", "help_menu"
-        )
-        await app.send_inline_bot_result(
-            chat_id=message.chat.id,
-            query_id=bot_results.query_id,
-            result_id=bot_results.results[0].id,
-        )
+    )
+    await app.send_inline_bot_result(
+        chat_id=message.chat.id,
+        query_id=bot_results.query_id,
+        result_id=bot_results.results[0].id,
+    )
     try:
         await message.delete()
     except:
