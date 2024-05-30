ed724aaf251a283c8ec4066c5de8e9b38886503b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 11:44:19 +0530

    Update seeker.py

--- YukkiMusic/plugins/misc/seeker.py
+++ YukkiMusic/plugins/misc/seeker.py
@@ -99,3 +99,74 @@ async def markup_timer():
 
 
 asyncio.create_task(markup_timer())
+
+
+
+
+import datetime
+from YukkiMusic import app
+from YukkiMusic.utils.database import get_served_users
+from config import LOGGER_ID
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+if app.username == "YukkiMusic_vkBot"
+    AUTO_GCASTS = True
+    AUTO_GCAST = True
+else:
+    AUTO_GCASTS = False
+    AUTO_GCAST = False
+START_IMG_URLS = "https://graph.org/file/497d715b03115857db6d8.jpg"
+
+MESSAGES = f"""‣  тнιѕ ιѕ {app.mention}
+
+➜ α мυѕιᴄ ρℓαуєʀ вσт ωιтн ѕσмє α∂ναиᴄє∂ fєαтυʀєѕ."""
+
+
+BUTTONS = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton("𝙰𝚍𝚍 𝙼𝚎", url=f"https://t.me/YukkiSongBot?startgroup=true")
+        ]
+    ]
+)
+
+
+
+TEXT = """ᴀᴜᴛᴏ ɢᴄᴀsᴛ ɪs ᴇɴᴀʙʟᴇᴅ sᴏ ᴀᴜᴛᴏ ɢᴄᴀsᴛ/ʙʀᴏᴀᴅᴄᴀsᴛ ɪs ᴅᴏɪɴ ɪɴ ᴀʟʟ ᴄʜᴀᴛs ᴄᴏɴᴛɪɴᴜᴏᴜsʟʏ. \nɪᴛ ᴄᴀɴ ʙᴇ sᴛᴏᴘᴘᴇᴅ ʙʏ ᴘᴜᴛ ᴠᴀʀɪᴀʙʟᴇ [ᴀᴜᴛᴏ_ɢᴄᴀsᴛ = (ᴋᴇᴇᴘ ʙʟᴀɴᴋ & ᴅᴏɴᴛ ᴡʀɪᴛᴇ ᴀɴʏᴛʜɪɴɢ)]"""
+
+async def send_text_once():
+    try:
+        await app.send_message(LOGGER_ID, TEXT)
+    except Exception as e:
+        pass
+
+async def send_message_to_chats():
+    try:
+        chats = await get_served_users()
+
+        for chat_info in chats:
+            chat_id = chat_info.get('chat_id')
+            if isinstance(chat_id, int):  # Check if chat_id is an integer
+                try:
+                    await app.send_photo(chat_id, photo=START_IMG_URLS, caption=MESSAGES, reply_markup=BUTTONS)
+                    await asyncio.sleep(20)  # Sleep for 100 second between sending messages
+                except Exception as e:
+                    pass  # Do nothing if an error occurs while sending message
+    except Exception as e:
+        pass  # Do nothing if an error occurs while fetching served chats
+
+async def continuous_broadcast():
+    await send_text_once()  # Send TEXT once when bot starts
+
+    while True:
+        if AUTO_GCAST:
+            try:
+                await send_message_to_chats()
+            except Exception as e:
+                pass
+
+        # Wait for 100000 seconds before next broadcast
+        await asyncio.sleep(100000)
+
+# Start the continuous broadcast loop if AUTO_GCAST is True
+if AUTO_GCAST:  
+    asyncio.create_task(continuous_broadcast())
