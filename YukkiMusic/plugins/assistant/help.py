1f1883947d84b0f3abcd82f0a1f30b7a89115ab3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 22:52:36 +0530

    Update help.py

--- YukkiMusic/plugins/assistant/help.py
+++ YukkiMusic/plugins/assistant/help.py
@@ -1,17 +1,20 @@
 from pyrogram import Client, filters
 
 from YukkiMusic import app
-
+import logging
 
 @Client.on_message(filters.command(["help"], prefixes=["."]))
 async def inline_help_menu(client: Client, message):
-    bot_results = await client.get_inline_bot_results(f"@{app.username}", "help_menu")
-    await client.send_inline_bot_result(
-        chat_id=message.chat.id,
-        query_id=bot_results.query_id,
-        result_id=bot_results.results[0].id,
-    )
     try:
-        await message.delete()
-    except:
-        pass
+        bot_results = await client.get_inline_bot_results(f"@{app.username}", "help_menu")
+        await client.send_inline_bot_result(
+            chat_id=message.chat.id,
+                query_id=bot_results.query_id,
+            result_id=bot_results.results[0].id,
+        )
+        try:
+            await message.delete()
+        except:
+            pass
+    except Exception as e:
+        logging.exception(e)
