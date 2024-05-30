27fb55961b3599e1df2ffb76b58f65d1dec29362
Vivek Kumar <VivekKumar-IN@users.noreply.github.com>
2024-05-30 17:22:55 +0000

    pylint: auto fixes

--- YukkiMusic/plugins/assistant/help.py
+++ YukkiMusic/plugins/assistant/help.py
@@ -1,15 +1,19 @@
+import logging
+
 from pyrogram import Client, filters
 
 from YukkiMusic import app
-import logging
+
 
 @Client.on_message(filters.command(["help"], prefixes=["."]))
 async def inline_help_menu(client: Client, message):
     try:
-        bot_results = await client.get_inline_bot_results(f"@{app.username}", "help_menu")
+        bot_results = await client.get_inline_bot_results(
+            f"@{app.username}", "help_menu"
+        )
         await client.send_inline_bot_result(
             chat_id=message.chat.id,
-                query_id=bot_results.query_id,
+            query_id=bot_results.query_id,
             result_id=bot_results.results[0].id,
         )
         try:
