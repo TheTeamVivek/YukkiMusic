184c5ff485e4be854ec4dd78d8d05eb648e3b50f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 15:19:42 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -9,9 +9,7 @@
 import asyncio
 import importlib
 from sys import argv
-
 from pyrogram import idle
-
 import config
 from config import BANNED_USERS
 from YukkiMusic import LOGGER, app, telethn, userbot
@@ -21,7 +19,6 @@ from YukkiMusic.utils.database import get_banned_users, get_gbanned
 
 loop = asyncio.get_event_loop_policy().get_event_loop()
 HELPABLE = {}
-
 
 async def init():
     global HELPABLE
@@ -47,16 +44,18 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
-        pass
+    except Exception as e:
+        LOGGER("YukkiMusic").error(f"Error fetching banned users: {e}")
+    
     await app.start()
+
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module("YukkiMusic.plugins." + all_module)
+        imported_module = importlib.import_module(f"YukkiMusic.plugins.{all_module}")
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
-            imported_module.__MODULE__ = imported_module.__MODULE__
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
-    LOGGER("Yukkimusic.plugins").info("Successfully Imported Modules ")
+
+    LOGGER("YukkiMusic.plugins").info("Successfully Imported Modules ")
     await userbot.start()
     await Yukki.start()
     await Yukki.decorators()
@@ -66,7 +65,6 @@ async def init():
         await telethn.disconnect()
     else:
         await telethn.run_until_disconnected()
-
 
 if __name__ == "__main__":
     telethn.start(bot_token=config.BOT_TOKEN)
