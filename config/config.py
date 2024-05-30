3cab5758cb7a1037be7e559d40ecb777f3559785
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 07:46:32 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -74,8 +74,9 @@ OWNER_ID = list(
 )  # Input type must be interger
 
 
-RADIO_URL = getenv("RADIO_URL", "http://peridot.streamguys.com:7150/Mirchi")
+RADIO_URL = getenv("RADIO_URL", ""https://www.youtube.com/live/eu191hR_LEc?si=T-9QYD548jd0Mogp"")
 
+#http://peridot.streamguys.com:7150/Mirchi
 # ________________________________________________________________________________#
 # Get it from http://dashboard.heroku.com/account
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
