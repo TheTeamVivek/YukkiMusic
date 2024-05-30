ab961d5b5973af3c58760bbdbbe0333f9cbceef3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 15:26:28 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -47,13 +47,13 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except Exception as e:
-        LOGGER("YukkiMusic").error(f"Error fetching banned users: {e}")
+    except Exception:
+        pass
 
     await app.start()
 
     for all_module in ALL_MODULES:
-        imported_module = importlib.import_module(f"YukkiMusic.plugins.{all_module}")
+        imported_module = importlib.import_module(f"YukkiMusic.plugins" + all_module)
         if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
             if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
                 HELPABLE[imported_module.__MODULE__.lower()] = imported_module
