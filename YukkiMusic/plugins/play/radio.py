e4001bc92a3b34d05d1750463db90d09e24c217f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-30 10:58:07 +0530

    Update radio.py

--- YukkiMusic/plugins/play/radio.py
+++ YukkiMusic/plugins/play/radio.py
@@ -24,7 +24,7 @@ from YukkiMusic.utils.database import (
 from YukkiMusic.utils.logger import play_logs
 from YukkiMusic.utils.stream.stream import stream
 
-RADIO_URL = {
+RADIO_STATION = {
     "Capital FM": "http://media-ice.musicradio.com/CapitalMP3?.mp3&listening-from-radio-garden=1616312105154",
     "English": "https://hls-01-regions.emgsound.ru/11_msk/playlist.m3u8",
     "Air Bilaspur": "http://air.pc.cdn.bitgravity.com/air/live/pbaudio110/playlist.m3u8",
@@ -34,7 +34,7 @@ RADIO_URL = {
 
 
 @app.on_message(
-    filters.command(["radio", "cradio", "vradio"]) & filters.group & ~BANNED_USERS
+    filters.command(["radio", "cradio"]) & filters.group & ~BANNED_USERS
 )
 async def radio(client, message: Message):
     msg = await message.reply_text("ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ᴀ ᴍᴏᴍᴇɴᴛ....")
@@ -115,57 +115,55 @@ async def radio(client, message: Message):
         except:
             pass
     await msg.delete()
-    language = await get_lang(message.chat.id)
-    _ = get_string(language)
-    playmode = await get_playmode(message.chat.id)
-    playty = await get_playtype(message.chat.id)
-    if playty != "Everyone":
-        if message.from_user.id not in SUDOERS:
-            admins = adminlist.get(message.chat.id)
-            if not admins:
-                return await message.reply_text(_["admin_18"])
-            else:
-                if message.from_user.id not in admins:
-                    return await message.reply_text(_["play_4"])
-    if message.command[0][0] == "c":
-        chat_id = await get_cmode(message.chat.id)
-        if chat_id is None:
-            return await message.reply_text(_["setting_12"])
-        try:
-            chat = await app.get_chat(chat_id)
-        except:
-            return await message.reply_text(_["cplay_4"])
-        channel = chat.title
-    else:
-        chat_id = message.chat.id
-        channel = None
-
-    video = None
-    if message.command[0][0] == "v":
-        video = True
-    else:
-        if "-v" in message.text:
-            video = True
+    station_name = " ".join(message.command[1:])
+    RADIO_URL = RADIO_STATION.get(station_name)
+    if RADIO_URL:
+        language = await get_lang(message.chat.id)
+        _ = get_string(language)
+        playmode = await get_playmode(message.chat.id)
+        playty = await get_playtype(message.chat.id)
+        if playty != "Everyone":
+            if message.from_user.id not in SUDOERS:
+                admins = adminlist.get(message.chat.id)
+                if not admins:
+                    return await message.reply_text(_["admin_18"])
+                else:
+                    if message.from_user.id not in admins:
+                        return await message.reply_text(_["play_4"])
+        if message.command[0][0] == "c":
+            chat_id = await get_cmode(message.chat.id)
+            if chat_id is None:
+                return await message.reply_text(_["setting_12"])
+            try:
+                chat = await app.get_chat(chat_id)
+            except:
+                return await message.reply_text(_["cplay_4"])
+            channel = chat.title
         else:
-            video = None
-    RADIO_URL = "http://media-ice.musicradio.com/CapitalMP3?.mp3&listening-from-radio-garden=1616312105154"
-    mystic = await message.reply_text(
-        _["play_2"].format(channel) if channel else _["play_1"]
-    )
-    try:
-        await stream(
-            _,
-            mystic,
-            message.from_user.id,
-            RADIO_URL,
-            chat_id,
-            message.from_user.mention,
-            message.chat.id,
-            video=video,
-            streamtype="index",
+            chat_id = message.chat.id
+            channel = None
+
+        video = None
+        mystic = await message.reply_text(
+            _["play_2"].format(channel) if channel else _["play_1"]
         )
-    except Exception as e:
-        ex_type = type(e).__name__
-        err = e if ex_type == "AssistantErr" else _["general_3"].format(ex_type)
-        return await mystic.edit_text(err)
-    return await play_logs(message, streamtype="M3u8 or Index Link")
+        try:
+            await stream(
+                _,
+                mystic,
+                message.from_user.id,
+                RADIO_URL,
+                chat_id,
+                message.from_user.mention,
+                message.chat.id,
+                video=video,
+                streamtype="index",
+            )
+        except Exception as e:
+            ex_type = type(e).__name__
+            err = e if ex_type == "AssistantErr" else _["general_3"].format(ex_type)
+            return await mystic.edit_text(err)
+        return await play_logs(message, streamtype="M3u8 or Index Link")
+    else:
+        valid_stations = ", ".join(RADIO_STATION.keys())
+        await message.reply(f"Invalid station name. Only these stations can be played: {valid_stations}")
