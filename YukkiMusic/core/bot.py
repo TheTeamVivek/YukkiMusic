0b35148d0441860c196bfcb54124cb9983d6a12e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-31 09:45:54 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -27,8 +27,6 @@ class YukkiBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            workdir="datafiles",
-            max_concurrent_transmissions=7,
         )
 
     async def start(self):
