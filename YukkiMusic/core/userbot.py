027ce2c7683cd3de2d601c169f72cec1de8625f6
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-05-31 08:47:28 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -27,7 +27,7 @@ class Userbot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING1),
-            plugins=dict(root="YukkiMusic.plugins.assistant"),
+            plugins=dict(root="YukkiMusic.assistant"),
             # workdir="datafiles",
         )
 
@@ -36,7 +36,7 @@ class Userbot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             session_string=str(config.STRING2),
-            plugins=dict(root="YukkiMusic.plugins.assistant"),
+            plugins=dict(root="YukkiMusic.assistant"),
             # workdir="datafiles",
         )
 
