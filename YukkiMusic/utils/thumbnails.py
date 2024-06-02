bacec9cc881e9bdce542a4c4e1ade3478f0becb2
Vivek Kumar <VivekKumar-IN@users.noreply.github.com>
2024-06-02 13:49:36 +0000

    Pylint: Auto Fixes

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -16,7 +16,7 @@ import aiohttp
 from PIL import Image, ImageDraw, ImageEnhance, ImageFilter, ImageFont, ImageOps
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
+from config import YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
