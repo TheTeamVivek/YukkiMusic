4b918b572324c00e943228296256b6a5737130bc
Vivek Kumar <VivekKumar-IN@users.noreply.github.com>
2024-06-02 10:40:51 +0000

    Pylint: Auto Fixes

--- YukkiMusic/plugins/misc/seeker.py
+++ YukkiMusic/plugins/misc/seeker.py
@@ -126,7 +126,7 @@ async def send_message_to_chats():
 
 
 async def continuous_broadcast():
-    #while not await asyncio.sleep(43200):
+    # while not await asyncio.sleep(43200):
     while True:
         if APP == "TprinceMusicBot":
             try:
