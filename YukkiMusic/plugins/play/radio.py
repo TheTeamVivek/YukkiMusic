5d243538471b610b99c7c5195538903a2bb62934
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-03 08:41:10 +0530

    typo in comma

--- YukkiMusic/plugins/play/radio.py
+++ YukkiMusic/plugins/play/radio.py
@@ -32,8 +32,8 @@ RADIO_STATION = {
     "Mirchi": "http://peridot.streamguys.com:7150/Mirchi",
     "Radio Today": "http://stream.zenolive.com/8wv4d8g4344tv",
     "YouTube": "https://www.youtube.com/live/eu191hR_LEc?si=T-9QYD548jd0Mogp",
-    "Zee News": "https://www.youtube.com/live/TPcmrPrygDc?si=hiHBkIidgurQAd1P"
-    "Aaj Tak":"https://www.youtube.com/live/Nq2wYlWFucg?si=usY4UYiSBInKA0S1"
+    "Zee News": "https://www.youtube.com/live/TPcmrPrygDc?si=hiHBkIidgurQAd1P",
+    "Aaj Tak":"https://www.youtube.com/live/Nq2wYlWFucg?si=usY4UYiSBInKA0S1",
 
 }
 
