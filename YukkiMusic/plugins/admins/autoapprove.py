4e037003ac3771b90b76f630bdc476ba04c24c85
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-07 21:32:21 +0530

    Update autoapprove.py

--- YukkiMusic/plugins/admins/autoapprove.py
+++ YukkiMusic/plugins/admins/autoapprove.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from pyrogram import filters
 from pyrogram.enums import ChatMembersFilter
 from pyrogram.types import ChatJoinRequest
