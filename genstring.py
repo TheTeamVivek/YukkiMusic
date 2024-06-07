fe11a37cec6248f159b2f098784c960e093115cb
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-07 21:25:52 +0530

    Update genstring.py

--- genstring.py
+++ genstring.py
@@ -3,8 +3,7 @@
 #
 # This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
 # and is released under the "GNU v3.0 License Agreement".
-# Please see <
-https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
 
