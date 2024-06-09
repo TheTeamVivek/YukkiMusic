113d1f0cfbaf3d518d92b9334e6f592de86a29a7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 18:24:27 +0530

    Update clean.py

--- YukkiMusic/plugins/tools/clean.py
+++ YukkiMusic/plugins/tools/clean.py
@@ -3,10 +3,11 @@ import shutil
 
 from pyrogram import filters
 
-from YukkiMusic import app
+from YukkiMusic import app, userbot
 from YukkiMusic.misc import SUDOERS
 
 
+@userbot.one.on_message(filters.command("clean"))
 @app.on_message(filters.command("clean") & SUDOERS)
 async def clean(_, message):
     A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
