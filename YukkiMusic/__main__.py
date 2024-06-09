c88a0893749fd47c458c44b376ac694ed06a5603
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 18:34:07 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -46,8 +46,8 @@ async def init():
     except Exception:
         pass
 
-    await app.start()
-    await userbot.start()
+    #await app.start()
+   # await userbot.start()
 
     for all_module in ALL_MODULES:
         imported_module = importlib.import_module(all_module)
