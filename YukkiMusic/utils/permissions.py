3f8845f03de4b78cd05f1f2e550f35a24054f599
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 10:24:41 +0530

    Update permissions.py

--- YukkiMusic/utils/permissions.py
+++ YukkiMusic/utils/permissions.py
@@ -1,3 +1,12 @@
+#
+# Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+
 import logging
 from functools import wraps
 from traceback import format_exc as err
