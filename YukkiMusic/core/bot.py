962a8ceb79727a1f54540577fd0e819fe35c6dc5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 20:05:27 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -18,12 +18,13 @@ import config
 
 from ..logging import LOGGER
 
+NAME = "YukkiMusicBot"
 
 class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "YukkiMusicBot",
+            NAME,
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
