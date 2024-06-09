79a832fb823c7461b2919c686c045d911248d462
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-09 10:23:35 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -2,48 +2,35 @@
 # Copyright (C) 2024 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
 #
 # This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
+# and is released under the MIT License .
 # Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
 #
+
 from config import YOUTUBE_IMG_URL
 from youtubesearchpython.__future__ import VideosSearch
 
 
-# Function to get thumbnail by video ID
+
+
 async def gen_thumb(videoid):
     try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return YOUTUBE_IMG_URL
-
-
-async def gen_qthumb(vidid):
-    try:
-        # Search for the video using video ID
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception as e:
-        return YOUTUBE_IMG_URL
-
-
-# Function to get thumbnail by video ID
-async def q(videoid):
-    try:
-        # Search for the video using video ID
         results = VideosSearch(videoid, limit=1)
         search_results = await results.next()
-        
-        # Extract the thumbnail URL
+        if "result" in search_results and search_results["result"]:
+            thumbnail = search_results["result"][0]["thumbnails"][0]["url"].split("?")[0]
+            return thumbnail
+        else:
+            return YOUTUBE_IMG_URL
+    except Exception as e:
+        return YOUTUBE_IMG_URL
+
+
+async def gen_qthumb(videoid):
+    try:
+        results = VideosSearch(videoid, limit=1)
+        search_results = await results.next()
         if "result" in search_results and search_results["result"]:
             thumbnail = search_results["result"][0]["thumbnails"][0]["url"].split("?")[0]
             return thumbnail
