f3b8b90366d8852aabcbd1acf90ad7b3f8a6c430
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-10 12:23:48 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -12,7 +12,7 @@ import os
 from pyrogram import Client
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.types import BotCommand
-
+from pyromod import listen
 import config
 
 from ..logging import LOGGER
