c6eb61b4ca9e7c6b41e5f830504112d43a4e4c4c
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 10:39:42 +0530

    Update Clean.py

--- YukkiMusic/Userbot/tools/Clean.py
+++ YukkiMusic/Userbot/tools/Clean.py
@@ -2,7 +2,7 @@ import os
 import shutil
 
 from pyrogram import filters
-
+from typing import Union, List
 from YukkiMusic import app, userbot
 from YukkiMusic.misc import SUDOERS
 from pyrogram import filters
