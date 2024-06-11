1effc8d6810b0d30cb2652e90a05b945f71b1909
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 08:44:07 +0530

    Create Clean.py

---
+++ YukkiMusic/Userbot/tools/Clean.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+
+from pyrogram import filters
+
+from YukkiMusic import app, userbot
+from YukkiMusic.misc import SUDOERS
+
+
+@userbot.one.on_message(filters.command("clea"))
+@app.on_message(filters.command("clea") & SUDOERS)
+async def clean(_, message):
+    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+    dir = "downloads"
+    dir1 = "cache"
+    shutil.rmtree(dir)
+    shutil.rmtree(dir1)
+    os.mkdir(dir)
+    os.mkdir(dir1)
+    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
