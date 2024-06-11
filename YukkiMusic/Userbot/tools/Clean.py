43993f752a2528c4763b77b0f76a49931e713ccb
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 10:42:01 +0530

    Update Clean.py

--- YukkiMusic/Userbot/tools/Clean.py
+++ YukkiMusic/Userbot/tools/Clean.py
@@ -7,29 +7,4 @@ from YukkiMusic import app, userbot
 from YukkiMusic.misc import SUDOERS
 from pyrogram import filters
 
-def commandx(commands: Union[str, List[str]]):
-    return filters.command(commands, ['.', '!'])
-
-
-@userbot.one.on_message(commandx("clea"))
-async def clean(_, message):
-    A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
-    dir = "downloads"
-    dir1 = "cache"
-    shutil.rmtree(dir)
-    shutil.rmtree(dir1)
-    os.mkdir(dir)
-    os.mkdir(dir1)
-    await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
-
-async def h():
-    @userbot.one.on_message(commandx("clea"))
-    async def clean(_, message):
-        A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
-        dir = "downloads"
-        dir1 = "cache"
-        shutil.rmtree(dir)
-        shutil.rmtree(dir1)
-        os.mkdir(dir)
-        os.mkdir(dir1)
-        await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
+ 
