51135af11e36f774a107a01971d0bce815b29bd8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 19:16:59 +0530

    Update memorydatabase.py

--- YukkiMusic/utils/database/memorydatabase.py
+++ YukkiMusic/utils/database/memorydatabase.py
@@ -29,7 +29,7 @@ autoenddb = mongodb.autoend
 coupledb = mongodb.couple
 notesdb = mongodb.notes
 welcomedb = mongodb.welcome
-
+filtersdb = db.filters
 
 # Shifting to memory [ mongo sucks often]
 loop = {}
@@ -50,6 +50,72 @@ vlimit = []
 maintenance = []
 suggestion = {}
 autoend = {}
+
+
+
+async def get_filters_count() -> dict:
+    chats_count = 0
+    filters_count = 0
+    async for chat in filtersdb.find({"chat_id": {"$lt": 0}}):
+        filters_name = await get_filters_names(chat["chat_id"])
+        filters_count += len(filters_name)
+        chats_count += 1
+    return {
+        "chats_count": chats_count,
+        "filters_count": filters_count,
+    }
+
+
+async def _get_filters(chat_id: int) -> Dict[str, int]:
+    _filters = await filtersdb.find_one({"chat_id": chat_id})
+    if not _filters:
+        return {}
+    return _filters["filters"]
+
+
+async def get_filters_names(chat_id: int) -> List[str]:
+    _filters = []
+    for _filter in await _get_filters(chat_id):
+        _filters.append(_filter)
+    return _filters
+
+
+async def get_filter(chat_id: int, name: str) -> Union[bool, dict]:
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    if name in _filters:
+        return _filters[name]
+    return False
+
+
+async def save_filter(chat_id: int, name: str, _filter: dict):
+    name = name.lower().strip()
+    _filters = await _get_filters(chat_id)
+    _filters[name] = _filter
+    await filtersdb.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"filters": _filters}},
+        upsert=True,
+    )
+
+
+async def delete_filter(chat_id: int, name: str) -> bool:
+    filtersd = await _get_filters(chat_id)
+    name = name.lower().strip()
+    if name in filtersd:
+        del filtersd[name]
+        await filtersdb.update_one(
+            {"chat_id": chat_id},
+            {"$set": {"filters": filtersd}},
+            upsert=True,
+        )
+        return True
+    return False
+
+
+async def deleteall_filters(chat_id: int):
+    return await filtersdb.delete_one({"chat_id": chat_id})
+
 
 
 async def get_notes_count() -> dict:
