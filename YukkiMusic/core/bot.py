a9cad39a70623fe76f8c076fc789d6b579b115a5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 09:21:35 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -25,7 +25,6 @@ class YukkiBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            workdir="datafiles",
         )
 
     async def start(self):
