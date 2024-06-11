08b64f79f17a32f79cdf11cf4f1d68d4108a71b7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 09:02:45 +0530

    Update Clean.py

--- YukkiMusic/Userbot/tools/Clean.py
+++ YukkiMusic/Userbot/tools/Clean.py
@@ -5,9 +5,27 @@ from pyrogram import filters
 
 from YukkiMusic import app, userbot
 from YukkiMusic.misc import SUDOERS
+from functools import wraps
+from pyrogram.handlers import MessageHandler
+from pyrogram import filters
+
+def userbot_command(command, prefixes=[".", "/"]):
+    def decorator(func):
+        @wraps(func)
+        async def wrapper(client, message):
+            return await func(client, message)
+        
+        for userbot_client in [userbot.one, userbot.two, userbot.three, userbot.four, userbot.five]:
+            if userbot_client:
+                userbot_client.add_handler(
+                    MessageHandler(wrapper, filters.command(command, prefixes=prefixes))
+                )
+        return wrapper
+    return decorator
 
 
-@userbot.one.on_message(filters.command("clea"))
+
+@userbot_command("clea")
 @app.on_message(filters.command("clea") & SUDOERS)
 async def clean(_, message):
     A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
