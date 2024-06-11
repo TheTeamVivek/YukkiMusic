8a399595b9843301cc69e8e9a8a0bb4179fd8996
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 10:35:13 +0530

    Update Clean.py

--- YukkiMusic/Userbot/tools/Clean.py
+++ YukkiMusic/Userbot/tools/Clean.py
@@ -21,3 +21,15 @@ async def clean(_, message):
     os.mkdir(dir)
     os.mkdir(dir1)
     await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
+
+async def h():
+    @userbot.one.on_message(commandx("clea"))
+    async def clean(_, message):
+        A = await message.reply_text("ᴄʟᴇᴀɴɪɴɢ ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs...")
+        dir = "downloads"
+        dir1 = "cache"
+        shutil.rmtree(dir)
+        shutil.rmtree(dir1)
+        os.mkdir(dir)
+        os.mkdir(dir1)
+        await A.edit("ᴛᴇᴍᴘ ᴅɪʀᴇᴄᴛᴏʀɪᴇs ᴀʀᴇ ᴄʟᴇᴀɴᴇᴅ")
