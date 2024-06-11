3155f8fca39b2367409a6501a1c57b6b5641d0df
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-11 10:09:41 +0530

    Update userbot.py

--- YukkiMusic/core/userbot.py
+++ YukkiMusic/core/userbot.py
@@ -57,9 +57,10 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("TeamYM")
                 await self.one.join_chat("TheYukki")
                 await self.one.join_chat("YukkiSupport")
+                await self.one.join_chat("TheTeamVivek")
+                await self.one.join_chat("TheTeamVk")
             except:
                 pass
             assistants.append(1)
@@ -89,9 +90,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("TeamYM")
                 await self.two.join_chat("TheYukki")
                 await self.two.join_chat("YukkiSupport")
+                await self.two.join_chat("TheTeamVivek")
+                await self.two.join_chat("TheTeamVk")
             except:
                 pass
             assistants.append(2)
@@ -121,9 +123,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("TeamYM")
                 await self.three.join_chat("TheYukki")
                 await self.three.join_chat("YukkiSupport")
+                await self.three.join_chat("TheTeamVivek")
+                await self.three.join_chat("TheTeamVk")
             except:
                 pass
             assistants.append(3)
@@ -153,9 +156,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("TeamYM")
                 await self.four.join_chat("TheYukki")
                 await self.four.join_chat("YukkiSupport")
+                await self.four.join_chat("TheTeamVivek")
+                await self.four.join_chat("TheTeamVk")
             except:
                 pass
             assistants.append(4)
@@ -185,9 +189,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("TeamYM")
                 await self.five.join_chat("TheYukki")
                 await self.five.join_chat("YukkiSupport")
+                await self.five.join_chat("TheTeamVivek")
+                await self.five.join_chat("TheTeamVk")
             except:
                 pass
             assistants.append(5)
