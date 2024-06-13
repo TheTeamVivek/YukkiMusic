5a69dec501e04f9e13e0212bf4aae7452d47c7c1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-13 09:56:23 +0530

    Update filter.py

--- YukkiMusic/utils/filter.py
+++ YukkiMusic/utils/filter.py
@@ -21,7 +21,7 @@ async def admin_check(message: Message) -> bool:
     if not message.from_user:
         return False
 
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
+    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.GROUP]:
         return False
 
     if message.from_user.id in [
