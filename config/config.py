61893279f65f5449dc18d07c859123921cde8a5d
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-13 18:25:15 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -14,6 +14,7 @@ from os import getenv
 from dotenv import load_dotenv
 from pyrogram import filters
 
+
 load_dotenv()
 # ________________________________________________________________________________#
 # Get it from my.telegram.org
