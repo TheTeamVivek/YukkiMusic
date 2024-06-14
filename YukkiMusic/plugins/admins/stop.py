c96d84edf8a84519cf4ccb97def389ebd7d9122b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-14 18:27:43 +0530

    Update stop.py

--- YukkiMusic/plugins/admins/stop.py
+++ YukkiMusic/plugins/admins/stop.py
@@ -9,12 +9,18 @@
 #
 from pyrogram import filters
 from pyrogram.types import Message
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
 
 from config import BANNED_USERS
 from strings import get_command
 from YukkiMusic import app
 from YukkiMusic.core.call import Yukki
-from YukkiMusic.utils.database import set_loop, delete_filter
+from YukkiMusic.utils.database import set_loop, delete_filter, is_maintenance, is_commanddelete_on, get_lang, is_nonadmin_chat, is_active_chat, get_cmode
+from config import adminlist
+from strings import get_string
+from YukkiMusic.misc import SUDOERS
+
 from YukkiMusic.utils.decorators import AdminRightsCheck
 
 # Commands
@@ -22,8 +28,58 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 
 
 @app.on_message(filters.command(STOP_COMMAND) & filters.group & ~BANNED_USERS)
-@AdminRightsCheck
-async def stop_music(cli, message: Message, _, chat_id):
+async def stop_music(cli, message: Message):
+    if await is_maintenance() is False:
+        if message.from_user.id not in SUDOERS:
+            return await message.reply_text(
+                "Bot is under maintenance. Please wait for some time..."
+            )
+    if await is_commanddelete_on(message.chat.id):
+        try:
+            await message.delete()
+        except:
+            pass
+        try:
+            language = await get_lang(message.chat.id)
+            _ = get_string(language)
+        except:
+            _ = get_string("en")
+
+        if message.sender_chat:
+            upl = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="How to Fix this? ",
+                            callback_data="AnonymousAdmin",
+                        ),
+                    ]
+                ]
+            )
+            return await message.reply_text(_["general_4"], reply_markup=upl)
+
+        if message.command[0][0] == "c":
+            chat_id = await get_cmode(message.chat.id)
+            if chat_id is None:
+                return await message.reply_text(_["setting_12"])
+            try:
+                await app.get_chat(chat_id)
+            except:
+                return await message.reply_text(_["cplay_4"])
+        else:
+            chat_id = message.chat.id
+        if not await is_active_chat(chat_id):
+            return await message.reply_text(_["general_6"])
+        is_non_admin = await is_nonadmin_chat(message.chat.id)
+        if not is_non_admin:
+            if message.from_user.id not in SUDOERS:
+                admins = adminlist.get(message.chat.id)
+                if not admins:
+                    return await message.reply_text(_["admin_18"])
+                else:
+                    if message.from_user.id not in admins:
+                        return await message.reply_text(_["admin_19"])      
+
     if len(message.command) < 2:
         await Yukki.stop_stream(chat_id)
         await set_loop(chat_id, 0)
