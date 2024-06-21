6d352316b39547f264c4ead558eb8b42ea119844
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-21 09:02:13 +0530

    Update assistant.py

--- YukkiMusic/utils/assistant.py
+++ YukkiMusic/utils/assistant.py
@@ -13,41 +13,41 @@ from YukkiMusic.utils.database import get_client
 
 async def get_assistant_details():
     ms = ""
-    msg = "ᴜsᴀsɢᴇ : /setassistant [ᴀssɪsᴛᴀɴᴛ ɴᴏ ] ᴛᴏ ᴄʜᴀɴɢᴇ ᴀɴᴅ sᴇᴛ ᴍᴀɴᴜᴀʟʟʏ ɢʀᴏᴜᴘ ᴀssɪsᴛᴀɴᴛ \n ʙᴇʟᴏᴡ sᴏᴍᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴀssɪsᴛᴀɴᴛ ᴅᴇᴛᴀɪʟ's ᴏɴ ʙᴏᴛ sᴇʀᴠᴇʀ\n"
+    msg = "**ᴜsᴀsɢᴇ** : /setassistant [ᴀssɪsᴛᴀɴᴛ ɴᴏ ] ᴛᴏ ᴄʜᴀɴɢᴇ ᴀɴᴅ sᴇᴛ ᴍᴀɴᴜᴀʟʟʏ ɢʀᴏᴜᴘ ᴀssɪsᴛᴀɴᴛ \n ʙᴇʟᴏᴡ sᴏᴍᴇ ᴀᴠᴀɪʟᴀʙʟᴇ ᴀssɪsᴛᴀɴᴛ ᴅᴇᴛᴀɪʟ's\n"
     try:
         a = await get_client(1)
-        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `1` ɴᴀᴍᴇ :- [{a.name}](https://t.me/{a.username})  ᴜsᴇʀɴᴀᴍᴇ :-  @{a.username} ɪᴅ :- {a.id}\n\n"
+        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `1` \nɴᴀᴍᴇ :- [{a.name}](https://t.me/{a.username})  \nᴜsᴇʀɴᴀᴍᴇ :-  @{a.username} \nɪᴅ :- {a.id}\n\n"
     except:
         pass
 
     try:
         b = await get_client(2)
-        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `2` ɴᴀᴍᴇ :- [{b.name}](https://t.me/{b.username})  ᴜsᴇʀɴᴀᴍᴇ :-  @{b.username} ɪᴅ :- {b.id}\n"
+        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `2` \nɴᴀᴍᴇ :- [{b.name}](https://t.me/{b.username})  \nᴜsᴇʀɴᴀᴍᴇ :-  @{b.username} \nɪᴅ :- {b.id}\n"
     except:
         pass
 
     try:
         c = await get_client(3)
-        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `3` ɴᴀᴍᴇ :- [{c.name}](https://t.me/{c.username})  ᴜsᴇʀɴᴀᴍᴇ :-  @{c.username} ɪᴅ :- {c.id}\n"
+        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `3` \nɴᴀᴍᴇ :- [{c.name}](https://t.me/{c.username})  \nᴜsᴇʀɴᴀᴍᴇ :-  @{c.username} \nɪᴅ :- {c.id}\n"
     except:
         pass
 
     try:
         d = await get_client(4)
-        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `4` ɴᴀᴍᴇ :- [{d.name}](https://t.me/{d.username})  ᴜsᴇʀɴᴀᴍᴇ :-  @{d.username} ɪᴅ :- {d.id}\n"
+        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `4` \nɴᴀᴍᴇ :- [{d.name}](https://t.me/{d.username})  \nᴜsᴇʀɴᴀᴍᴇ :-  @{d.username} \nɪᴅ :- {d.id}\n"
     except:
         pass
 
     try:
         e = await get_client(5)
-        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `5` ɴᴀᴍᴇ :- [{e.name}](https://t.me/{e.username})  ᴜsᴇʀɴᴀᴍᴇ :-  @{e.username} ɪᴅ :- {e.id}\n"
+        msg += f"ᴀssɪsᴛᴀɴᴛ ɴᴜᴍʙᴇʀ:- `5`\nɴᴀᴍᴇ :- [{e.name}](https://t.me/{e.username})\nᴜsᴇʀɴᴀᴍᴇ :-  @{e.username} \nɪᴅ :- {e.id}\n"
     except:
         pass
 
     return msg
 
 
-async def assistant():
+async def is_avl_assistant():
     from config import STRING1, STRING2, STRING3, STRING4, STRING5
 
     filled_count = sum(
