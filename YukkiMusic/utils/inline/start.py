75bddde1a5a71351b0221ca0ac54dac147f3b98d
VivekKumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-24 13:43:12 +0000

    Pylint: Auto Fixes

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -74,19 +74,19 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     )
     if GITHUB_REPO and OWNER:
         buttons.append(
-          [
-              InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-              InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-         ]
-       )
+            [
+                InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
+                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+            ]
+        )
     else:
-        
+
         if GITHUB_REPO:
             buttons.append(
                 [
-                  InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
-              ]
-           )
+                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                ]
+            )
 
         if OWNER:
             buttons.append(
