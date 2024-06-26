76f9addf1e11db6a847818b7d68cf545a37699bd
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-26 11:28:22 +0530

    Update thumbnails.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -11,32 +11,24 @@
 from config import YOUTUBE_IMG_URL
 from youtubesearchpython.__future__ import VideosSearch
 
-
-async def gen_thumb(videoid):
+async def get_thumb(videoid):
     try:
-        results = VideosSearch(videoid, limit=1)
-        search_results = await results.next()
-        if "result" in search_results and search_results["result"]:
-            thumbnail = search_results["result"][0]["thumbnails"][0]["url"].split("?")[
-                0
-            ]
-            return thumbnail
-        else:
-            return YOUTUBE_IMG_URL
+        # Search for the video using video ID
+        query = f"https://www.youtube.com/watch?v={videoid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
     except Exception as e:
         return YOUTUBE_IMG_URL
 
 
-async def gen_qthumb(videoid):
+async def get_thumb(vidid):
     try:
-        results = VideosSearch(videoid, limit=1)
-        search_results = await results.next()
-        if "result" in search_results and search_results["result"]:
-            thumbnail = search_results["result"][0]["thumbnails"][0]["url"].split("?")[
-                0
-            ]
-            return thumbnail
-        else:
-            return YOUTUBE_IMG_URL
+        query = f"https://www.youtube.com/watch?v={vidid}"
+        results = VideosSearch(query, limit=1)
+        for result in (await results.next())["result"]:
+            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
+        return thumbnail
     except Exception as e:
         return YOUTUBE_IMG_URL
