561c1a22185a7c3472e7ee56cb631dfe8482bd16
VivekKumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-30 06:12:37 +0000

    Pylint: Auto Fixes

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'GreyMatters'
+    return "GreyMatters"
 
 
 if __name__ == "__main__":
