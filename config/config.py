3464eb012cbac303a2acebf8ca4daa9257fd4045
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-01 17:35:46 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -204,19 +204,11 @@ autoclean = []
 
 # Images
 
-
-PHOTO = list(
-    filter(
-        None,
-        getenv("PHOTO_LINKS", "").split(),
-    )
-)
-
-
 START_IMG_URL = getenv(
     "START_IMG_URL",
     "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg",
 )
+
 PING_IMG_URL = getenv(
     "PING_IMG_URL",
     "https://telegra.ph/file/91533956c91d0fd7c9f20.jpg",
