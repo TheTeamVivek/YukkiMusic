1c2791bc2d1686ae40f44cf6b2538826b6f4ed0e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-04 08:48:37 +0530

    Update utils

---
+++ YukkiMusic/utils/cleanmode.py
@@ -0,0 +1,12 @@
+from YukkiMusic import app
+protected_messages = {}
+
+async def protect_message(chat_id, message_id):
+    if chat_id not in protected_messages:
+        protected_messages[chat_id] = []
+    protected_messages[chat_id].append(message_id)
+
+
+async def send_message(chat_id, text):
+    message = await YukkiBot().send_message(chat_id, text)
+    await protect_message(chat_id, message.message_id)
