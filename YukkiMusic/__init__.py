722e84ab0e6b7289aad04794bdc83fe40f562074
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-04 08:51:46 +0530

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -15,7 +15,6 @@ from YukkiMusic.core.git import git
 from YukkiMusic.core.userbot import Userbot
 from YukkiMusic.misc import dbb, heroku, sudo
 from .logging import LOGGER
-from YukkiMusic.utils.cleanmode import send_message
 
 # Directories
 dirr()
