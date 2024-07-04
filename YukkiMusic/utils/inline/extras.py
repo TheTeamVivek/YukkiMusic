40c05cd97196a792f171b4024ccbf5ef42e166f9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-04 18:42:30 +0530

    Update extras.py

--- YukkiMusic/utils/inline/extras.py
+++ YukkiMusic/utils/inline/extras.py
@@ -12,7 +12,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from config import SUPPORT_GROUP
 
 
-def supp_markup(_):
+def support_group_markup(_):
     upl = InlineKeyboardMarkup(
         [
             [
