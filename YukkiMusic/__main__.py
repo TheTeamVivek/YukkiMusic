26cf842cc1fb8f72903c0463d1049dcd880b964e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-06 13:18:29 +0530

    Update __main__.py

--- YukkiMusic/__main__.py
+++ YukkiMusic/__main__.py
@@ -52,7 +52,7 @@ async def init():
                     HELPABLE[imported_module.__MODULE__.lower()] = imported_module
                 else:
                     raise Exception(
-                        "Can't have two modules with the same name! Please change one"
+                        f"Can't have two modules with {imported_module.__MODULE__} name! Please change one"
                     )
 
     LOGGER("YukkiMusic.plugins").info("Successfully Imported All Modules ")
