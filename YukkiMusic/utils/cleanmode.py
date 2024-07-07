4be0e8adcf0521eb1e933fe68f04970b7e536e04
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-07 08:15:53 +0530

    Update cleanmode.py

--- YukkiMusic/utils/cleanmode.py
+++ YukkiMusic/utils/cleanmode.py
@@ -1,4 +1,5 @@
 from YukkiMusic import app
+from config import LOG_GROUP_ID
 
 protected_messages = {}
 
@@ -9,6 +10,16 @@ async def protect_message(chat_id, message_id):
     protected_messages[chat_id].append(message_id)
 
 
-async def send_message(chat_id, text):
-    message = await app.send_message(chat_id, text)
-    await protect_message(chat_id, message.id)
+async def send_message(chat_id, text, reply=None):
+    if reply:
+        try:
+            message = await app.send_message(chat_id, text,reply_to_message_id=reply, disable_web_page_preview=True)
+            await protect_message(chat_id, message.id)
+        except Exception as e:
+            return await app.send_message(LOG_GROUP_ID, e)
+    else:
+        try: 
+            message = await app.send_message(chat_id, text, disable_web_page_preview=True)
+            await protect_message(chat_id, message.id)
+        except Exception as e:
+            return await app.send_message(LOG_GROUP_ID, e)
