44a663fa8bfd6adf37c9b14d922682620224a1c9
VivekKumar-IN <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-07-07 02:46:11 +0000

    Pylint: Auto Fixes

--- YukkiMusic/utils/cleanmode.py
+++ YukkiMusic/utils/cleanmode.py
@@ -13,13 +13,17 @@ async def protect_message(chat_id, message_id):
 async def send_message(chat_id, text, reply=None):
     if reply:
         try:
-            message = await app.send_message(chat_id, text,reply_to_message_id=reply, disable_web_page_preview=True)
+            message = await app.send_message(
+                chat_id, text, reply_to_message_id=reply, disable_web_page_preview=True
+            )
             await protect_message(chat_id, message.id)
         except Exception as e:
             return await app.send_message(LOG_GROUP_ID, e)
     else:
-        try: 
-            message = await app.send_message(chat_id, text, disable_web_page_preview=True)
+        try:
+            message = await app.send_message(
+                chat_id, text, disable_web_page_preview=True
+            )
             await protect_message(chat_id, message.id)
         except Exception as e:
             return await app.send_message(LOG_GROUP_ID, e)
