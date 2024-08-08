99fdf99213bb5b957b109739ea52b82e7f6c5303
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-08-08 15:42:52 +0530

    Update Youtube.py

--- YukkiMusic/platforms/Youtube.py
+++ YukkiMusic/platforms/Youtube.py
@@ -460,33 +460,28 @@ class YTM:
 
                     return file_path
 
-        response = requests.get(
-            f"https://pipedapi-libre.kavin.rocks/streams/{vidid}"
-        ).json()
+        response =  requests.get(f"https://pipedapi-libre.kavin.rocks/streams/{vidid}").json()
         loop = asyncio.get_running_loop()
+
         if songvideo:
-            url = response.get("videoStreams", [])[-1]["url"]
-            fpath = await loop.run_in_executor(
-                None, lambda: asyncio.run(song_video_dl(url))
-            )
+
+            url = response.get("videoStreams", [])[-1]['url']
+            fpath = await loop.run_in_executor(None, lambda: asyncio.run(song_video_dl(url)))
             return fpath
+
         elif songaudio:
-            url = response.get("videoStreams", [])[-1]["url"]
-            fpath = await loop.run_in_executor(
-                None, lambda: asyncio.run(song_audio_dl(url))
-            )
-            return fpath
+            return response.get("audioStreams", [])[4]["url"]  
+
+
         elif video:
-            url = response.get("videoStreams", [])[-1]["url"]
+            url = response.get("videoStreams", [])[-1]['url']
             direct = True
-            downloaded_file = await loop.run_in_executor(
-                None, lambda: asyncio.run(video_dl(url))
-            )
+            downloaded_file = await loop.run_in_executor(None, lambda: asyncio.run(video_dl(url)))
+
+
         else:
-            url = response.get("audioStreams", [])[4]["url"]
             direct = True
-            downloaded_file = await loop.run_in_executor(
-                None, lambda: asyncio.run(audio_dl(url))
-            )
+            downloaded_file = response.get("audioStreams", [])[4]['url']
+
 
         return downloaded_file, direct
