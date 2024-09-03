3122838e67eba641fafe9ba7f7f56451d9f275d9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-03 11:24:12 +0530

    Update exceptions.py

--- YukkiMusic/utils/exceptions.py
+++ YukkiMusic/utils/exceptions.py
@@ -12,3 +12,8 @@
 class AssistantErr(Exception):
     def __init__(self, errr: str):
         super().__init__(errr)
+
+
+class DownloadError(Exception):
+    """Custom exception for download failures."""
+    pass
