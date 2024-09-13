2143eec3ec2dfb91d320a9f2d08bd978d1bf4a29
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-09-13 11:48:41 +0530

    old is gold

--- YukkiMusic/utils/inline/playlist.py
+++ YukkiMusic/utils/inline/playlist.py
@@ -10,6 +10,7 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
+
 def botplaylist_markup(_):
     buttons = [
         [
@@ -17,16 +18,14 @@ def botplaylist_markup(_):
                 text=_["PL_B_1"],
                 callback_data="get_playlist_playmode",
             ),
-        ],
-        [
             InlineKeyboardButton(text=_["PL_B_8"], callback_data="get_top_playlists"),
         ],
         [
+            InlineKeyboardButton(text=_["PL_B_4"], callback_data="PM"),
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
         ],
     ]
     return buttons
-
 
 def top_play_markup(_):
     buttons = [
