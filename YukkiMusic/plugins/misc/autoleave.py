cef43d9d7033f4574da88dafe7faaec1eaf17ef8
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-10-02 20:19:06 +0530

    i guess asyncio.gather is better

--- YukkiMusic/plugins/misc/autoleave.py
+++ YukkiMusic/plugins/misc/autoleave.py
@@ -29,36 +29,43 @@ autoend = {}
 
 async def auto_leave():
     if config.AUTO_LEAVING_ASSISTANT == str(True):
+        from YukkiMusic.core.userbot import assistants
+        
+        async def leave_inactive_chats(client):
+            left = 0
+            try:
+                async for i in client.get_dialogs():
+                    chat_type = i.chat.type
+                    if chat_type in [
+                        ChatType.SUPERGROUP,
+                        ChatType.GROUP,
+                        ChatType.CHANNEL,
+                    ]:
+                        chat_id = i.chat.id
+                        if chat_id not in [
+                            config.LOG_GROUP_ID,
+                            -1002159045835,
+                            -1002146211959,
+                        ]:
+                            if left == 20:
+                                break
+                            if not await is_active_chat(chat_id):
+                                try:
+                                    await client.leave_chat(chat_id)
+                                    left += 1
+                                except:
+                                    continue
+            except:
+                pass
+        
         while not await asyncio.sleep(config.AUTO_LEAVE_ASSISTANT_TIME):
-            from YukkiMusic.core.userbot import assistants
-
+            tasks = []
             for num in assistants:
                 client = await get_client(num)
-                left = 0
-                try:
-                    async for i in client.get_dialogs():
-                        chat_type = i.chat.type
-                        if chat_type in [
-                            ChatType.SUPERGROUP,
-                            ChatType.GROUP,
-                            ChatType.CHANNEL,
-                        ]:
-                            chat_id = i.chat.id
-                            if chat_id not in [
-                                config.LOG_GROUP_ID,
-                                -1002159045835,
-                                -1002146211959,
-                            ]:
-                                if left == 20:
-                                    continue
-                                if not await is_active_chat(chat_id):
-                                    try:
-                                        await client.leave_chat(chat_id)
-                                        left += 1
-                                    except:
-                                        continue
-                except:
-                    pass
+                tasks.append(leave_inactive_chats(client))
+            
+            # Using asyncio.gather for running the leave_inactive_chats and same time for all assistant 
+            await asyncio.gather(*tasks)
 
 
 async def auto_end():
