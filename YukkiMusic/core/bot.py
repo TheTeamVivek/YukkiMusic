2c165123cacc0331da177337e133f4f2f3bc52f9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-03 17:06:35 +0530

    let's see

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -110,6 +110,7 @@ class YukkiBot(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group. Make sure that you have added your bot to your log channel and promoted as admin!"
             )
+            LOGGER(__name__).error("An error occurred", exc_info=True)
             sys.exit()
         if config.SET_CMDS == str(True):
             try:
