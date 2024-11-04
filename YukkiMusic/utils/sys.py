0b9155593b69986d8a61981e82e9d14fc70af010
~ Ksᴋ <123794242+KSKOP69@users.noreply.github.com>
2024-11-04 12:15:05 +0530

    Improvements (#16)

--- YukkiMusic/utils/sys.py
+++ YukkiMusic/utils/sys.py
@@ -18,11 +18,8 @@ from .formatters import get_readable_time
 
 async def bot_sys_stats():
     bot_uptime = int(time.time() - _boot_)
-    cpu = psutil.cpu_percent(interval=0.5)
-    mem = psutil.virtual_memory().percent
-    disk = psutil.disk_usage("/").percent
-    UP = f"{get_readable_time((bot_uptime))}"
-    CPU = f"{cpu}%"
-    RAM = f"{mem}%"
-    DISK = f"{disk}%"
+    UP = f"{get_readable_time(bot_uptime)}"
+    CPU = f"{psutil.cpu_percent(interval=0.5)}%"
+    RAM = f"{psutil.virtual_memory().percent}%"
+    DISK = f"{psutil.disk_usage('/').percent}%"
     return UP, CPU, RAM, DISK
