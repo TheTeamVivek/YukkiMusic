5cefc661808621347f05b5a9e111f8329dd923e3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-11-18 16:58:39 +0530

    Don't use

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -40,7 +40,6 @@ class YukkiBot(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
             sleep_threshold=240,
             max_concurrent_transmissions=5,
             workers=50,
