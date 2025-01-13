ec95cc6bdee1c5fff13d2444cdb242c3c8b875e3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-01-13 10:18:06 +0530

    Disabling Extra Plugins for some time

--- YukkiMusic/plugins/__init__.py
+++ YukkiMusic/plugins/__init__.py
@@ -24,7 +24,8 @@ logger = LOGGER(__name__)
 
 ROOT_DIR = abspath(join(dirname(__file__), "..", ".."))
 EXTERNAL_REPO_PATH = join(ROOT_DIR, EXTRA_PLUGINS_FOLDER)
-extra_plugins_enabled = EXTRA_PLUGINS.lower() == "true"
+# extra_plugins_enabled = EXTRA_PLUGINS.lower() == "true"
+extra_plugins_enabled = False
 
 if extra_plugins_enabled:
     if not os.path.exists(EXTERNAL_REPO_PATH):
