e50bc5f5500dcc393ec6ae3d924cbed5973e3c7f
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-01-30 11:29:02 +0530

    Update youtube.py
    Signed-off-by: Viyom <151810083+ViYomX@users.noreply.github.com>

--- YukkiMusic/core/youtube.py
+++ YukkiMusic/core/youtube.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass, field
+import asyncio
 
+from config import cookies
+from yt_dlp import YoutubeDL
+from youtubesearchpython.__future__ import VideosSearch
 from YukkiMusic.utils.formatters import seconds_to_min, time_to_seconds
-
+from YukkiMusic.decorators.asyncify import asyncify
 
 @dataclass
 class Track:
@@ -18,6 +22,48 @@ class Track:
         elif self.duration_sec is not None and self.duration_min is None:
             self.duration_min = seconds_to_min(self.duration_sec)
 
-
 class YouTube:
-    pass
+    def __init__(self, query):
+        self.query = query
+
+    async def search(self) -> Track:
+        try:
+            results = VideosSearch(self.query, limit=1)
+            for result in (await results.next())["result"]:
+                return Track(
+                    title=result["title"],
+                    vidid=result["id"],
+                    link=result["link"],
+                    duration_min=int(result["duration"])
+                    if result["duration"]
+                    else None,
+                    duration_sec=None,
+                    thumb=result["thumbnails"][0]["url"].split("?")[0],
+                )
+        except Exception:
+            return await self._search_yt_dlp()
+
+    @asyncify
+    def _search_yt_dlp(self) -> Track:
+        options = {
+            "format": "best",
+            "noplaylist": True,
+            "quiet": True,
+            "extract_flat": "in_playlist",
+            "cookiefile": cookies(),
+        }
+
+        with YoutubeDL(options) as ydl:
+            info_dict = ydl.extract_info(f"ytsearch: {self.query}", download=False)
+            details = info_dict.get("entries", [None])[0]
+            if not details:
+                raise ValueError("No results found.")
+
+            return Track(
+                title=details["title"],
+                vidid=details["id"],
+                link=details["url"],
+                duration_min=None,
+                duration_sec=details["duration"] if details["duration"] != 0 else None,
+                thumb=details["thumbnails"][0]["url"],
+            )
