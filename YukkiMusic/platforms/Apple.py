88dbe87a6a34be6a14b644ddc10937c29340bc1d
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-01-30 12:23:55 +0530

    Update Apple.py

--- YukkiMusic/platforms/Apple.py
+++ YukkiMusic/platforms/Apple.py
@@ -15,6 +15,7 @@ from bs4 import BeautifulSoup
 from youtubesearchpython.__future__ import VideosSearch
 
 from ..core.request import Request
+from ..core.youtube import YouTube, Track
 from .base import PlatformBase
 
 
@@ -27,7 +28,7 @@ class Apple(PlatformBase):
         return bool(re.search(self.regex, link))
 
     @alru_cache(maxsize=None)
-    async def track(self, url: str):
+    async def track(self, url: str) -> Track| bool:
         html = await Request.get_text(url)
         soup = BeautifulSoup(html, "html.parser")
         search = None
@@ -36,21 +37,9 @@ class Apple(PlatformBase):
                 search = tag.get("content", None)
         if search is None:
             return False
-        results = VideosSearch(search, limit=1)
-        for result in (await results.next())["result"]:
-            title = result["title"]
-            ytlink = result["link"]
-            vidid = result["id"]
-            duration_min = result["duration"]
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        track_details = {
-            "title": title,
-            "link": ytlink,
-            "vidid": vidid,
-            "duration_min": duration_min,
-            "thumb": thumbnail,
-        }
-        return track_details, vidid
+        youtube = YouTube(search)
+        return await youtube.search()
+        
 
     @alru_cache(maxsize=None)
     async def playlist(self, url, playid: bool | str = None):
