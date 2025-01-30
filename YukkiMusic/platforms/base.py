0a766e6080fa3389d39e4c4ebe4c6a1e0dcef949
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-01-30 13:29:32 +0530

    Update base.py

--- YukkiMusic/platforms/base.py
+++ YukkiMusic/platforms/base.py
@@ -5,23 +5,6 @@ from YukkiMusic.utils.formatters import time_to_seconds
 from ..core.enum import SourceType
 from ..core.youtube import Track
 
-class TrackDetails:
-    def __init__(self, track_info: dict, type: SourceType):
-        self.title = track_info.get("title")
-        self.link = track_info.get("link")
-        self.vidid = track_info.get("vidid")
-        self.duration_min = track_info.get("duration_min")
-        self.duration_sec = track_info.get("duration_sec", None)
-        self.thumb = track_info.get("thumb")
-
-        try:
-            if self.duration_sec is None and self.duration_min is not None:
-                self.duration_sec = time_to_seconds(self.duration_min)
-        except ValueError:
-            self.duration_sec = 0
-        self.type = type
-
-
 class PlatformBase(ABC):
 
     @abstractmethod
