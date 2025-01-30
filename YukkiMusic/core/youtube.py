b6f0727c20ffb913506601a1f10829de92e2e1c9
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-01-30 12:42:14 +0530

    Update youtube.py

--- YukkiMusic/core/youtube.py
+++ YukkiMusic/core/youtube.py
@@ -20,6 +20,8 @@ class Track:
     duration_sec: int | None = field(default=None)
 
     def __post_init__(self):
+        if "&" in self.link:
+            self.link = self.link.split("&")[0]
         if self.duration_min is not None and self.duration_sec is None:
             self.duration_sec = time_to_seconds(self.duration_min)
         elif self.duration_sec is not None and self.duration_min is None:
