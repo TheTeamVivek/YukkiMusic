55f2a719d9ede7861b504d686126718f4956729b
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-03 22:42:15 +0530

    Update inlinequery.py

--- YukkiMusic/utils/inlinequery.py
+++ YukkiMusic/utils/inlinequery.py
@@ -7,94 +7,88 @@
 #
 # All rights reserved.
 
+from telethon.tl.types import InputBotInlineResult
+from telethon.tl.types import InputBotInlineMessageText
+from telethon.tl.types import InputWebDocument
+from telethon.tl.types import DocumentAttributeImageSize
 from uuid import uuid4
 
-from telethon.tl.types import (
-    DocumentAttributeImageSize,
-    InputBotInlineMessageText,
-    InputBotInlineResult,
-    InputWebDocument,
-)
+from YukkiMusic import tbot
+from telethon.tl.custom.inlinebuilder import InlineBuilder
 
+builder = InlineBuilder(tbot)
 
-def article(title, description, thumb_url, input_message_content):
-    return InputBotInlineResult(
-        id=str(uuid4()),
-        type="article",
-        send_message=InputBotInlineMessageText(message=input_message_content),
-        title=title,
-        description=description,
-        thumb=InputWebDocument(
-            url=thumb_url,
-            size=0,
-            mime_type="image/jpeg",
-            attributes=[DocumentAttributeImageSize(w=0, h=0)],
-        ),
+def thumb(url):
+    return InputWebDocument(
+        url=url,
+        size=0,
+        mime_type="image/jpeg",
+        attributes=[DocumentAttributeImageSize(w=0, h=0)],
     )
-
-
+    
 answer = []
 
 answer.extend(
     [
-        article(
+        builder.article(
             title="Pause Stream",
             description="Pause the current playing song on voice chat.",
-            thumb_url="https://telegra.ph/file/c0a1c789def7b93f13745.png",
-            input_message_content="/pause",
+            thumb=thumb("https://telegra.ph/file/c0a1c789def7b93f13745.png"),
+            text="/pause",
         ),
-        article(
+        builder.article(
             title="Resume Stream",
             description="Resume the paused song on voice chat.",
-            thumb_url="https://telegra.ph/file/02d1b7f967ca11404455a.png",
-            input_message_content="/resume",
+            thumb=thumb("https://telegra.ph/file/02d1b7f967ca11404455a.png"),
+            text="/resume",
         ),
-        article(
+        builder.article(
             title="Mute Stream",
             description="Mute the ongoing song on voice chat.",
-            thumb_url="https://telegra.ph/file/66516f2976cb6d87e20f9.png",
-            input_message_content="/vcmute",
+            thumb=thumb("https://telegra.ph/file/66516f2976cb6d87e20f9.png"),
+            text="/vcmute",
         ),
-        article(
+        builder.article(
             title="Unmute Stream",
             description="Unmute the ongoing song on voice chat.",
-            thumb_url="https://telegra.ph/file/3078794f9341ffd582e18.png",
-            input_message_content="/vcunmute",
+            thumb=thumb("https://telegra.ph/file/3078794f9341ffd582e18.png"),
+            text="/vcunmute",
         ),
-        article(
+        builder.article(
             title="Skip Stream",
             description=(
                 "Skip to next track. | Skip to next track. | "
                 "For specific track number: /skip [number]"
             ),
-            thumb_url="https://telegra.ph/file/98b88e52bc625903c7a2f.png",
-            input_message_content="/skip",
+            thumb=thumb("https://telegra.ph/file/98b88e52bc625903c7a2f.png"),
+            text="/skip",
         ),
-        article(
+        builder.article(
             title="End Stream",
             description="Stop the ongoing song on group voice chat.",
-            thumb_url="https://telegra.ph/file/d2eb03211baaba8838cc4.png",
-            input_message_content="/stop",
+            thumb=thumb("https://telegra.ph/file/d2eb03211baaba8838cc4.png"),
+            text="/stop",
         ),
-        article(
+        builder.article(
             title="Shuffle Stream",
             description="Shuffle the queued tracks list.",
-            thumb_url="https://telegra.ph/file/7f6aac5c6e27d41a4a269.png",
-            input_message_content="/shuffle",
+            thumb=thumb("https://telegra.ph/file/7f6aac5c6e27d41a4a269.png"),
+            text="/shuffle",
         ),
-        article(
+        builder.article(
             title="Seek Stream",
             description="Seek the ongoing stream to a specific duration.",
-            thumb_url="https://telegra.ph/file/cd25ec6f046aa8003cfee.png",
-            input_message_content="/seek 10",
+            thumb=thumb("https://telegra.ph/file/cd25ec6f046aa8003cfee.png"),
+            text="/seek 10",
         ),
-        article(
+        builder.article(
             title="Loop Stream",
             description=(
-                "Loop the current playing music. Usage: /loop [enable|disable]"
+                "Loop the current playing music. "
+                "Usage: /loop [enable|disable]"
             ),
-            thumb_url="https://telegra.ph/file/081c20ce2074ea3e9b952.png",
-            input_message_content="/loop 3",
+            thumb=thumb("https://telegra.ph/file/081c20ce2074ea3e9b952.png"),
+            text="/loop 3",
         ),
     ]
 )
