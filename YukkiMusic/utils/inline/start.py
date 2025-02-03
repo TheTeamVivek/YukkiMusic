279f39712a3213d37f8d5a816355a5185e56d273
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-03 12:55:41 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -8,89 +8,88 @@
 # All rights reserved.
 #
 
-from pyrogram.types import InlineKeyboardButton
+from telethon import Button
 
 from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
 from YukkiMusic import app
 
-
 def start_pannel(_):
     buttons = [
         [
-            InlineKeyboardButton(
+            Button.url(
                 text=_["S_B_1"],
                 url=f"https://t.me/{app.username}?start=help",
             ),
-            InlineKeyboardButton(text=_["S_B_2"], callback_data="settings_helper"),
+            Button.inline(text=_["S_B_2"], data="settings_helper"),
         ],
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+                Button.url(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+                Button.url(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
             ]
         )
     else:
         if SUPPORT_CHANNEL:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+                [Button.url(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
             )
         if SUPPORT_GROUP:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+                [Button.url(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
             )
     return buttons
 
 
-def private_panel(_, bot_username, owner: bool | int = None):
+def private_panel(_,  owner: bool | int = None):
     buttons = [
-        [InlineKeyboardButton(text=_["S_B_8"], callback_data="settings_back_helper")]
+        [Button.inline(text=_["S_B_8"], data="settings_back_helper")]
     ]
     if SUPPORT_CHANNEL and SUPPORT_GROUP:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
-                InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
+                Button.url(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}"),
+                Button.url(text=_["S_B_3"], url=f"{SUPPORT_GROUP}"),
             ]
         )
     else:
         if SUPPORT_CHANNEL:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
+                [Button.url(text=_["S_B_4"], url=f"{SUPPORT_CHANNEL}")]
             )
         if SUPPORT_GROUP:
             buttons.append(
-                [InlineKeyboardButton(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
+                [Button.url(text=_["S_B_3"], url=f"{SUPPORT_GROUP}")]
             )
     buttons.append(
         [
-            InlineKeyboardButton(
+            Button.url(
                 text=_["S_B_5"],
-                url=f"https://t.me/{bot_username}?startgroup=true",
+                url=f"https://t.me/{tbot.username}?startgroup=true",
             )
         ]
     )
     if GITHUB_REPO and owner:
         buttons.append(
             [
-                InlineKeyboardButton(text=_["S_B_7"], user_id=owner),
-                InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                Button.url(text=_["S_B_7"], url=f"tg://user?id={owner}"),
+                Button.url(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
             ]
         )
     else:
         if GITHUB_REPO:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
+                    Button.url(text=_["S_B_6"], url=f"{GITHUB_REPO}"),
                 ]
             )
 
         if owner:
             buttons.append(
                 [
-                    InlineKeyboardButton(text=_["S_B_7"], user_id=owner),
+                    Button.url(text=_["S_B_7"], url=f"tg://user?id={owner}"),
                 ]
             )
-    buttons.append([InlineKeyboardButton(text=_["ST_B_6"], callback_data="LG")])
+    buttons.append([Button.inline(text=_["ST_B_6"], data="LG")])
     return buttons
