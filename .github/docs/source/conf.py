b126a7399a2e63d932ae61619e63e04270f6c53e
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-05 07:23:35 +0530

    Update conf.py
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- .github/docs/source/conf.py
+++ .github/docs/source/conf.py
@@ -70,4 +70,5 @@ html_context = {
     "github_repo": "YukkiMusic",
     "github_version": "dev",
     "conf_py_path": "/.github/docs/source/",
+    "copyright": '© Copyright 2024-2025, <a href="https://github.com/TheTeamVivek" target="_blank">TheTeamVivek</a>.'
 }
