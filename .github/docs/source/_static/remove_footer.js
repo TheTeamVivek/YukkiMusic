ab0a5e27a239e16b3431f5f3922e45ea24872285
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-05 07:02:40 +0530

    Update and rename remove_footer.js to remove_footer.css
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- .github/docs/source/_static/remove_footer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  // Wait a little for the theme_switcher.js to finish
-  setTimeout(function(){
-    // Remove the link(s) by matching their hrefs
-    $('footer a[href="https://github.com/MrDogeBro/sphinx_rtd_dark_mode"]').remove();
-    $('footer a[href="http://mrdogebro.com"]').remove();
-  }, 500);  // Adjust the delay (in milliseconds) if needed
-});
