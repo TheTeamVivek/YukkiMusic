a9e947af6354991d4e9e9453ffcb5f5ac15b3f0f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-05 06:33:20 +0530

    Delete .github/docs/source/_static/remove_footer.js
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- .github/docs/source/_static/remove_footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(() => {
-    const footerText = document.querySelector('footer a[href="https://github.com/MrDogeBro/sphinx_rtd_dark_mode"]');
-    if (footerText) {
-      footerText.parentNode.removeChild(footerText);
-    }
-  }, 300);
-});
