a852ee7dc918f8ecbb389e33d1c5db0fe170b81c
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-02-06 12:08:08 +0530

    Update conf.py
    Signed-off-by: Viyom <151810083+ViYomX@users.noreply.github.com>

--- .github/docs/source/conf.py
+++ .github/docs/source/conf.py
@@ -10,7 +10,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../.."))
+sys.path.insert(0, os.path.abspath("../../.."))
 
 project = "YukkiMusic"
 copyright = "2024-2025, TheTeamVivek"
