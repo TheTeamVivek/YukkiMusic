0dcf21f531398dcc0e833521f3fb1b25fc651dd9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-11 15:36:56 +0530

    convert in Telethon
    Signed-off-by: Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>

--- YukkiMusic/plugins/tools/active.py
+++ YukkiMusic/plugins/tools/active.py
@@ -7,11 +7,7 @@
 #
 # All rights reserved.
 #
-from pyrogram.errors import ChannelInvalid
-from pyrogram.types import Message
-
-from strings import command
-from YukkiMusic import app
+from YukkiMusic import tbot
 from YukkiMusic.misc import SUDOERS, db
 from YukkiMusic.utils.database.memorydatabase import (
     get_active_chats,
@@ -28,61 +24,64 @@ async def _clear_(chat_id):
     await remove_active_chat(chat_id)
 
 
-@app.on_message(command("ACTIVEVC_COMMAND") & SUDOERS)
-async def activevc(_, message: Message):
-    mystic = await message.reply_text("Getting Active Voicechats....\nPlease hold on")
+@tbot.on_message(command="ACTIVEVC_COMMAND" , from_users=list(SUDOERS))
+async def active_audio(event):
+    mystic = await event.reply("Getting Active Voicechats....\nPlease hold on")
     served_chats = await get_active_chats()
     text = ""
     j = 0
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
-                text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+            chat = await tbot.get_entity(x)
+            title = chat.title
+            if getattr(chat, "username"):
+                text += f"**{j + 1}.**  [{title}](https://t.me/{chat.username})[`{x}`]\n"
             else:
-                text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+                text += f"**{j + 1}. {title}** [`{x}`]\n"
             j += 1
-        except ChannelInvalid:
-            await _clear_(x)
+        except ValueError as e:
+            await tbot.handle_error(e) # Just for verifying the error
+            # await _clear_(x)
             continue
     if not text:
-        await mystic.edit_text("No active Chats Found")
+        await mystic.edit("No active Chats Found")
     else:
-        await mystic.edit_text(
+        await mystic.edit(
             f"**Active Voice Chat's:-**\n\n{text}",
-            disable_web_page_preview=True,
+            link_preview=False,
         )
 
 
-@app.on_message(command("ACTIVEVIDEO_COMMAND") & SUDOERS)
-async def activevi_(_, message: Message):
-    mystic = await message.reply_text("Getting Active Voicechats....\nPlease hold on")
+@tbot.on_message(command="ACTIVEVIDEO_COMMAND" , from_users=list(SUDOERS))
+async def active_video(event):
+    mystic = await event.reply("Getting Active Voicechats....\nPlease hold on")
     served_chats = await get_active_video_chats()
     text = ""
     j = 0
     for x in served_chats:
         try:
-            title = (await app.get_chat(x)).title
-            if (await app.get_chat(x)).username:
-                user = (await app.get_chat(x)).username
-                text += f"<b>{j + 1}.</b>  [{title}](https://t.me/{user})[`{x}`]\n"
+            chat = await tbot.get_entity(x)
+            title = chat.title
+            if getattr(chat, "username"):
+                text += f"**{j + 1}.** [{title}](https://t.me/{chat.username})[`{x}`]\n"
             else:
-                text += f"<b>{j + 1}. {title}</b> [`{x}`]\n"
+                text += f"**{j + 1}. {title}** [`{x}`]\n"
             j += 1
-        except ChannelInvalid:
-            await _clear_(x)
+        except ValueError:
+          #  await _clear_(x)
             continue
     if not text:
-        await mystic.edit_text("No active Chats Found")
+        await mystic.edit("No active Chats Found")
     else:
-        await mystic.edit_text(
+        await mystic.edit(
             f"**Active Video Chat's:-**\n\n{text}",
-            disable_web_page_preview=True,
+            link_preview=False,
         )
 
 
-@app.on_message(command("AC_COMMAND") & SUDOERS)
-async def vc(client, message: Message):
-    ac_audio = str(len(await get_active_chats()))
-    await message.reply_text(f"Active Chats info: {ac_audio}")
+@tbot.on_message(command="AC_COMMAND" , from_users=list(SUDOERS))
+async def ac_counts(event):
+    ac_audio = len(await get_active_chats())
+    ac_video = len(await get_active_video_chats())
+    total_audio = int(ac_audio - ac_video)
+    await event.reply(f"Active Chats info:\nAudio: {total_audio}\nVideo: {ac_video}")
