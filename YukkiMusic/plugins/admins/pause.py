bf3f22c492be1050d78eba184a9f0fd4373f9689
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-23 14:49:06 +0530

    Update pause.py

--- YukkiMusic/plugins/admins/pause.py
+++ YukkiMusic/plugins/admins/pause.py
@@ -19,13 +19,13 @@ from YukkiMusic.utils.database import is_music_playing, music_off
 from YukkiMusic.utils.decorators import admin_rights_check
 
 
-@app.on_message(command("PAUSE_COMMAND") & filters.group & ~BANNED_USERS)
+@tbot.on_message(flt.command("PAUSE_COMMAND") & flt.group & ~flt.user(BANNED_USERS))
 @admin_rights_check
-async def pause_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["COMMAND_USAGE_ERROR"])
+async def pause_admin(event, _, chat_id):
+    if not len(event.text.split()) == 1:
+        return await event.reply(_["COMMAND_USAGE_ERROR"])
     if not await is_music_playing(chat_id):
-        return await message.reply_text(_["admin_1"])
+        return await event.reply(_["admin_1"])
     await music_off(chat_id)
     await Yukki.pause_stream(chat_id)
-    await message.reply_text(_["admin_2"].format(message.from_user.mention))
+    await event.reply(_["admin_2"].format((await event.get_sender()).first_name))
