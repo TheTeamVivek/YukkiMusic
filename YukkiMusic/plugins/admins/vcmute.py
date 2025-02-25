ed119267e6a66d9570f39d22e2f45f3399588990
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-02-25 13:03:57 +0530

    Update vcmute.py

--- YukkiMusic/plugins/admins/vcmute.py
+++ YukkiMusic/plugins/admins/vcmute.py
@@ -13,35 +13,37 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import command
-from YukkiMusic import app
+from YukkiMusic import app, tbot
 from YukkiMusic.core.call import Yukki
 from YukkiMusic.utils.database import is_muted, mute_off, mute_on
 from YukkiMusic.utils.decorators import admin_rights_check
 
 
-@app.on_message(command("MUTE_COMMAND") & filters.group & ~BANNED_USERS)
+@tbot.on_message(flt.command("MUTE_COMMAND") & flt.group & ~flt.user(BANNED_USERS))
 @admin_rights_check
-async def mute_admin(cli, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
+async def mute_admin(event, _, chat_id):
+    if not len(event.text.split()) == 1 or event.reply_to:
         return
     if await is_muted(chat_id):
-        return await message.reply_text(_["admin_5"], link_preview=False)
+        return await event.reply(_["admin_5"], link_preview=False)
     await mute_on(chat_id)
     await Yukki.mute_stream(chat_id)
-    await message.reply_text(
-        _["admin_6"].format(message.from_user.mention), link_preview=False
+    mention= await tbot.create_mention(await event.get_sender())
+    await event.reply(
+        _["admin_6"].format(mention), link_preview=False
     )
 
 
-@app.on_message(command("UNMUTE_COMMAND") & filters.group & ~BANNED_USERS)
+@tbot.on_message(flt.command("UNMUTE_COMMAND") & flt.group & ~flt.user(BANNED_USERS))
 @admin_rights_check
 async def unmute_admin(Client, message: Message, _, chat_id):
-    if not len(message.command) == 1 or message.reply_to_message:
+    if not len(event.text.split()) == 1 or event.reply_to:
         return
     if not await is_muted(chat_id):
-        return await message.reply_text(_["admin_7"], link_preview=False)
+        return await event.reply(_["admin_7"], link_preview=False)
     await mute_off(chat_id)
     await Yukki.unmute_stream(chat_id)
-    await message.reply_text(
-        _["admin_8"].format(message.from_user.mention), link_preview=False
+    mention= await tbot.create_mention(await event.get_sender())
+    await event.reply(
+        _["admin_8"].format(mention), link_preview=False
     )
