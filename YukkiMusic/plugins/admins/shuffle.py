cff8c9150c86cb919525beac6e7ec1b8453e0fd1
Viyom <151810083+ViYomX@users.noreply.github.com>
2025-02-25 12:58:30 +0530

    Update shuffle.py

--- YukkiMusic/plugins/admins/shuffle.py
+++ YukkiMusic/plugins/admins/shuffle.py
@@ -15,27 +15,28 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import command
-from YukkiMusic import app
+from YukkiMusic import app, tbot
 from YukkiMusic.misc import db
 from YukkiMusic.utils.decorators import admin_rights_check
 
 
-@app.on_message(command("SHUFFLE_COMMAND") & filters.group & ~BANNED_USERS)
+@tbot.on_message(flt.command("SHUFFLE_COMMAND", True) & flt.group & ~flt.user(BANNED_USERS))
 @admin_rights_check
-async def admins(Client, message: Message, _, chat_id):
-    if not len(message.command) == 1:
-        return await message.reply_text(_["COMMAND_USAGE_ERROR"])
+async def admins(event_, chat_id):
+    if not len(event.text.split()) == 1:
+        return await event.reply(_["COMMAND_USAGE_ERROR"])
     check = db.get(chat_id)
     if not check:
-        return await message.reply_text(_["admin_21"])
+        return await event.reply(_["admin_21"])
     try:
         popped = check.pop(0)
     except Exception:
-        return await message.reply_text(_["admin_22"])
+        return await event.reply(_["admin_22"])
     check = db.get(chat_id)
     if not check:
         check.insert(0, popped)
-        return await message.reply_text(_["admin_22"])
+        return await event.reply(_["admin_22"])
     random.shuffle(check)
     check.insert(0, popped)
-    await message.reply_text(_["admin_23"].format(message.from_user.mention))
+    mention = await tbot.create_mention(await event.get_sender())
+    await event.reply(_["admin_23"].format(mention))
