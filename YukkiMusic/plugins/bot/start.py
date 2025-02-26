8c081d98d9b56b23c2743b3a1c54162c86bb92b4
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-02-26 22:06:21 +0530

    Update start.py

--- YukkiMusic/plugins/bot/start.py
+++ YukkiMusic/plugins/bot/start.py
@@ -15,11 +15,13 @@ from pyrogram.enums import ChatType, ParseMode
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 from youtubesearchpython.__future__ import VideosSearch
 
+from telethon import Button, events
 import config
 from config import BANNED_USERS, START_IMG_URL
 from config.config import OWNER_ID
 from strings import command, get_string
-from YukkiMusic import Platform, app
+from YukkiMusic import Platform, tbot
+from YukkiMusic.platforms import youtube, telegram
 from YukkiMusic.misc import SUDOERS, _boot_
 from YukkiMusic.plugins.bot.help import paginate_modules
 from YukkiMusic.plugins.play.playlist import del_plist_msg
@@ -42,45 +44,45 @@ from YukkiMusic.utils.inline import private_panel, start_pannel
 loop = asyncio.get_running_loop()
 
 
-@app.on_message(command("START_COMMAND") & filters.private & ~BANNED_USERS)
+@tbot.on_message(flt.command("START_COMMAND", True) & flt.private & ~flt.user(BANNED_USERS))
 @language(no_check=True)
-async def start_comm(client, message: Message, _):
-    chat_id = message.chat.id
-    await add_served_user(message.from_user.id)
-    if len(message.text.split()) > 1:
-        name = message.text.split(None, 1)[1]
+async def start_comm(event, _):
+    chat_id = event.chat_id
+    await add_served_user(event.sender_id)
+    if len(event.text.split()) > 1:
+        name = event.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = await paginate_modules(0, chat_id, close=True)
 
             if config.START_IMG_URL:
-                return await message.reply_photo(
-                    photo=START_IMG_URL,
-                    caption=_["help_1"],
+                return await event.reply(
+                    file=START_IMG_URL,
+                    message=_["help_1"],
                     buttons=keyboard,
                 )
             else:
-                return await message.reply_text(
-                    text=_["help_1"],
+                return await event.reply(
+                    message=_["help_1"],
                     buttons=keyboard,
                 )
         if name[0:4] == "song":
-            await message.reply_text(_["song_2"])
+            await event.reply(_["song_2"])
             return
         if name == "mkdwn_help":
             await message.reply(
                 MARKDOWN,
-                parse_mode=ParseMode.HTML,
+                parse_mode="HTML",
                 link_preview=False,
             )
         if name == "greetings":
             await message.reply(
                 WELCOMEHELP,
-                parse_mode=ParseMode.HTML,
+                parse_mode="HTML",
                 link_preview=False,
             )
         if name[0:3] == "sta":
-            m = await message.reply_text("🔎 Fetching Your personal stats.!")
-            stats = await get_userss(message.from_user.id)
+            m = await event.reply("🔎 Fetching Your personal stats.!")
+            stats = await get_userss(event.sender_id)
             tot = len(stats)
             if not stats:
                 await asyncio.sleep(1)
@@ -123,22 +125,21 @@ async def start_comm(client, message: Message, _):
             try:
                 videoid, msg = await loop.run_in_executor(None, get_stats)
             except Exception as e:
-                print(e)
                 return
-            thumbnail = await Platform.youtube.thumbnail(videoid, True)
+            thumbnail = await youtube.thumbnail(videoid, True)
             await m.delete()
-            await message.reply_photo(photo=thumbnail, caption=msg)
+            await event.reply(file=thumbnail, message=msg)
             return
         if name[0:3] == "sud":
-            await sudoers_list(client=client, message=message, _=_)
+            await sudoers_list(client=event.client, message=event, _=_)
             await asyncio.sleep(1)
             if await is_on_off(config.LOG):
-                sender_id = message.from_user.id
-                message.from_user.mention
-                sender_name = message.from_user.first_name
-                return await app.send_message(
+            	sender = await event.get_sender()
+                sender_id = sender.id
+                sender_name = sender.first_name
+                return await tbot.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} Has just started bot to check <code>Sudolist </code>\n\n**User Id:** {sender_id}\n**User Name:** {sender_name}",
+                    f"{await tbot.create_mention(sender)} Has just started bot to check `Sudolist`\n\n**User Id:** {sender_id}\n**User Name:** {sender_name}",
                 )
             return
         if name[0:3] == "lyr":
@@ -146,17 +147,17 @@ async def start_comm(client, message: Message, _):
             lyrical = config.lyrical
             lyrics = lyrical.get(query)
             if lyrics:
-                async for chunk_msg in Platform.telegram.split_text(lyrics):
-                    await message.reply_text(chunk_msg)
+                async for chunk_msg in telegram.split_text(lyrics):
+                    await event.reply(chunk_msg)
                 return
             else:
-                await message.reply_text("Failed to get lyrics ")
+                await event.reply("Failed to get lyrics ")
                 return
         if name[0:3] == "del":
-            await del_plist_msg(client=client, message=message, _=_)
+            await del_plist_msg(client=event.client, message=event, _=_)
             await asyncio.sleep(1)
         if name[0:3] == "inf":
-            m = await message.reply_text("🔎 Fetching info..")
+            m = await event.reply("🔎 Fetching info..")
             query = (str(name)).replace("info_", "", 1)
             query = f"https://www.youtube.com/watch?v={query}"
             results = VideosSearch(query, limit=1)
@@ -181,117 +182,113 @@ async def start_comm(client, message: Message, _):
 📎**Channel Link:** [Visit from here]({channellink})
 🔗**Videp linl:** [Link]({link})
 """
-            key = InlineKeyboardMarkup(
-                [
+            key =   [
                     [
-                        InlineKeyboardButton(text="🎥 Watch ", url=f"{link}"),
-                        InlineKeyboardButton(text="🔄 Close", callback_data="close"),
+                        Button.url(text="🎥 Watch ", url=f"{link}"),
+                        Button.inline(text="🔄 Close", data="close"),
                     ],
                 ]
-            )
             await m.delete()
-            await app.send_photo(
-                message.chat.id,
-                photo=thumbnail,
-                caption=searched_text,
-                parse_mode=ParseMode.MARKDOWN,
+            await event.respond(
+                file=thumbnail,
+                message=searched_text,
                 buttons=key,
             )
             await asyncio.sleep(1)
             if await is_on_off(config.LOG):
-                sender_id = message.from_user.id
-                sender_name = message.from_user.first_name
-                return await app.send_message(
+            	sender = await event.get_sender()
+                sender_id = event.sender_id
+                sender_name = sender.first_name
+                return await tbot.send_message(
                     config.LOG_GROUP_ID,
-                    f"{message.from_user.mention} Has just started bot ot check <code> Video information  </code>\n\n**User Id:** {sender_id}\n**User Name** {sender_name}",
+                    f"{await tbot.create_mention(sender)} Has just started bot ot check `Video information `\n\n**User Id:** {sender_id}\n**User Name** {sender_name}",
                 )
     else:
         try:
-            await app.resolve_peer(OWNER_ID[0])
+            await tbot.get_entity(OWNER_ID[0])
             OWNER = OWNER_ID[0]
         except Exception:
             OWNER = None
         out = private_panel(_, OWNER)
         if config.START_IMG_URL:
             try:
-                await message.reply_photo(
-                    photo=config.START_IMG_URL,
-                    caption=_["start_1"].format(app.mention),
-                    buttons=InlineKeyboardMarkup(out),
+                await event.reply(
+                    file=config.START_IMG_URL,
+                    message=_["start_1"].format(tbot.mention),
+                    buttons=out,
                 )
             except Exception:
-                await message.reply_text(
-                    text=_["start_1"].format(app.mention),
-                    buttons=InlineKeyboardMarkup(out),
+                await event.reply(
+                    message=_["start_1"].format(tbot.mention),
+                    buttons=out,
                 )
         else:
-            await message.reply_text(
-                text=_["start_1"].format(app.mention),
-                buttons=InlineKeyboardMarkup(out),
+            await event.reply(
+                message=_["start_1"].format(tbot.mention),
+                buttons=out,
             )
         if await is_on_off(config.LOG):
-            sender_id = message.from_user.id
-            sender_name = message.from_user.first_name
-            return await app.send_message(
+            sender_id = event.sender_id
+            sender = await event.get_sender()
+            sender_name = sender.first_name
+            return await tbot.send_message(
                 config.LOG_GROUP_ID,
-                f"{message.from_user.mention} Has started bot. \n\n**User id :** {sender_id}\n**User name:** {sender_name}",
+                f"{await tbot.create_mention(sender)} Has started bot. \n\n**User id :** {sender_id}\n**User name:** {sender_name}",
             )
 
 
-@app.on_message(command("START_COMMAND") & filters.group & ~BANNED_USERS)
+@tbot.on_message(flt.command("START_COMMAND", True) & flt.group & ~flt.user(BANNED_USERS))
 @language(no_check=True)
-async def testbot(client, message: Message, _):
+async def testbot(event , _):
     uptime = int(time.time() - _boot_)
-    message.chat.id
-    await message.reply_text(_["start_7"].format(get_readable_time(uptime)))
-
-    return await add_served_chat(message.chat.id)
+    await event.reply(_["start_7"].format(get_readable_time(uptime)))
+    return await add_served_chat(event.chat_id)
 
 
-@app.on_message(filters.new_chat_members, group=-1)
+@tbot.on_message(filters.new_chat_members, group=-1) # Not completed yet
 async def welcome(client, message: Message):
-    chat_id = message.chat.id
+    chat_id = event.chat_id
     if config.PRIVATE_BOT_MODE == str(True):
-        if not await is_served_private_chat(message.chat.id):
-            await message.reply_text(
+        if not await is_served_private_chat(event.chat_id):
+            await event.reply(
                 "This Bot's private mode has been enabled only my owner can use this if want to use in your chat so say my Owner to authorize your chat."
             )
-            return await app.leave_chat(message.chat.id)
+            return await tbot.leave_chat(event.chat_id)
     else:
         await add_served_chat(chat_id)
     for member in message.new_chat_members:
         try:
-            language = await get_lang(message.chat.id)
+            language = await get_lang(event.chat_id)
             _ = get_string(language)
-            if member.id == app.id:
+            if member.id == tbot.id:
                 chat_type = message.chat.type
                 if chat_type != ChatType.SUPERGROUP:
-                    await message.reply_text(_["start_5"])
-                    return await app.leave_chat(message.chat.id)
+                    await event.reply(_["start_5"])
+                    return await tbot.leave_chat(event.chat_id)
                 if chat_id in await blacklisted_chats():
-                    await message.reply_text(
+                    await event.reply(
                         _["start_6"].format(
-                            f"https://t.me/{app.username}?start=sudolist"
+                            f"https://t.me/{tbot.username}?start=sudolist"
                         )
                     )
-                    return await app.leave_chat(chat_id)
-                userbot = await get_assistant(message.chat.id)
+                    return await tbot.leave_chat(chat_id)
+                userbot = await get_assistant(event.chat_id)
                 out = start_pannel(_)
-                await message.reply_text(
+                await event.reply(
                     _["start_2"].format(
-                        app.mention,
+                        tbot.mention,
                         userbot.username,
                         userbot.id,
                     ),
-                    buttons=InlineKeyboardMarkup(out),
+                    buttons=out,
                 )
             if member.id in config.OWNER_ID:
-                return await message.reply_text(
-                    _["start_3"].format(app.mention, member.mention)
+                return await event.reply(
+                    _["start_3"].format(tbot.mention, member.mention)
                 )
             if member.id in SUDOERS:
-                return await message.reply_text(
-                    _["start_4"].format(app.mention, member.mention)
+                return await event.reply(
+                    _["start_4"].format(tbot.mention, member.mention)
                 )
             return
         except Exception:
