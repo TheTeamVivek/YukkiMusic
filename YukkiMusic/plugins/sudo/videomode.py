ce6b7ca711b3c260b652d87e3aab0a36b3c2adb7
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-01 14:23:51 +0530

    Update videomode.py

--- YukkiMusic/plugins/sudo/videomode.py
+++ YukkiMusic/plugins/sudo/videomode.py
@@ -12,19 +12,19 @@ from pyrogram.types import Message
 
 import config
 from strings import command
-from YukkiMusic import app
+from YukkiMusic import app, tbot
 from YukkiMusic.misc import SUDOERS
 from YukkiMusic.utils.database import add_off, add_on
 from YukkiMusic.utils.decorators.language import language
 
 
-@app.on_message(command("VIDEOMODE_COMMAND") & SUDOERS)
+@tbot.on_message(flt.command("VIDEOMODE_COMMAND", True) & flt.user(SUDOERS))
 @language
-async def videoloaymode(client, message: Message, _):
+async def videoloaymode(event, _):
     usage = _["vidmode_1"]
-    if len(message.command) != 2:
+    if len(event.text.split()) != 2:
         return await event.reply(usage)
-    state = message.text.split(None, 1)[1].strip()
+    state = event.text.split(None, 1)[1].strip()
     state = state.lower()
     if state == "download":
         await add_on(config.YTDOWNLOADER)
