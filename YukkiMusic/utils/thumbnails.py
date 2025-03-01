37cdcebe407a2939487dd22770bfb7ed9f804bcc
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-01 19:56:47 +0530

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -13,24 +13,10 @@ from youtubesearchpython.__future__ import VideosSearch
 
 
 @alru_cache(maxsize=None)
-async def gen_thumb(videoid):
-    try:
-        query = f"https://www.youtube.com/watch?v={videoid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception:
-        return f"https://img.youtube.com/vi/{videoid}/maxresdefault.jpg"
+async def gen_thumb(videoid, thumb_url):
+    return thumb_url
 
 
 @alru_cache(maxsize=None)
-async def gen_qthumb(vidid):
-    try:
-        query = f"https://www.youtube.com/watch?v={vidid}"
-        results = VideosSearch(query, limit=1)
-        for result in (await results.next())["result"]:
-            thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-        return thumbnail
-    except Exception:
-        return f"https://img.youtube.com/vi/{vidid}/maxresdefault.jpg"
+async def gen_qthumb(vidid, thumb_url):
+    return thumb_url
