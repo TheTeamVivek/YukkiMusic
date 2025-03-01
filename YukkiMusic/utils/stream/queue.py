f83b897202bb4c6b24d29cc1b82bfb2de8dc05e8
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-01 19:39:09 +0530

    Update queue.py

--- YukkiMusic/utils/stream/queue.py
+++ YukkiMusic/utils/stream/queue.py
@@ -10,48 +10,25 @@
 
 
 from config import autoclean, chatstats, time_to_seconds, userstats
-from YukkiMusic.core.youtube import Track  # noqa
+from YukkiMusic.core.youtube import Track 
 from YukkiMusic.misc import db
 
 
 async def put_queue(
     chat_id,
     original_chat_id,
-    file,
-    title,
-    duration,
-    user,
-    vidid,
     user_id,
-    stream,
-    url: str = None,
-    forceplay: bool | str = None,
+    track: Track,
+    video: bool = False,
+    is_index: bool = False,
+    forceplay: bool | None = None,
 ):
-    # async def put_queue(
-    #     chat_id,
-    #     original_chat_id,
-    #     user_id,
-    #     track: Track,
-    #     video: bool = False
-    #     forceplay: bool | None = None,
-    # ):
 
-    title = title.title()
-    try:
-        duration_in_seconds = time_to_seconds(duration) - 3
-    except Exception:
-        duration_in_seconds = 0
+    track.title = track.title.title()
     put = {
-        "title": title,
-        "dur": duration,
-        "streamtype": stream,
-        "by": user,
         "chat_id": original_chat_id,
-        "file": file,
-        "vidid": vidid,
-        "seconds": duration_in_seconds,
+        "track": track,
         "played": 0,
-        "url": url,
     }
     if forceplay:
         if check := db.get(chat_id):
@@ -62,52 +39,15 @@ async def put_queue(
     else:
         db[chat_id].append(put)
     autoclean.append(file)
-    vidid = "telegram" if vidid == "soundcloud" or "saavn" in vidid else vidid
 
-    to_append = {"vidid": vidid, "title": title}
-    if chat_id not in chatstats:
-        chatstats[chat_id] = []
-    chatstats[chat_id].append(to_append)
-    if user_id not in userstats:
-        userstats[user_id] = []
-    userstats[user_id].append(to_append)
-    return
+    if is_index is None:
+        vidid = track.vidid if track.is_youtube else track.vidid.value
 
-
-async def put_queue_index(
-    chat_id,
-    original_chat_id,
-    file,
-    title,
-    duration,
-    user,
-    vidid,
-    stream,
-    forceplay: bool | str = None,
-):
-    # async def put_queue(
-    #     chat_id,
-    #     original_chat_id,
-    #     user_id,
-    #     track: Track,
-    #     forceplay: bool | str = None,
-    # ):
-    put = {
-        "title": title,
-        "dur": duration,
-        "streamtype": stream,
-        "by": user,
-        "chat_id": original_chat_id,
-        "file": file,
-        "vidid": vidid,
-        "seconds": 0,
-        "played": 0,
-    }
-    if forceplay:
-        if check := db.get(chat_id):
-            check.insert(0, put)
-        else:
-            db[chat_id] = []
-            db[chat_id].append(put)
-    else:
-        db[chat_id].append(put)
+        to_append = {"vidid": vidid, "title": title}
+        if chat_id not in chatstats:
+            chatstats[chat_id] = []
+        chatstats[chat_id].append(to_append)
+        if user_id not in userstats:
+            userstats[user_id] = []
+        userstats[user_id].append(to_append)
+        return
