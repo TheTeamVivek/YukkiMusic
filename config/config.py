3be94452c187228eaf6ff7960484d527e8da1aff
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-02 16:53:10 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -121,7 +121,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/TheTeamVivek/YukkiMusic")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/Vivekkumar-IN/YukkiMusic")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
