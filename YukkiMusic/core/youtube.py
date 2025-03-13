9be94cb921fb93c97f2d0c9a85a9afaa876cb819
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-13 16:36:47 +0530

    Update youtube.py

--- YukkiMusic/core/youtube.py
+++ YukkiMusic/core/youtube.py
@@ -14,7 +14,6 @@ from YukkiMusic.utils.formatters import time_to_seconds
 
 from .enum import SourceType
 
-
 @dataclass
 class Track:
     title: str
@@ -23,21 +22,23 @@ class Track:
     duration: int  # duration in seconds
     streamtype: SourceType
     video: bool  # The song is audio or video
-    # by: str | None = None  # None but required
+        
     download_url: str | None = (
         None  # If provided directly used to download instead self.link
     )
-
     is_live: bool | None = None
     vidid: str | None = None
     file_path: str | None = None
 
     def __post_init__(self):
-        if self.is_youtube:
+        self.download_url = self.download_url if self.download_url else self.link
+        if self.is_youtube and self.vidid is None:
             pattern = r"(?:v=|\/)([0-9A-Za-z_-]{11})"
-            url = self.download_url if self.download_url else self.link
-            p_match = re.search(pattern, url)
+            p_match = re.search(pattern, self.download_url)
             self.vidid = p_match.group(1)
+        else:
+            self.vidid = ""
+            
         self.title = self.title.title() if self.title is not None else None
         if (
             not self.duration and self.is_live is None
@@ -47,9 +48,8 @@ class Track:
                 SourceType.RESSO,
                 SourceType.SPOTIFY,
                 SourceType.YOUTUBE,
-                None,
-            ]:  # NONE BEACUSE IN THESE PLATFORMS THE streamtype and been provided by later
-                self.is_live = True  # MAY BE I DON'T KNOW CORRECTLY
+            ]:
+                self.is_live = True
 
     async def is_exists(self):
         exists = False
@@ -66,22 +66,20 @@ class Track:
 
     @property
     def is_youtube(self) -> bool:
-        url = self.download_url if self.download_url else self.link
-        return "youtube.com" in url or "youtu.be" in url
+        return "youtube.com" in self.download_url or "youtu.be" in self.download_url
 
     @property
     def is_m3u8(self) -> bool:
-        return bool(self.is_live and not self.is_youtube)
-        # return bool(self.is_live and not self.title and not self.duration)
-
-    async def download(self, options: dict | None = None):
-        url = self.download_url if self.download_url else self.link
-
+        return self.streamtype == SourceType.M3U8
+        
+        
+    async def download(self):
         if (
             self.file_path is not None and await self.is_exists()
-        ):  # THIS CONDITION FOR TELEGRAM FILES
+        ):  # THIS CONDITION FOR TELEGRAM FILES BECAUSE THESE FILES ARE ALREADY DOWNLOADED
             return self.file_path
-        if await is_on_off(YTDOWNLOADER) and not self.is_live:
+            
+        if await is_on_off(YTDOWNLOADER) and not (self.is_live or self.is_m3u8):
             ytdl_opts = {
                 "format": (
                     "(bestvideo[height<=?720][width<=?1280][ext=mp4])+(bestaudio[ext=m4a])"
@@ -101,32 +99,27 @@ class Track:
             if self.is_youtube:
                 ytdl_opts["cookiefile"] = cookies()
 
-            if options:
-                if isinstance(options, dict):
-                    ytdl_opts.update(options)
-                else:
-                    raise Exception(
-                        f"Expected 'options' to be a dict but got {type(options).__name__}"
-                    )
-
             @asyncify
             def _download():
                 with YoutubeDL(ytdl_opts) as ydl:
-                    info = ydl.extract_info(url, False)
+                    info = ydl.extract_info(self.download_url, False)
                     self.file_path = os.path.join(
                         "downloads", f"{info['id']}.{info['ext']}"
                     )
 
                     if not os.path.exists(self.file_path):
-                        ydl.download([url])
+                        ydl.download([self.download_url])
 
                     return self.file_path
 
             return await _download()
 
         else:
+            if self.is_m3u8:
+                return self.link or self.download_url
+                
             format_code = "b" if self.video else "bestaudio/best"  # Keep "b" not "best"
-            command = f'yt-dlp -g -f "{format_code}" {"--cookies " + cookies() if self.is_youtube else ""} "{url}"'
+            command = f'yt-dlp -g -f "{format_code}" {"--cookies " + cookies() if self.is_youtube else ""} "{self.download_url}"'
             process = await asyncio.create_subprocess_shell(
                 command,
                 stdout=asyncio.subprocess.PIPE,
@@ -135,17 +128,17 @@ class Track:
             stdout, stderr = await process.communicate()
 
             if stdout:
-                self.file_path = stdout.decode().strip()
+                self.file_path = stdout.decode('utf-8').strip().split()[0]
                 return self.file_path
             else:
-                raise Exception(f"Failed to get file path: {stderr.decode().strip()}")
+                raise Exception(f"Failed to get file path: {stderr.decode('utf-8').strip()}")
 
-    async def __call__(self, audio: bool = True):
+    async def __call__(self):
         return self.file_path or await self.download()
 
 
 @alru_cache(maxsize=None)
-async def search(query):
+async def search(query: str, video: bool = False):
     try:
         results = VideosSearch(query, limit=1)
         for result in (await results.next())["result"]:
@@ -158,8 +151,8 @@ async def search(query):
                     time_to_seconds(duration) if str(duration) != "None" else 0
                 ),  # TODO: CHECK THAT THE YOUTBE SEARCH PYTHON RETUNS DURATION IS None or "None"
                 thumb=result["thumbnails"][0]["url"].split("?")[0],
-                streamtype=None,
-                video=None,
+                streamtype=SourceType.YOUTUBE,
+                video=video,
             )
     except Exception:
         return await search_from_ytdlp(query)
@@ -167,7 +160,7 @@ async def search(query):
 
 @alru_cache(maxsize=None)
 @asyncify
-def search_from_ytdlp(query):
+def search_from_ytdlp(query: str, video: bool = False):
     options = {
         "format": "best",
         "noplaylist": True,
@@ -194,6 +187,6 @@ def search_from_ytdlp(query):
             download_url=details["webpage_url"],
             duration=details["duration"],
             thumb=details["thumbnails"][0]["url"],
-            streamtype=None,
-            video=None,
+            streamtype=SourceType.YOUTUBE, # KEEP HERE YOUTUBE LATER WE CAN CHANGE IT TO CORRECT SourceType
+            video=video,
         )
