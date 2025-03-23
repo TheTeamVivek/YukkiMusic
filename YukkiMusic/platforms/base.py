9a943e4f2f830a8d5c13f06bd2a43c047ebba0b9
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-23 12:45:21 +0530

    Update base.py

--- YukkiMusic/platforms/base.py
+++ YukkiMusic/platforms/base.py
@@ -4,19 +4,41 @@ from ..core.youtube import Track
 
 
 class PlatformBase(ABC):
-
     @abstractmethod
     async def valid(self, link: str) -> bool:
         """
-        Validates whether the given URL matches the expected format for this service.
+        Checks if the given link is a valid URL for this platform.
 
         Args:
-            url (str): The URL to validate.
+            link (str): The URL to validate.
 
         Returns:
-            bool: True if the URL is valid for this service, False otherwise.
+            bool: True if the URL is valid for this platform, False otherwise.
         """
 
     @abstractmethod
     async def track(self, url: str) -> Track:
-        pass
+        """
+        Retrieves a track instance from the given URL.
+
+        Args:
+            url (str): The URL of the track.
+
+        Returns:
+            Track: An instance of the Track class representing the track.
+        """
+
+    async def playlist(self, url: str) -> list[Track, str]:
+        """
+        Retrieves a playlist from the given URL.
+
+        Args:
+            url (str): The URL of the playlist.
+
+        Returns:
+            list[Track, str]: A list where:
+                - The first element is a `Track` instance representing the main track.
+                - The other element depends on the platform:
+                    - If the platform is YouTube, it will be the video ID (`vidid`).
+                    - For other streaming services, it will be the song name.
+        """
