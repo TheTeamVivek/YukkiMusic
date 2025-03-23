e6c99b08254c180e11f304059a85c7718f6de6f9
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-23 12:23:10 +0530

    Update queue.py

--- YukkiMusic/utils/inline/queue.py
+++ YukkiMusic/utils/inline/queue.py
@@ -49,7 +49,7 @@ def queue_markup(
             ),
         ],
     ]
-    upl = InlineKeyboardMarkup(not_dur if duration == "Unknown" else dur)
+    upl = InlineKeyboardMarkup(not_dur if duration == "UNKNOWN" else dur)
     return upl
 
 
