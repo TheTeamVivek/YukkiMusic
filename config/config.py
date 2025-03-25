601c1618c54fc0a69f5debd468f0552298baf16c
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-03-25 15:21:57 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -42,7 +42,7 @@ DURATION_LIMIT_MIN = int(
 )  # Remember to give value in Minutes
 
 
-EXTRA_PLUGINS = getenv("EXTRA_PLUGINS", True)
+EXTRA_PLUGINS = getenv("EXTRA_PLUGINS", "False")
 
 # Fill False if you Don't want to load extra plugins
 
