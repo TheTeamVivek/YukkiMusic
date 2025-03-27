59094ed87b9d7b0d486b1238f7db56cfc8eaa795
﮼محمد <saroksurarazo@gmail.com>
2025-03-27 14:48:59 +0300

    Update config.py
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>

--- config/config.py
+++ config/config.py
@@ -45,7 +45,7 @@ DURATION_LIMIT_MIN = int(
 )  # Remember to give value in Minutes
 
 
-EXTRA_PLUGINS = getenv("EXTRA_PLUGINS", "False")
+EXTRA_PLUGINS = is_true(getenv("EXTRA_PLUGINS", "False"))
 
 # Fill False if you Don't want to load extra plugins
 
