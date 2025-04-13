2a13faf89d7da5822d9b1601563ef773830b9e3d
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-13 15:21:04 +0530

    Update enum.py

--- YukkiMusic/core/enum.py
+++ YukkiMusic/core/enum.py
@@ -6,13 +6,18 @@ class PlayType(Enum):
     PAUSED = auto()  # Paused
     MUTED = auto()  # Muted
 
+class SpotifyType(Enum):
+    TRACK = auto()
+    PLAYLIST = auto()
+    ALBUM = auto()
+    ARTIST = auto()
 
 class SourceType(Enum):
     APPLE = "Apple"
     RESSO = "Resson"
     SAAVN = "JioSaavn"
     SOUNDCLOUD = "Soundcloud"
-    SPOTIFY = "Spotify"
+    SPOTIFY = SpotifyType
     TELEGRAM = "Telegram"
     YOUTUBE = "YouTube"
     M3U8 = "M3U8 Urls"
