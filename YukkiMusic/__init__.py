3579ef1334cd25e730c3082e3751eb4c49bc1608
विवेक कुमार <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-17 17:26:11 +0530

    Update __init__.py

--- YukkiMusic/__init__.py
+++ YukkiMusic/__init__.py
@@ -15,7 +15,7 @@ from YukkiMusic.core.bot import YukkiBot
 from YukkiMusic.core.dir import dirr
 from YukkiMusic.core.git import git
 from YukkiMusic.core.telethon import TelethonClient
-from YukkiMusic.core.userbot import Userbot
+from YukkiMusic.core.userbot import userbot
 
 from .logging import logger
 
@@ -37,7 +37,6 @@ tbot = TelethonClient(
     api_hash=config.API_HASH,
     flood_sleep_threshold=240,
 )
-userbot = Userbot()
 
 # Directories
 dirr()
