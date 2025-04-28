180976767f9551d90f6bc314ba8464065433fb57
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-28 18:12:43 +0530

    Update __init__.py

--- YukkiMusic/utils/__init__.py
+++ YukkiMusic/utils/__init__.py
@@ -12,6 +12,6 @@ from .channelplay import *
 from .database import *
 from .decorators import *
 from .formatters import *
-from .inline import *
+#from .inline import *
 from .pastebin import *
 from .sys import *
