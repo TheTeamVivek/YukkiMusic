24e7cf62167799641de77130630e0c636bab3f6a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-28 14:35:43 +0530

    Update misc.py

--- YukkiMusic/utils/misc.py
+++ YukkiMusic/utils/misc.py
@@ -1,3 +1,13 @@
+#
+# Copyright (C) 2024-2025 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from telethon.tl import types as _types
 from YukkiMusic.database import get_lang as _get_lang
 
