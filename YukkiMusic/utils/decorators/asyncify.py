59e8439fe25b2d7909b1182a36851b94ed432836
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-28 21:23:08 +0530

    Update asyncify.py

--- YukkiMusic/utils/decorators/asyncify.py
+++ YukkiMusic/utils/decorators/asyncify.py
@@ -8,11 +8,8 @@ __all__ = ["asyncify"]
 
 def asyncify(func: Callable) -> Callable[..., Any]:
     @wraps(func)
-    def wrapper(*args: Any, **kwargs: Any) -> Any:
-        async def run():
-            pfunc = partial(func, *args, **kwargs)
-            return await asyncio.to_thread(pfunc)
-
-        return run()
+    async def wrapper(*args: Any, **kwargs: Any) -> Any:
+        pfunc = partial(func, *args, **kwargs)
+        return await asyncio.to_thread(pfunc)
 
     return wrapper
