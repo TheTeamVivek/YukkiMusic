a1faade4486bae26c9bbd24fba2fa8a38707c490
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-28 14:00:20 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+# pylint: disabe=*
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
