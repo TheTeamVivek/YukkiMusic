53dc8b419f125c373341422704cde567744a696a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-28 14:28:25 +0530

    update YukkiMusic/utils/__init__.py

--- YukkiMusic/utils/__init__.py
+++ YukkiMusic/utils/__init__.py
@@ -11,7 +11,7 @@ from telethon.tl import types as _types
 
 from .channelplay import *
 from .database import *
-from .decorators import *
+#from .decorators import *
 from .exceptions import *
 from .formatters import *
 from .inline import *
