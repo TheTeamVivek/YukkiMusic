ef323102a4df2d17eacef48e915be3357ba653d3
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-29 20:59:05 +0530

    Update language.py

--- YukkiMusic/utils/decorators/language.py
+++ YukkiMusic/utils/decorators/language.py
@@ -7,7 +7,7 @@
 #
 # All rights reserved.
 #
-from decorator import decorator
+import functools
 from telethon.tl.types import User
 
 from strings import get_string
@@ -21,30 +21,38 @@ from YukkiMusic.utils.database import (
 __all__ = ["language"]
 
 
-@decorator
-async def language(func, event, no_check=False):
-    chat = await event.get_chat()
-    try:
-        language = await get_lang(chat.id)
-        language = get_string(language)
-    except Exception:
-        language = get_string("en")
+def language(func=None, *, no_check=False):
+    def decorator(f):
+        @functools.wraps(f)
+        async def wrapper(event):
+            chat = await event.get_chat()
+            try:
+                lang_code = await get_lang(chat.id)
+                language = get_string(lang_code)
+            except Exception:
+                language = get_string("en")
 
-    if no_check:
-        return await func(event, language)
+            if no_check:
+                return await f(event, language)
 
-    if not await is_maintenance():
-        if event.sender_id not in SUDOERS:
-            if isinstance(chat, User):
-                if event.message:
-                    return await event.reply(language["maint_4"])
-                return await event.answer(language["maint_4"], alert=True)
-            return
+            if not await is_maintenance():
+                if event.sender_id not in SUDOERS:
+                    if isinstance(chat, User):
+                        if event.message:
+                            return await event.reply(language["maint_4"])
+                        return await event.answer(language["maint_4"], alert=True)
+                    return
 
-    if event.message and await is_commanddelete_on(chat.id):
-        try:
-            await update.delete()
-        except Exception:
-            pass
+            if event.message and await is_commanddelete_on(chat.id):
+                try:
+                    await event.delete()
+                except Exception:
+                    pass
 
-    return await func(event, language)
+            return await f(event, language)
+        return wrapper
+
+    if func is None:
+        return decorator
+    else:
+        return decorator(func)
