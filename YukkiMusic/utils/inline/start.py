27ee5d6ac3f65c1539e7939e08e466f4932c9939
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-04-30 09:03:35 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -41,57 +41,8 @@ def start_pannel(_):
     return buttons
 
 
-"""def private_panel(_, owner: bool | int = None):
-    buttons = [[_Button.inline(text=_["S_B_8"], data="settings_back_helper")]]
-    if _config.SUPPORT_CHANNEL and _config.SUPPORT_GROUP:
-        buttons.append(
-            [
-                _Button.url(text=_["S_B_4"], url=_config.SUPPORT_CHANNEL),
-                _Button.url(text=_["S_B_3"], url=_config.SUPPORT_GROUP),
-            ]
-        )
-    else:
-        if _config.SUPPORT_CHANNEL:
-            buttons.append([_Button.url(text=_["S_B_4"], url=_config.SUPPORT_CHANNEL)])
-        if _config.SUPPORT_GROUP:
-            buttons.append([_Button.url(text=_["S_B_3"], url=_config.SUPPORT_GROUP)])
-    buttons.append(
-        [
-            _Button.url(
-                text=_["S_B_5"],
-                url=f"https://t.me/{_tbot.username}?startgroup=true",
-            )
-        ]
-    )
-    if _config.GITHUB_REPO and owner:
-        buttons.append(
-            [
-                _types.InputKeyboardButtonUserProfile(text=_["S_B_7"], user_id=owner),
-                _Button.url(text=_["S_B_6"], url=_config.GITHUB_REPO),
-            ]
-        )
-    else:
-        if _config.GITHUB_REPO:
-            buttons.append(
-                [
-                    _Button.url(text=_["S_B_6"], url=_config.GITHUB_REPO),
-                ]
-            )
-
-        if owner:
-            buttons.append(
-                [
-                    _types.InputKeyboardButtonUserProfile(
-                        text=_["S_B_7"], user_id=owner
-                    ),
-                ]
-            )
-    buttons.append([_Button.inline(text=_["ST_B_6"], data="LG")])
-    return buttons"""
-
-
 def private_panel(_, owner: bool | int = None):
-    keyboard = InlineKeyboard(row_width=2)
+    keyboard = InlineKeyboard(row_width=2) # Using PyKeyboard because [ may be not sure] Telethon didn't think that _types.InputKeyboardButtonUserProfile is an inline button
 
     keyboard.row(_Button.inline(text=_["S_B_8"], data="settings_back_helper"))
 
@@ -114,7 +65,7 @@ def private_panel(_, owner: bool | int = None):
 
     if _config.GITHUB_REPO and owner:
         keyboard.add(
-            _types.InputKeyboardButtonUserProfile(text=_["S_B_7"], user_id=owner),
+            _types.InputKeyboardButtonUserProfile(text=_["S_B_7"], user_id=await tbot.get_input_entity(owner)),
             _Button.url(text=_["S_B_6"], url=_config.GITHUB_REPO),
         )
     else:
@@ -122,9 +73,9 @@ def private_panel(_, owner: bool | int = None):
             keyboard.row(_Button.url(text=_["S_B_6"], url=_config.GITHUB_REPO))
         if owner:
             keyboard.row(
-                _types.InputKeyboardButtonUserProfile(text=_["S_B_7"], user_id=owner)
+                _types.InputKeyboardButtonUserProfile(text=_["S_B_7"], user_id=await tbot.get_input_entity(owner))
             )
 
     keyboard.row(_Button.inline(text=_["ST_B_6"], data="LG"))
 
-    return keyboard.rows
+    return keyboard
