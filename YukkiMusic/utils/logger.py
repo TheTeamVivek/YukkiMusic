4b3424fd52d1f0fc3a9318e2f02e0e10c4f82371
﮼محمد <saroksurarazo@gmail.com>
2025-05-16 10:20:48 +0300

    Support multiple lang in logger ! (#85)
    * Update logger.py
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    * Update ar.yml
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    * Update ckb.yml
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    * Update en.yml
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    * Update hi.yml
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    * Update tr.yml
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>
    
    ---------
    
    Signed-off-by: ﮼محمد <saroksurarazo@gmail.com>

--- YukkiMusic/utils/logger.py
+++ YukkiMusic/utils/logger.py
@@ -6,31 +6,47 @@
 # Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
 #
 # All rights reserved.
-from config import LOG, LOG_GROUP_ID
+
 from YukkiMusic import app
-from YukkiMusic.utils.database import is_on_off
+from YukkiMusic.utils.database import get_lang, is_on_off
+from config import LOG, LOG_GROUP_ID
+from strings import get_string
 
 
 async def play_logs(message, streamtype):
     if await is_on_off(LOG):
+        try:
+            language = await get_lang(message.chat.id)
+            _ = get_string(language)
+        except Exception:
+            _ = get_string("en")
+
         if message.chat.username:
             chatusername = f"@{message.chat.username}"
         else:
             chatusername = "Private Group"
 
-        logger_text = f"""
-**{app.mention} Play Log**
+        if message.from_user.username:
+            username = f"@{message.from_user.username}"
+        else:
+            username = "Unknow"
 
-**Chat ID:** `{message.chat.id}`
-**Chat Name:** {message.chat.title}
-**Chat Username:** {chatusername}
+        if message.reply_to_message:
+            query = "Replied Message"
+        else:
+            query = message.text.split(None, 1)[1]
 
-**User ID:** `{message.from_user.id}`
-**Name:** {message.from_user.mention}
-**Username:** @{message.from_user.username}
-
-**Query:** {message.text.split(None, 1)[1]}
-**Stream Type:** {streamtype}"""
+        logger_text = _["logger_text"].format(
+            bot_mention=app.mention,
+            chat_id=message.chat.id,
+            title=message.chat.title,
+            chatusername=chatusername,
+            sender_id=message.from_user.id,
+            user_mention=message.from_user.mention,
+            username=username,
+            query=query,
+            streamtype=streamtype,
+        )
         if message.chat.id != LOG_GROUP_ID:
             try:
                 await app.send_message(
@@ -38,6 +54,6 @@ async def play_logs(message, streamtype):
                     text=logger_text,
                     disable_web_page_preview=True,
                 )
-            except Exception:
-                pass
+            except Exception as e:
+                print(e)
         return
