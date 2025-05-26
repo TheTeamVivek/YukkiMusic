72285d1ce08b5fe9052c9825504bf2028cf43113
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-26 10:27:58 +0530

    raise error to disable fallback

--- YukkiMusic/utils/fallback.py
+++ YukkiMusic/utils/fallback.py
@@ -12,6 +12,7 @@ from YukkiMusic.platforms import saavn
 
 
 async def download(title, video):
+    raise ValueError("Failed to download song from youtube")
     video = None
     path, details = await saavn.download(title)
     return path, details, video
