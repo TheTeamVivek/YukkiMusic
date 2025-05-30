09c59b2e8943687d43cae3b213e167ef6f401700
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-30 08:46:21 +0530

    refactor: logging simplified

--- YukkiMusic/logging.py
+++ YukkiMusic/logging.py
@@ -10,12 +10,12 @@
 
 import logging
 from logging.handlers import RotatingFileHandler
-
 from config import LOG_FILE_NAME
 
 logging.basicConfig(
     level=logging.INFO,
-    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    format="{asctime} - {levelname} - {message}",
+    style="{",
     datefmt="%d-%b-%y %H:%M:%S",
     handlers=[
         RotatingFileHandler(LOG_FILE_NAME, maxBytes=5000000, backupCount=10),
@@ -23,15 +23,8 @@ logging.basicConfig(
     ],
 )
 
-logging.getLogger("httpx").setLevel(logging.ERROR)
-logging.getLogger("ntgcalls").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
-logging.getLogger("pytgcalls").setLevel(logging.ERROR)
-logging.getLogger("pymongo").setLevel(logging.ERROR)
-
-# ntgcalls_logger = logging.getLogger("ntgcalls")
-# ntgcalls_logger.setLevel(logging.CRITICAL)
-# ntgcalls_logger.propagate = False
+for noisy in ["httpx", "ntgcalls", "pyrogram", "pytgcalls", "pymongo"]:
+    logging.getLogger(noisy).setLevel(logging.ERROR)
 
 
 def LOGGER(name: str) -> logging.Logger:
