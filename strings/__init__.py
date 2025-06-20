72acb099fb2c19a7a9070d2dfa089907b884ea89
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-06-20 09:26:03 +0530

    fix: return English dict as fallback in get_string

--- strings/__init__.py
+++ strings/__init__.py
@@ -56,7 +56,7 @@ def load_yaml(file_path: str) -> dict:
 
 
 def get_string(lang: str):
-    return languages.get(lang, "en")
+    return languages.get(lang, languages["en"])
 
 
 def format_value(value):
