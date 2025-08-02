20f7174bf2ad4dbc6f75c07d34589e349166f270
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-08-02 18:51:41 +0530

    improve

--- strings/attrdict.py
+++ strings/attrdict.py
@@ -9,24 +9,22 @@
 
 # pylint: disable=missing-module-docstring
 
-
 class AttrDict(dict):
     """Dict with attribute-style access."""
 
     def __init__(self, *args, **kwargs):
         super().__init__()
-        data = dict(*args, **kwargs)
-        for k, v in data.items():
-            self[k] = self._wrap(v)
+        for k, v in dict(*args, **kwargs).items():
+            super().__setitem__(k, self._wrap(v))
 
     def __getattr__(self, item):
         try:
             return self[item]
-        except KeyError as e:
-            raise AttributeError(f"'AttrDict' has no attribute '{item}'") from e
+        except KeyError:
+            raise AttributeError(f"{self.__class__.__name__!r} object has no attribute {item!r}")
 
     def __setattr__(self, key, value):
-        self[key] = value
+        self[key] = self._wrap(value)
 
     def __setitem__(self, key, value):
         super().__setitem__(key, self._wrap(value))
@@ -38,10 +36,21 @@ class AttrDict(dict):
     def setdefault(self, key, default=None):
         return super().setdefault(key, self._wrap(default))
 
-    @staticmethod
-    def _wrap(value):
+    def __dir__(self):
+        return list(self.keys()) + super().__dir__()
+
+    def copy(self):
+        return AttrDict(self)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({super().__repr__()})"
+
+    @classmethod
+    def _wrap(cls, value):
         if isinstance(value, dict):
-            return AttrDict(value)
+            return cls(value)
         if isinstance(value, list):
-            return [AttrDict(v) if isinstance(v, dict) else v for v in value]
+            return [cls._wrap(v) for v in value]
+        if isinstance(value, tuple):
+            return tuple(cls._wrap(v) for v in value)
         return value
