303f0694b090c56cf3575929ca81cc2bdc952d95
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-11-14 08:26:30 +0530

    Update skip.go

--- internal/modules/skip.go
+++ internal/modules/skip.go
@@ -41,81 +41,90 @@ func cskipHandler(m *telegram.NewMessage) error {
 }
 
 func handleSkip(m *telegram.NewMessage, cplay bool) error {
-	r, err := getEffectiveRoom(m, cplay)
-	if err != nil {
-		m.Reply(err.Error())
-		return telegram.EndGroup
-	}
-	chatID := m.ChannelID()
-	if !r.IsActiveChat() {
-		m.Reply("⚠️ <b>No active playback.</b>\nThere’s nothing playing right now.")
-		return telegram.EndGroup
-	}
-	mention := utils.MentionHTML(m.Sender)
-	if len(r.Queue) == 0 && r.Loop == 0 {
-		r.Destroy()
-		m.Reply(fmt.Sprintf("⏹️ Playback stopped. Queue is empty.\nSkipped by: %s", mention))
-		return telegram.EndGroup
-	}
+        r, err := getEffectiveRoom(m, cplay)
+        if err != nil {
+                m.Reply(err.Error())
+                return telegram.EndGroup
+        }
 
-	t := r.NextTrack()
-	mystic, err := core.Bot.SendMessage(chatID, "📥 Downloading your next track...")
-	if err != nil {
-		gologging.ErrorF("[skip.go] Failed to send status message: %v", err)
-	}
+        chatID := m.ChannelID()
+        if !r.IsActiveChat() {
+                m.Reply(F(chatID, "room_no_active"))
+                return telegram.EndGroup
+        }
 
-	path, err := platforms.Download(context.Background(), t, mystic)
-	if err != nil {
-		gologging.ErrorF("Download failed for %s: %v", t.URL, err)
-		errMsg := fmt.Sprintf("❌ Failed to download next track.\nError: %v", err)
-		if mystic != nil {
-			utils.EOR(mystic, errMsg)
-		} else {
-			core.Bot.SendMessage(chatID, errMsg)
-		}
-		r.Destroy()
-		return telegram.EndGroup
-	}
+        mention := utils.MentionHTML(m.Sender)
 
-	err = r.Play(t, path)
-	if err != nil {
-		errMsg := "❌ Failed to play song."
-		if mystic != nil {
-			utils.EOR(mystic, errMsg)
-		} else {
-			core.Bot.SendMessage(chatID, errMsg)
-		}
-		r.Destroy()
-		return telegram.EndGroup
-	}
+        if len(r.Queue) == 0 && r.Loop == 0 {
+                r.Destroy()
+                m.Reply(F(chatID, "skip_stopped", locales.Arg{
+                        "user": mention,
+                }))
+                return telegram.EndGroup
+        }
 
-	title := utils.ShortTitle(t.Title, 25)
-	safeTitle := html.EscapeString(title)
-	msgText := fmt.Sprintf(
-		"<b>🎵 Now Playing:</b>\n\n<b>▫ Track:</b> <a href=\"%s\">%s</a>\n<b>▫ Duration:</b> %s\n<b>▫ Requested by:</b> %s",
-		t.URL,
-		safeTitle,
-		formatDuration(t.Duration),
-		t.BY,
-	)
-	opt := telegram.SendOptions{
-		ParseMode:   "HTML",
-		ReplyMarkup: core.GetPlayMarkup(r, false),
-	}
-	if t.Artwork != "" {
-		opt.Media = utils.CleanURL(t.Artwork)
-	}
+        t := r.NextTrack()
 
-	var newMystic *telegram.NewMessage
-	if mystic != nil {
-		newMystic, _ = utils.EOR(mystic, msgText, opt)
-	} else {
-		newMystic, _ = core.Bot.SendMessage(chatID, msgText, &opt)
-	}
+        mystic, err := core.Bot.SendMessage(chatID, F(chatID, "stream_downloading_next"))
+        if err != nil {
+                gologging.ErrorF("[skip.go] err: %v", err)
+        }
 
-	if newMystic != nil {
-		r.SetMystic(newMystic)
-	}
+        path, err := platforms.Download(context.Background(), t, mystic)
+        if err != nil {
+                txt := F(chatID, "stream_download_fail", locales.Arg{
+                        "error": err.Error(),
+                })
 
-	return telegram.EndGroup
+                if mystic != nil {
+                        utils.EOR(mystic, txt)
+                } else {
+                        core.Bot.SendMessage(chatID, txt)
+                }
+
+                r.Destroy()
+                return telegram.EndGroup
+        }
+
+        if err := r.Play(t, path); err != nil {
+                txt := F(chatID, "stream_play_fail")
+                if mystic != nil {
+                        utils.EOR(mystic, txt)
+                } else {
+                        core.Bot.SendMessage(chatID, txt)
+                }
+                r.Destroy()
+                return telegram.EndGroup
+        }
+
+        title := utils.ShortTitle(t.Title, 25)
+        safeTitle := html.EscapeString(title)
+
+        msg := F(chatID, "stream_now_playing", locales.Arg{
+                "url":      t.URL,
+                "title":    safeTitle,
+                "duration": formatDuration(t.Duration),
+                "by":       t.BY,
+        })
+
+        opt := telegram.SendOptions{
+                ParseMode:   "HTML",
+                ReplyMarkup: core.GetPlayMarkup(r, false),
+        }
+        if t.Artwork != "" {
+                opt.Media = utils.CleanURL(t.Artwork)
+        }
+
+        var newMystic *telegram.NewMessage
+        if mystic != nil {
+                newMystic, _ = utils.EOR(mystic, msg, opt)
+        } else {
+                newMystic, _ = core.Bot.SendMessage(chatID, msg, &opt)
+        }
+
+        if newMystic != nil {
+                r.SetMystic(newMystic)
+        }
+
+        return telegram.EndGroup
 }
