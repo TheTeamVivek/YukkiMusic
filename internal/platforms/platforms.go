9aed3daa1c521464112d4a16509274cb39341eb1
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-12-06 12:34:19 +0530

    Update platforms.go

--- internal/platforms/platforms.go
+++ internal/platforms/platforms.go
@@ -144,7 +144,8 @@ func GetTracks(m *telegram.NewMessage, video bool) ([]*state.Track, error) {
 			// for tg medias we allow only Video when replied media is a video
 			t.Video = isVideo
 			if isVideo {
-				thumbPath := filepath.Join("downloads", "thumb_"+t.ID+".jpg")
+os.MkdirAll("cache", os.ModePerm)
+				thumbPath := filepath.Join("cache", "thumb_"+t.ID+".jpg")
 
 				if _, err := os.Stat(thumbPath); os.IsNotExist(err) {
 					path, err := rmsg.Download(&telegram.DownloadOptions{
