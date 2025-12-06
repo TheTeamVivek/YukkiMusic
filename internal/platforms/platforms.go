aa1917b36619e3d7817ac1ab147cbe71086bc400
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-12-06 12:56:26 +0530

    Update platforms.go

--- internal/platforms/platforms.go
+++ internal/platforms/platforms.go
@@ -144,7 +144,11 @@ func GetTracks(m *telegram.NewMessage, video bool) ([]*state.Track, error) {
 			// for tg medias we allow only Video when replied media is a video
 			t.Video = isVideo
 			if isVideo {
-				os.MkdirAll("cache", os.ModePerm)
+				err := os.MkdirAll("cache", os.ModePerm)
+if err != nil {
+gologiing.Error("Failed to create cache folder: "+err.Error())
+                        return []*state.Track{t}, nil
+}
 				thumbPath := filepath.Join("cache", "thumb_"+t.ID+".jpg")
 
 				if _, err := os.Stat(thumbPath); os.IsNotExist(err) {
