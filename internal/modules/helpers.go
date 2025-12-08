44c99ed170e35200d91ba4f2e9af3db0da7aee8a
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-12-08 08:18:02 +0530

    Update helpers.go

--- internal/modules/helpers.go
+++ internal/modules/helpers.go
@@ -318,9 +318,8 @@ func warnAndLeave(client *tg.Client, chatID int64) {
 		if err := client.LeaveChannel(chatID); err != nil {
 			gologging.ErrorF("Failed to leave non-supergroup chatID=%d: %v", chatID, err)
 		}
-		if ass, err := core.Assistants.ForChat(chatID); err == nil {
-			ass.Client.LeaveChannel(chatID)
-		}
+core.Assistans.WithAssistant(chatID, func(ass *core.Assistant){ass.Client.LeaveChannel(chatID)}
+		
 	}()
 }
 
